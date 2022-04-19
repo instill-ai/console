8f647d7fc29da9786939d28275b31a5279f8f759
pochun-chiu <pochun.chiu@instill.tech>
2022-04-19 13:15:29 +0800

    refactor: remove not-used components

--- src/components/common/Navbar/NavbarRoot.tsx
+++
@@ -1,7 +0,0 @@
-import { FC } from "react";
-
-interface Props {}
-
-export const NavbarRoot: FC<Props> = ({ children }) => {
-  return <div>{children}</div>;
-};
--- src/components/ui/Link/Link.tsx
+++
@@ -1,9 +0,0 @@
-import NextLink, { LinkProps as NextLinkProps } from "next/link";
-
-export const Link: React.FC<NextLinkProps> = ({ href, children, ...props }) => {
-  return (
-    <NextLink href={href}>
-      <a {...props}>{children}</a>
-    </NextLink>
-  );
-};
