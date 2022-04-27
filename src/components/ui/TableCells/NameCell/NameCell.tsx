c0306069490d9944853d61f56df0e44b96d2b369
pochun-chiu <pochun.chiu@instill.tech>
2022-04-27 16:04:37 +0800

    feat: adapt StatusIndicator at NameCell

--- src/components/ui/TableCells/NameCell/NameCell.tsx
+++ src/components/ui/TableCells/NameCell/NameCell.tsx
@@ -1,11 +1,7 @@
-import { FC, ReactNode } from "react";
+import { FC } from "react";
 import cn from "clsx";
-import {
-  StatusErrorIcon,
-  StatusOffIcon,
-  StatusOnIcon,
-} from "@instill-ai/design-system";
 import { getHumanReadableStringFromTime } from "utils";
+import StatusIndicator from "components/ui/StatusIndicator";
 
 export type NameCellProps = {
   status: "on" | "off" | "error";
@@ -15,35 +11,16 @@ export type NameCellProps = {
 };
 
 const NameCell: FC<NameCellProps> = ({ status, width, updatedAt, name }) => {
-  let statusIcon: ReactNode;
-
   const time = getHumanReadableStringFromTime(updatedAt, Date.now());
-
-  switch (status) {
-    case "error":
-      statusIcon = (
-        <StatusErrorIcon
-          width="w-[18px]"
-          height="h-[18px]"
-          position="mb-auto"
-        />
-      );
-      break;
-    case "on":
-      statusIcon = (
-        <StatusOnIcon width="w-[18px]" height="h-[18px]" position="mb-auto" />
-      );
-      break;
-    case "off":
-      statusIcon = (
-        <StatusOffIcon width="w-[18px]" height="h-[18px]" position="mb-auto" />
-      );
-      break;
-  }
 
   return (
     <div className={cn("flex flex-row", width)}>
-      {statusIcon}
+      <StatusIndicator
+        status={status}
+        width="w-[18px]"
+        height="h-[18px]"
+        position="mb-auto"
+      />
       <div className="flex flex-col gap-y-2">
         <h3 className="instill-text-h3">{name}</h3>
         <p className="instill-text-small text-instillGrey50">{`last sync ${time}`}</p>
