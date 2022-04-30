789f5b7c21f093c0aac20f6d831b3528c206e35e
pochun-chiu <pochun.chiu@instill.tech>
2022-04-30 16:42:32 +0800

    feat: add hooks into import alias and fix storybook import issue

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,15 @@
+const path = require("path");
 module.exports = {
+  webpackFinal: async (config) => {
+    config.resolve.alias["@"] = path.resolve(__dirname, "../src");
+    config.resolve.alias["@/components"] = path.resolve(
+      __dirname,
+      "../src/components"
+    );
+    config.resolve.alias["@/types"] = path.resolve(__dirname, "../src/types");
+    config.resolve.alias["@/hooks"] = path.resolve(__dirname, "../src/hooks");
+    return config;
+  },
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
