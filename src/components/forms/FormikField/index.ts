7de348eec4cf9b421a78afdef76c2f75708454fa
pochun-chiu <pochun.chiu@instill.tech>
2022-05-03 22:02:51 +0800

    feat: add Formik UploadFielField wrapper

--- src/components/forms/FormikField/index.ts
+++ src/components/forms/FormikField/index.ts
@@ -1,4 +1,15 @@
 import SingleSelect from "./SingleSelect";
 import type { SingleValueProps } from "react-select";
+import TextField from "./TextField";
+import type { TextFieldProps } from "./TextField";
+import UploadFileField from "./UploadFileField";
+import type { UploadFileFieldProps } from "./UploadFileField";
 
-export { SingleSelect, SingleValueProps };
+export {
+  SingleSelect,
+  SingleValueProps,
+  TextField,
+  TextFieldProps,
+  UploadFileField,
+  UploadFileFieldProps,
+};
