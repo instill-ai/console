10c7c6fc23f957dc3aa509a5f703c21799fea9bb
pochun-chiu <pochun.chiu@instill.tech>
2022-05-04 19:48:11 +0800

    feat: componentize SteupModelStep and export it

--- src/components/forms/CreatePipelineForm/SetupModelStep/SetupModelStep.tsx
+++ src/components/forms/CreatePipelineForm/SetupModelStep/SetupModelStep.tsx
@@ -1,4 +1,5 @@
-import { Dispatch, FC, SetStateAction } from "react";
+import { FormVerticalDividers } from "@/components/ui";
+import { FC } from "react";
 import { FormikStep } from "../../FormikMultiStep";
 import { StepNumberState } from "../CreatePipelineDataSourceForm/CreatePipelineDataSourceForm";
 import CreateNewModelFlow from "./CreateNewModelFlow";
@@ -11,12 +12,7 @@ const SetupModelStep: FC<SetupModelStepProps> = (props) => {
     <FormikStep multiGroup={true}>
       <div className="flex flex-1 flex-row">
         <UseExistingModelFlow {...props} />
-        <div className="relative mx-5 h-full w-6">
-          <p className="instill-text-h3 absolute top-5 z-10 bg-instillGrey05">
-            OR
-          </p>
-          <div className="absolute left-1/2 h-full border-r"></div>
-        </div>
+        <FormVerticalDividers />
         <CreateNewModelFlow {...props} />
       </div>
     </FormikStep>
