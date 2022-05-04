c9a8bd4d5397de97b6fc2ccd9f6af216d95688c6
pochun-chiu <pochun.chiu@instill.tech>
2022-05-04 23:25:36 +0800

    feat: add SetupSourceStep

---
+++ src/components/forms/CreatePipelineForm/SetupSourceStep/SetupSourceStep.tsx
@@ -0,0 +1,22 @@
+import { FormVerticalDividers } from "@/components/ui";
+import { FC } from "react";
+import { FormikStep } from "../../FormikMultiStep";
+import { StepNumberState } from "../CreatePipelineForm";
+import CreateNewSourceFlow from "./CreateNewSourceFlow";
+import UseExistingSourceFlow from "./UseExistingSourceFlow";
+
+export type SetupSourceStepProps = StepNumberState;
+
+const SetupSourceStep: FC<SetupSourceStepProps> = (props) => {
+  return (
+    <FormikStep>
+      <div className="flex flex-1 flex-row">
+        <UseExistingSourceFlow {...props} />
+        <FormVerticalDividers {...props} />
+        <CreateNewSourceFlow {...props} />
+      </div>
+    </FormikStep>
+  );
+};
+
+export default SetupSourceStep;
