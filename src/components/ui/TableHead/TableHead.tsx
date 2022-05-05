da0e18eb46b2d5e4771cdf24575f1ae6d6fd9d84
pochun-chiu <pochun.chiu@instill.tech>
2022-05-05 21:45:34 +0800

    fix: fix TableHead style issue

--- src/components/ui/TableHead/TableHead.tsx
+++ src/components/ui/TableHead/TableHead.tsx
@@ -1,17 +1,39 @@
 import { FC } from "react";
 import { Item } from "../Tables";
+import cn from "clsx";
 
 export type TableHeadProps = {
   items: Item[];
+  bgColor: string;
 };
 
-const TableHead: FC<TableHeadProps> = ({ items }) => {
+const TableHead: FC<TableHeadProps> = ({ items, bgColor }) => {
   return (
-    <thead>
+    <thead className={cn("px-[24px] py-[5px]", bgColor)}>
       <tr>
-        {items.map((e) => (
-          <th key={e.key}>{e.item}</th>
-        ))}
+        {items.map((e, index) => {
+          if (index === 0) {
+            return (
+              <th className="py-[5px] pl-[23px]" key={e.key}>
+                {e.item}
+              </th>
+            );
+          }
+
+          if (index === items.length - 1) {
+            return (
+              <th className="py-[5px] pr-[23px]" key={e.key}>
+                {e.item}
+              </th>
+            );
+          }
+
+          return (
+            <th className="py-[5px]" key={e.key}>
+              {e.item}
+            </th>
+          );
+        })}
       </tr>
     </thead>
   );
