843872df733391fedc38e20087c2f2685d986dfa
pochun-chiu <pochun.chiu@instill.tech>
2022-05-06 14:39:31 +0800

    feat: export Table related components

---
+++ src/components/ui/TableCells/index.ts
@@ -0,0 +1,8 @@
+import InstanceCell from "./InstanceCell/InstanceCell";
+import type { InstanceCellProps } from "./InstanceCell/InstanceCell";
+import NameCell from "./NameCell";
+import type { NameCellProps } from "./NameCell";
+import TypeCell, { TypeCellProps } from "./TypeCell";
+
+export { InstanceCell, NameCell, TypeCell };
+export type { InstanceCellProps, NameCellProps, TypeCellProps };
--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -3,11 +3,32 @@ import StatusIndicator from "./StatusIndicator";
 import type { StatusIndicatorProps } from "./StatusIndicator";
 import FormVerticalDividers from "./FormVerticalDividers";
 import PipelinePagePlaceholder from "./PipelinePagePlaceholder";
+import TableContainer from "./TableContainer";
+import TableRow from "./TableRow";
+import { InstanceCell, NameCell, TypeCell } from "./TableCells";
+import type {
+  InstanceCellProps,
+  NameCellProps,
+  TypeCellProps,
+} from "./TableCells";
+import { PipelineTableHead , PipelineTableHeadProps } from "./TableHeads";
 
 export {
   Sidebar,
   StatusIndicator,
   FormVerticalDividers,
   PipelinePagePlaceholder,
+  TableContainer,
+  TableRow,
+  InstanceCell,
+  NameCell,
+  TypeCell,
+  PipelineTableHead,
 };
-export type { StatusIndicatorProps };
+export type {
+  StatusIndicatorProps,
+  InstanceCellProps,
+  NameCellProps,
+  TypeCellProps,
+  PipelineTableHeadProps,
+};
