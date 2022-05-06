4364243604ec959bbf863d45fbc25a362e8caef3
pochun-chiu <pochun.chiu@instill.tech>
2022-05-06 09:56:33 +0800

    refactor: refactor PageTitle to let it have title, breadcrumb and buttom

--- src/components/ui/PageTitle/PageTitle.tsx
+++ src/components/ui/PageTitle/PageTitle.tsx
@@ -1,11 +1,40 @@
-import { FC } from "react";
+import { useRouter } from "next/router";
+import { FC, useCallback } from "react";
+import Breadcrumb, { BreadcrumbProps } from "../Breadcrumb";
+import { PrimaryButton } from "../Buttons";
 
 export type PageTitleProps = {
   title: string;
+  buttonName: string;
+  buttonLink: string;
+  breadcrumbs: BreadcrumbProps["breadcrumbs"];
 };
 
-const PageTitle: FC<PageTitleProps> = ({ title }) => {
-  return <h2 className="instill-text-h2 text-black">{title}</h2>;
+const PageTitle: FC<PageTitleProps> = ({
+  title,
+  buttonName,
+  buttonLink,
+  breadcrumbs,
+}) => {
+  const router = useRouter();
+  const onClickHandler = useCallback(() => {
+    router.push(buttonLink);
+  }, [router]);
+  return (
+    <div className="flex flex-col">
+      <Breadcrumb breadcrumbs={breadcrumbs} />
+      <div className="flex flex-row">
+        <h2 className="instill-text-h2 mr-auto text-black">{title}</h2>;
+        <PrimaryButton
+          type="button"
+          disabled={false}
+          onClickHandler={onClickHandler}
+        >
+          {buttonName}
+        </PrimaryButton>
+      </div>
+    </div>
+  );
 };
 
 export default PageTitle;
