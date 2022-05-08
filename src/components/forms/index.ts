43a36e60eb1db6129181ee77488d770daafa7bef
pochun-chiu <pochun.chiu@instill.tech>
2022-05-08 09:41:33 +0800

    feat: export forms related component

---
+++ src/components/forms/index.ts
@@ -0,0 +1,8 @@
+import CreatePipelineForm from "./CreatePipelineForm";
+import type { StepNumberState } from "./CreatePipelineForm/CreatePipelineForm";
+
+import CreateSourceForm from "./CreateSourceForm";
+import OnboardingForm from "./OnboardingForm";
+
+export { CreatePipelineForm, CreateSourceForm, OnboardingForm };
+export { StepNumberState };
