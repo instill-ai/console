3001aaf5974afcb7c8edb5d1e1e1943c5f3ecd50
pochun-chiu <pochun.chiu@instill.tech>
2022-05-08 09:24:38 +0800

    feat: re-export FormikStep related module

--- src/components/formik/index.ts
+++ src/components/formik/index.ts
@@ -14,11 +14,23 @@ import type {
   UploadFileFieldProps,
 } from "./FormikField";
 
-export { SingleSelect, TextArea, TextField, ToggleField, UploadFileField };
+import { FormikStep, FormikMultiStep } from "./FormikMultiStep";
+import type { FormikMultiStepProps } from "./FormikMultiStep";
+
+export {
+  SingleSelect,
+  TextArea,
+  TextField,
+  ToggleField,
+  UploadFileField,
+  FormikStep,
+  FormikMultiStep,
+};
 export type {
   SingleSelectProps,
   TextAreaProps,
   TextFieldProps,
   ToggleFieldProps,
   UploadFileFieldProps,
+  FormikMultiStepProps,
 };
