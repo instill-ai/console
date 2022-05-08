2ac68a1af16b7cc565d79ab631a52dd6d15e9572
pochun-chiu <pochun.chiu@instill.tech>
2022-05-08 09:57:46 +0800

    feat: add TablePlaceholderBase

---
+++ src/components/ui/TablePlaceholders/TablePlaceholderBase/TablePlaceholderBase.tsx
@@ -0,0 +1,36 @@
+import { FC, ReactElement } from "react";
+import { useRouter } from "next/router";
+
+import { PrimaryButton } from "../../Buttons";
+
+export type TablePlaceholderBaseProps = {
+  placeholderImage: ReactElement;
+  createButtonLink: string;
+};
+
+const TablePlaceholderBase: FC<TablePlaceholderBaseProps> = ({
+  placeholderImage,
+  createButtonLink,
+}) => {
+  const router = useRouter();
+  const handleOnClick = () => {
+    router.push(createButtonLink);
+  };
+  return (
+    <div className="flex h-[300px] w-full flex-row border border-instillGrey15 bg-white">
+      {placeholderImage}
+      <div className="m-auto flex gap-y-5">
+        <h3 className="instill-text-h3 text-instillGrey80">No Pipeline</h3>
+        <PrimaryButton
+          type="button"
+          disabled={false}
+          onClickHandler={handleOnClick}
+        >
+          Create first pipeline
+        </PrimaryButton>
+      </div>
+    </div>
+  );
+};
+
+export default TablePlaceholderBase;
