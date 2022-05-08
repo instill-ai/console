61e783c9f118fb9cbe293f1011684dd30f1412a1
pochun-chiu <pochun.chiu@instill.tech>
2022-05-08 10:15:24 +0800

    feat: export TablePlaceholders

---
+++ src/components/ui/TablePlaceholders/index.ts
@@ -0,0 +1,9 @@
+import DestinationTablePlaceholder from "./DestinationTablePlaceholder";
+import SourceTablePlaceholder from "./SourceTablePlaceholder";
+import PipelineTablePlaceholder from "./PipelineTablePlaceholder";
+
+export {
+  DestinationTablePlaceholder,
+  SourceTablePlaceholder,
+  PipelineTablePlaceholder,
+};
