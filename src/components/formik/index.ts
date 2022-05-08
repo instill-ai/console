da99e48a77e305a961c8188cc0b6af4d96eb6e30
pochun-chiu <pochun.chiu@instill.tech>
2022-05-08 08:54:56 +0800

    feat: re-export FormikField to formik level folder

---
+++ src/components/formik/index.ts
@@ -0,0 +1,24 @@
+import {
+  SingleSelect,
+  TextArea,
+  TextField,
+  ToggleField,
+  UploadFileField,
+} from "./FormikField";
+
+import type {
+  SingleSelectProps,
+  TextAreaProps,
+  TextFieldProps,
+  ToggleFieldProps,
+  UploadFileFieldProps,
+} from "./FormikField";
+
+export { SingleSelect, TextArea, TextField, ToggleField, UploadFileField };
+export type {
+  SingleSelectProps,
+  TextAreaProps,
+  TextFieldProps,
+  ToggleFieldProps,
+  UploadFileFieldProps,
+};
