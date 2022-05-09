974f20dac7179449921e2553b245526e32a98d5d
pochun-chiu <pochun.chiu@instill.tech>
2022-05-09 19:43:23 +0800

    feat: add transformModelStateToStatus

--- src/services/model/ModelServices.tsx
+++ src/services/model/ModelServices.tsx
@@ -1,5 +1,20 @@
+import { Status } from "@/types/general";
+
 export type ModelState =
   | "STATE_ONLINE"
   | "STATE_OFFLINE"
   | "STATE_ERROR"
   | "STATE_UNSPECIFIC";
+
+export const transformModelStateToStatus = (state: ModelState): Status => {
+  switch (state) {
+    case "STATE_ONLINE":
+      return "active";
+    case "STATE_OFFLINE":
+      return "inactive";
+    case "STATE_ERROR":
+      return "error";
+    default:
+      return "unspecific";
+  }
+};
