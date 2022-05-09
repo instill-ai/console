c3e9d03370e88549b91ff97ad6ac0439ea559fb8
pochun-chiu <pochun.chiu@instill.tech>
2022-05-09 15:02:53 +0800

    feat: add CreateModelForm

--- src/components/forms/index.ts
+++ src/components/forms/index.ts
@@ -4,11 +4,13 @@ import type { StepNumberState } from "./CreatePipelineForm/CreatePipelineForm";
 import CreateDestinationForm from "./CreateDestinationForm";
 import CreateSourceForm from "./CreateSourceForm";
 import OnboardingForm from "./OnboardingForm";
+import CreateModelForm from "./CreateModelForm";
 
 export {
   CreatePipelineForm,
   CreateSourceForm,
   OnboardingForm,
   CreateDestinationForm,
+  CreateModelForm,
 };
 export { StepNumberState };
