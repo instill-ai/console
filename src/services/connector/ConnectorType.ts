c0b3954a8df618492f2e6833328f1463a809f74e
pochun-chiu <pochun.chiu@instill.tech>
2022-05-11 10:15:07 +0800

    feat: add connector type

---
+++ src/services/connector/ConnectorType.ts
@@ -0,0 +1,9 @@
+export type Connector = {
+  description: string;
+  // configuration: {},
+  tombstone: boolean;
+  user: string;
+  org: string;
+  create_time: string;
+  update_time: string;
+};
