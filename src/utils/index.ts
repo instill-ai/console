43fd5a7c134fa60666324910647654ef1fccecc9
pochun-chiu <pochun.chiu@instill.tech>
2022-05-11 13:49:05 +0800

    feat: add groupBy

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,10 +1,12 @@
 import { getHumanReadableStringFromTime } from "./timeUtils";
 import { createMockRouter, renderWithContext } from "./testUtils";
 import { getTextWidth } from "./dimensionUtils";
+import { groupBy } from "./arrayUtils";
 
 export {
   getHumanReadableStringFromTime,
   createMockRouter,
   renderWithContext,
   getTextWidth,
+  groupBy,
 };
