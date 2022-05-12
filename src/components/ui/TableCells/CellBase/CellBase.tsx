f0b20fdccb3c70a859e2a33311a900f24d71de09
pochun-chiu <pochun.chiu@instill.tech>
2022-05-12 16:31:40 +0800

    feat: move Link component from CellBase to NameCell

--- src/components/ui/TableCells/CellBase/CellBase.tsx
+++ src/components/ui/TableCells/CellBase/CellBase.tsx
@@ -1,13 +1,11 @@
 import { FC } from "react";
 import cn from "clsx";
-import Link from "next/link";
 
 export type CellBaseProps = {
   paddingTop: string;
   paddingLeft: string;
   paddingRight: string;
   paddingBottom: string;
-  link?: string;
 };
 
 const CellBase: FC<CellBaseProps> = ({
@@ -16,32 +14,11 @@ const CellBase: FC<CellBaseProps> = ({
   paddingBottom,
   paddingLeft,
   paddingRight,
-  link,
 }) => {
   return (
-    <>
-      {link ? (
-        <Link href={link}>
-          <td
-            className={cn(
-              paddingTop,
-              paddingBottom,
-              paddingLeft,
-              paddingRight,
-              link ? "cursor-pointer" : "cursor-auto"
-            )}
-          >
-            {children}
-          </td>
-        </Link>
-      ) : (
-        <td
-          className={cn(paddingTop, paddingBottom, paddingLeft, paddingRight)}
-        >
-          {children}
-        </td>
-      )}
-    </>
+    <td className={cn(paddingTop, paddingBottom, paddingLeft, paddingRight)}>
+      {children}
+    </td>
   );
 };
 
--- src/components/ui/TableCells/NameCell/NameCell.tsx
+++ src/components/ui/TableCells/NameCell/NameCell.tsx
@@ -4,12 +4,14 @@ import StatusIndicator from "../../StatusIndicator";
 import { getHumanReadableStringFromTime } from "@/utils/timeUtils";
 import { Status } from "@/types/general";
 import CellBase, { CellBaseProps } from "../CellBase";
+import Link from "next/link";
 
 export type NameCellProps = {
   status: Status;
   width: string;
   updatedAt: string;
   name: string;
+  link?: string;
 } & CellBaseProps;
 
 const NameCell: FC<NameCellProps> = ({
@@ -21,8 +23,26 @@ const NameCell: FC<NameCellProps> = ({
   paddingLeft,
   paddingRight,
   paddingTop,
+  link,
 }) => {
   const time = getHumanReadableStringFromTime(updatedAt, Date.now());
+
+  const CellItem = () => (
+    <div className={cn("flex flex-row gap-x-2.5", width)}>
+      <div className="flex h-8 w-8">
+        <StatusIndicator
+          status={status}
+          width="w-[18px]"
+          height="h-[18px]"
+          position="m-auto"
+        />
+      </div>
+      <div className="flex flex-col gap-y-2">
+        <h3 className="instill-text-h3">{name}</h3>
+        <p className="instill-text-small text-instillGrey50">{`last sync ${time}`}</p>
+      </div>
+    </div>
+  );
 
   return (
     <CellBase
@@ -31,20 +51,15 @@ const NameCell: FC<NameCellProps> = ({
       paddingRight={paddingRight}
       paddingBottom={paddingBottom}
     >
-      <div className={cn("flex flex-row gap-x-2.5", width)}>
-        <div className="flex h-8 w-8">
-          <StatusIndicator
-            status={status}
-            width="w-[18px]"
-            height="h-[18px]"
-            position="m-auto"
-          />
-        </div>
-        <div className="flex flex-col gap-y-2">
-          <h3 className="instill-text-h3">{name}</h3>
-          <p className="instill-text-small text-instillGrey50">{`last sync ${time}`}</p>
-        </div>
-      </div>
+      {link ? (
+        <Link href={link}>
+          <a>
+            <CellItem />
+          </a>
+        </Link>
+      ) : (
+        <CellItem />
+      )}
     </CellBase>
   );
 };
