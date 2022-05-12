95baa32c0493d065935c50da4d0444e9757746d5
pochun-chiu <pochun.chiu@instill.tech>
2022-05-12 18:12:16 +0800

    feat: add SourcePipelinesTable

---
+++ src/services/connector/SourcePipelinesTable.tsx
@@ -0,0 +1,91 @@
+import {
+  ConnectionTypeCell,
+  InstanceCell,
+  NameCell,
+  SourcePipelinesTableHead,
+  TableBody,
+  TableContainer,
+  TableRow,
+} from "@/components/ui";
+import { FC, memo } from "react";
+import { Pipeline } from "../pipeline/PipelineServices";
+
+export type SourcePipelinesTableProps = {
+  pipelines: Pipeline[];
+  isLoading: boolean;
+};
+
+const SourcePipelinesTable: FC<SourcePipelinesTableProps> = ({
+  pipelines,
+  isLoading,
+}) => {
+  if (isLoading) {
+    return <div>isLoading</div>;
+  }
+
+  return (
+    <TableContainer tableLayout="table-auto" borderCollapse="border-collapse">
+      <SourcePipelinesTableHead
+        offlineCounts={1}
+        onlineCounts={1}
+        errorCounts={1}
+      />
+      <TableBody>
+        {pipelines.map((pipeline) => (
+          <TableRow
+            borderColor="border-instillGrey20"
+            bgColor="bg-white"
+            key={pipeline.id}
+          >
+            <NameCell
+              name={pipeline.id}
+              width="w-[191px]"
+              updatedAt={pipeline.update_time}
+              status={pipeline.status}
+              paddingBottom="pb-5"
+              paddingTop="pt-5"
+              paddingLeft="pl-[15px]"
+              paddingRight=""
+            />
+            <ConnectionTypeCell
+              width="w-[125px]"
+              type={pipeline.recipe.source.type}
+              name={pipeline.recipe.source.name}
+              cellType="shrink"
+              paddingBottom="pb-5"
+              paddingTop="pt-5"
+              paddingLeft=""
+              paddingRight=""
+            />
+            <InstanceCell
+              type="model"
+              width="w-[190px]"
+              instances={pipeline.recipe.models.map((model) => {
+                return {
+                  name: `${model.id}/${model.instance}`,
+                  status: model.status,
+                };
+              })}
+              paddingBottom="pb-5"
+              paddingTop="pt-5"
+              paddingLeft=""
+              paddingRight=""
+            />
+            <ConnectionTypeCell
+              width="w-[160px]"
+              cellType="shrink"
+              type={pipeline.recipe.destination.type}
+              name={pipeline.recipe.destination.name}
+              paddingBottom="pb-5"
+              paddingTop="pt-5"
+              paddingLeft=""
+              paddingRight="pr-5"
+            />
+          </TableRow>
+        ))}
+      </TableBody>
+    </TableContainer>
+  );
+};
+
+export default memo(SourcePipelinesTable);
