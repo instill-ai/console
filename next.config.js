d06d8a90bb1fcb8851dd188a3a2fad87717bcc43
pochun-chiu <pochun.chiu@instill.tech>
2022-05-17 08:39:29 +0800

    feat: add nextjs webpack config for hot reloading

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
-// @ts-check
-
 module.exports = {
   swcMinify: true,
   reactStrictMode: true,
   experimental: {
     outputStandalone: true,
   },
-}
+  webpackDevMiddleware: (config) => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    };
+    return config;
+  },
+};
