c67de642de597189de082997931aca3eadb9cb7b
pochun-chiu <pochun.chiu@instill.tech>
2022-05-18 14:45:01 +0800

    feat: add ModelTablePlaceholder

--- src/components/ui/TablePlaceholders/index.ts
+++ src/components/ui/TablePlaceholders/index.ts
@@ -1,9 +1,11 @@
 import DestinationTablePlaceholder from "./DestinationTablePlaceholder";
 import SourceTablePlaceholder from "./SourceTablePlaceholder";
 import PipelineTablePlaceholder from "./PipelineTablePlaceholder";
+import ModelTablePlaceholder from "./ModelTablePlaceholder";
 
 export {
   DestinationTablePlaceholder,
   SourceTablePlaceholder,
   PipelineTablePlaceholder,
+  ModelTablePlaceholder,
 };
