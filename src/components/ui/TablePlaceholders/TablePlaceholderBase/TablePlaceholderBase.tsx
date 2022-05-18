4bd269817f399d7c8360b23f4af7e453e684dec9
pochun-chiu <pochun.chiu@instill.tech>
2022-05-18 13:36:25 +0800

    refactor: adjust TablePlaceholderBase style

--- src/components/ui/TablePlaceholders/TablePlaceholderBase/TablePlaceholderBase.tsx
+++ src/components/ui/TablePlaceholders/TablePlaceholderBase/TablePlaceholderBase.tsx
@@ -1,15 +1,18 @@
-import { FC, ReactElement } from "react";
+import { FC, Fragment, ReactElement } from "react";
 import { useRouter } from "next/router";
 
 import { PrimaryButton } from "../../Buttons";
 
 export type TablePlaceholderBaseProps = {
-  placeholder: ReactElement;
+  placeholderItems: {
+    id: string;
+    item: ReactElement;
+  }[];
   createButtonLink: string;
 };
 
 const TablePlaceholderBase: FC<TablePlaceholderBaseProps> = ({
-  placeholder,
+  placeholderItems,
   createButtonLink,
 }) => {
   const router = useRouter();
@@ -17,9 +20,13 @@ const TablePlaceholderBase: FC<TablePlaceholderBaseProps> = ({
     router.push(createButtonLink);
   };
   return (
-    <div className="flex h-[300px] w-full flex-row border border-instillGrey15 bg-white">
-      {placeholder}
-      <div className="m-auto flex gap-y-5">
+    <div className="flex h-[300px] w-full flex-row border border-instillGrey15 bg-white px-[9px] py-[18px]">
+      <div className="grid grid-cols-3 grid-rows-2 gap-x-2 gap-y-2 px-[27px] opacity-10">
+        {placeholderItems.map((e) => (
+          <Fragment key={e.id}>{e.item}</Fragment>
+        ))}
+      </div>
+      <div className="m-auto flex flex-col gap-y-5">
         <h3 className="instill-text-h3 text-instillGrey80">No Pipeline</h3>
         <PrimaryButton
           type="button"
