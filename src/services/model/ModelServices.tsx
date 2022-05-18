60371cc645d7d06026e7239caa689ceb603c52fd
pochun-chiu <pochun.chiu@instill.tech>
2022-05-18 09:58:17 +0800

    chore: remove redundant code and add useModel placeholder

--- src/services/model/ModelServices.tsx
+++ src/services/model/ModelServices.tsx
@@ -1,26 +1,3 @@
-import { Status } from "@/types/general";
-
-export type ModelState =
-  | "STATE_ONLINE"
-  | "STATE_OFFLINE"
-  | "STATE_ERROR"
-  | "STATE_UNSPECIFIC";
-
-export type Model = {
-  id: string;
-  instance: string;
-  status: Status;
-};
-
-export const transformModelStateToStatus = (state: ModelState): Status => {
-  switch (state) {
-    case "STATE_ONLINE":
-      return "active";
-    case "STATE_OFFLINE":
-      return "inactive";
-    case "STATE_ERROR":
-      return "error";
-    default:
-      return "unspecific";
-  }
+export const useModel = () => {
+  return "placeholder";
 };
