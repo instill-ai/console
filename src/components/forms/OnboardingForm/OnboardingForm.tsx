28050fb6e6068e0ffb29cdada2ebaa7f77fa86b4
pochun-chiu <pochun.chiu@instill.tech>
2022-05-19 11:09:52 +0800

    feat: add submit function into OnboardingForm

--- src/components/forms/OnboardingForm/OnboardingForm.tsx
+++ src/components/forms/OnboardingForm/OnboardingForm.tsx
@@ -1,22 +1,44 @@
 import { FC } from "react";
 import { Form, Formik } from "formik";
+import { useRouter } from "next/router";
 
 import { PrimaryButton } from "@/components/ui/Buttons";
 import { mockMgmtRoles } from "@/services/mgmt/MgmtServices";
 import { SingleSelect, TextField, ToggleField } from "../../formik";
+import axios from "axios";
 
 const OnboardingForm: FC = () => {
+  const router = useRouter();
   return (
     <Formik
       initialValues={{
         email: null,
-        username: null,
         company_name: null,
         role: null,
         usage_data_collection: null,
         newsletter_subscription: null,
       }}
-      onSubmit={(values) => console.log(values)}
+      onSubmit={async (values) => {
+        try {
+          const res = await axios.post("/api/submit-onboarded-form", {
+            email: values.email,
+            company_name: values.company_name,
+            role: values.role,
+            usage_data_collection: values.usage_data_collection,
+            newsletter_subscription: values.newsletter_subscription,
+          });
+
+          if (res.status === 200) {
+            await axios.post("/api/set-user-onboarded-cookie");
+            router.push("/pipelines");
+          }
+        } catch (err) {
+          console.error(
+            "Something whet wrong when submit onboarding form",
+            err
+          );
+        }
+      }}
     >
       {(formik) => {
         return (
@@ -31,17 +53,6 @@ const OnboardingForm: FC = () => {
               placeholder=""
               type="email"
               autoComplete="on"
-            />
-            <TextField
-              name="username"
-              label="User name"
-              description="Pick a username"
-              disabled={false}
-              readOnly={false}
-              required={true}
-              placeholder=""
-              type="text"
-              autoComplete="off"
             />
             <TextField
               name="company_name"
