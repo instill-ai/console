5d8c3e97f41a1aefdb2fce588fcab5db37107aae
pochun-chiu <pochun.chiu@instill.tech>
2022-05-20 17:52:27 +0800

    feat: post backend when setup sync mode source

--- src/components/forms/CreatePipelineForm/SetupPipelineModeStep/SetupPipelineModeStep.tsx
+++ src/components/forms/CreatePipelineForm/SetupPipelineModeStep/SetupPipelineModeStep.tsx
@@ -1,4 +1,4 @@
-import { FC, useMemo } from "react";
+import { FC, useEffect, useMemo, useState } from "react";
 import { useFormikContext } from "formik";
 import {
   AsyncIcon,
@@ -7,9 +7,14 @@ import {
 } from "@instill-ai/design-system";
 
 import { SingleSelect, FormikStep } from "../../../formik";
-import { syncDataConnectionOptions } from "../../MockData";
 import { StepNumberState, Values } from "../CreatePipelineForm";
 import { PrimaryButton } from "@/components/ui/Buttons";
+import {
+  useCreateSource,
+  useSourceDefinitions,
+} from "@/services/connector/SourceServices";
+import ConnectorIcon from "@/components/ui/ConnectorIcon";
+import { CreateSourcePayload } from "@/lib/instill";
 
 export type SetupSourceStepProps = StepNumberState;
 
@@ -20,7 +25,7 @@ const SetupPipelineModeStep: FC<SetupSourceStepProps> = ({
   const modeOptions = [
     {
       label: "Sync",
-      value: "sync",
+      value: "MODE_SYNC",
       startIcon: (
         <SyncIcon
           color="fill-instillGrey90"
@@ -32,7 +37,7 @@ const SetupPipelineModeStep: FC<SetupSourceStepProps> = ({
     },
     {
       label: "Async",
-      value: "async",
+      value: "MODE_ASYNC",
       startIcon: (
         <AsyncIcon
           color="fill-instillGrey90"
@@ -44,15 +49,97 @@ const SetupPipelineModeStep: FC<SetupSourceStepProps> = ({
     },
   ];
 
+  const sourceDefinitions = useSourceDefinitions();
   const { values, setFieldValue } = useFormikContext<Values>();
+
+  const [syncConnectionOptions, setSyncConnectionOptions] = useState<
+    SingleSelectOption[]
+  >([]);
+
+  useEffect(() => {
+    if (!sourceDefinitions.isSuccess) return;
+
+    console.log(sourceDefinitions);
+
+    if (values.pipeline.mode === "MODE_SYNC") {
+      const syncDefinitions = sourceDefinitions.data.filter(
+        (e) =>
+          e.connector_definition.connection_type ===
+          "CONNECTION_TYPE_DIRECTNESS"
+      );
+
+      setSyncConnectionOptions(
+        syncDefinitions.map((e) => {
+          return {
+            label: e.connector_definition.title,
+            value: e.id,
+            startIcon: (
+              <ConnectorIcon
+                type={e.connector_definition.title}
+                iconColor="fill-instillGrey90"
+                iconHeight="h-[30px]"
+                iconWidth="w-[30px]"
+                iconPosition="my-auto"
+              />
+            ),
+          };
+        })
+      );
+
+      return;
+    }
+
+    const asyncDefinitions = sourceDefinitions.data.filter(
+      (e) =>
+        e.connector_definition.connection_type !== "CONNECTION_TYPE_DIRECTNESS"
+    );
+
+    setSyncConnectionOptions(
+      asyncDefinitions.map((e) => {
+        return {
+          label: e.connector_definition.title,
+          value: e.id,
+          startIcon: (
+            <ConnectorIcon
+              type={e.connector_definition.title}
+              iconColor="fill-instillGrey90"
+              iconHeight="h-[30px]"
+              iconWidth="w-[30px]"
+              iconPosition="my-auto"
+            />
+          ),
+        };
+      })
+    );
+  }, [sourceDefinitions.isSuccess, values.pipeline.mode]);
 
   const canGoNext = useMemo(() => {
     if (!values.pipeline.mode) return false;
+    if (values.pipeline.mode === "MODE_SYNC" && !values.source.existing.id) {
+      return false;
+    }
+
     return true;
-  }, [values.pipeline.mode]);
+  }, [values.pipeline.mode, values.source.existing]);
+
+  const createSource = useCreateSource();
 
   const handleGoNext = () => {
-    if (values.pipeline.mode === "sync") {
+    if (values.pipeline.mode === "MODE_SYNC") {
+      const payload: CreateSourcePayload = {
+        id: values.source.existing.id,
+        source_connector_definition: "source-connector-definitions/source-http",
+        connector: {
+          configuration: "{}",
+        },
+      };
+
+      createSource.mutate(payload, {
+        onSuccess: (newSource) => {
+          setFieldValue("source.existing.name", newSource.name);
+        },
+      });
+
       setStepNumber(stepNumber + 2);
       return;
     }
@@ -69,35 +156,38 @@ const SetupPipelineModeStep: FC<SetupSourceStepProps> = ({
 
   return (
     <FormikStep>
-      <div className="mb-5 flex flex-col gap-y-5">
+      <div className="mb-[60px] flex flex-col gap-y-5">
         <SingleSelect
           name="pipeline.mode"
           instanceId="pipeline-mode"
           label="Pipeline mode"
           description={"Setup Guide"}
-          disabled={false}
+          disabled={true}
           readOnly={false}
           required={true}
           options={modeOptions}
+          defaultValue={modeOptions[0]}
         />
+        {values.pipeline.mode === "MODE_SYNC" ? (
+          <SingleSelect
+            name="source.existing.id"
+            instanceId="data-source-type"
+            label="Source type"
+            description={"Setup Guide"}
+            disabled={false}
+            readOnly={false}
+            required={true}
+            options={syncConnectionOptions}
+            onChangeCb={sourceTypeOnChangeCb}
+          />
+        ) : null}
       </div>
-      {values.pipeline.mode === "sync" ? (
-        <SingleSelect
-          name="dataSource.existing.type"
-          instanceId="data-source-type"
-          label="Source type"
-          description={"Setup Guide"}
-          disabled={false}
-          readOnly={false}
-          required={true}
-          options={syncDataConnectionOptions}
-          onChangeCb={sourceTypeOnChangeCb}
-        />
-      ) : null}
+
       <PrimaryButton
         onClickHandler={handleGoNext}
         disabled={canGoNext ? false : true}
         type="button"
+        position="ml-auto"
       >
         Next
       </PrimaryButton>
