cf23405809f6e2d3d340db455160bba5c152edbd
pochun-chiu <pochun.chiu@instill.tech>
2022-05-21 07:28:55 +0800

    refactor: remove data structure transformation of model

--- src/lib/instill/model.ts
+++ src/lib/instill/model.ts
@@ -1,103 +1,65 @@
 import axios from "axios";
 
 export type GetModelResponse = {
-  model: {
-    name: string;
-    uid: string;
-    id: string;
-    description: string;
-    model_definition: string;
-    configuration: string;
-    visibility: string;
-    user: string;
-    org: string;
-    create_time: string;
-    update_time: string;
-  };
+  model: Model;
 };
 
 export type Model = {
-  id: string;
   name: string;
+  uid: string;
+  id: string;
   description: string;
-  modelDefinition: string;
+  model_definition: string;
   configuration: string;
   visibility: string;
   user: string;
   org: string;
-  createTime: string;
-  updateTime: string;
+  create_time: string;
+  update_time: string;
 };
 
 export const getModelQuery = async (modelId: string): Promise<Model> => {
   try {
-    const res = await axios.post<GetModelResponse>("/api/model/get-model", {
-      id: modelId,
-    });
-
-    return Promise.resolve({
-      id: res.data.model.id,
-      name: res.data.model.name,
-      description: res.data.model.description,
-      modelDefinition: res.data.model.model_definition,
-      configuration: res.data.model.configuration,
-      visibility: res.data.model.visibility,
-      user: res.data.model.user,
-      org: res.data.model.org,
-      createTime: res.data.model.create_time,
-      updateTime: res.data.model.update_time,
-    });
+    const { data } = await axios.post<GetModelResponse>(
+      "/api/model/get-model",
+      {
+        id: modelId,
+      }
+    );
+    return Promise.resolve(data.model);
   } catch (err) {
     return Promise.reject(err);
   }
 };
 
 export type GetModelInstanceResponse = {
-  instance: {
-    name: string;
-    uid: string;
-    id: string;
-    state: string;
-    task: string;
-    model_definition: string;
-    configuration: string;
-    create_time: string;
-    update_time: string;
-  };
+  instance: ModelInstance;
 };
 
 export type ModelState = "STATE_ONLINE" | "STATE_OFFLINE" | "STATE_ERROR";
 
 export type ModelInstance = {
-  id: string;
   name: string;
-  state: ModelState;
+  uid: string;
+  id: string;
+  state: string;
   task: string;
-  modelDefinition: string;
+  model_definition: string;
   configuration: string;
-  createTime: string;
-  updateTime: string;
+  create_time: string;
+  update_time: string;
 };
 
 export const getModelInstanceQuery = async (
   modelInstanceId: string
 ): Promise<ModelInstance> => {
   try {
-    const res = await axios.post<GetModelInstanceResponse>(
+    const { data } = await axios.post<GetModelInstanceResponse>(
       "/api/model/get-model-instance",
       { id: modelInstanceId }
     );
 
-    return Promise.resolve({
-      id: res.data.instance.id,
-      name: res.data.instance.name,
-      state: res.data.instance.state as ModelState,
-      task: res.data.instance.task,
-      modelDefinition: res.data.instance.model_definition,
-      configuration: res.data.instance.configuration,
-      createTime: res.data.instance.create_time,
-      updateTime: res.data.instance.update_time,
-    });
+    return Promise.resolve(data.instance);
   } catch (err) {
     return Promise.reject(err);
   }
