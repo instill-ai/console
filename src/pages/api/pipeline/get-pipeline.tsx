af2e7d5fab21c380d77c92a0fb8c8ec14a3f7b78
pochun-chiu <pochun.chiu@instill.tech>
2022-05-20 09:12:57 +0800

    feat: add get-pipeline api route

---
+++ src/pages/api/pipeline/get-pipeline.tsx
@@ -0,0 +1,25 @@
+import axios from "axios";
+import { NextApiRequest, NextApiResponse } from "next";
+import { GetPipelineResponse } from "@/lib/instill";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { method, body } = req;
+
+  if (method !== "POST") {
+    res.setHeader("Allow", ["POST"]);
+    res.status(405).end(`Method ${method} Not Allowed`);
+  }
+
+  try {
+    const { data } = await axios.get<GetPipelineResponse>(
+      `${process.env.NEXT_PUBLIC_PIPELINE_API_ENDPOINT}/${process.env.NEXT_PUBLIC_API_VERSION}/${body.id}`
+    );
+
+    return res.status(200).json(data);
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json(JSON.stringify(err));
+  }
+};
+
+export default handler;
