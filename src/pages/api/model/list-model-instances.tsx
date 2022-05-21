bbd637e806123c6bca91e967800ec109aa60a664
pochun-chiu <pochun.chiu@instill.tech>
2022-05-21 08:09:28 +0800

    feat: add list-model-instances api route

---
+++ src/pages/api/model/list-model-instances.tsx
@@ -0,0 +1,29 @@
+import axios from "axios";
+import { NextApiRequest, NextApiResponse } from "next";
+import { ListModelInstancesResponse } from "@/lib/instill";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { method, body } = req;
+
+  if (method !== "POST") {
+    res.setHeader("Allow", ["POST"]);
+    res.status(405).end(`Method ${method} Not Allowed`);
+  }
+
+  if (!body.id) {
+    return res.status(500).json("Model id not provided");
+  }
+
+  try {
+    const { data } = await axios.get<ListModelInstancesResponse>(
+      `${process.env.NEXT_PUBLIC_MODEL_API_ENDPOINT}/${process.env.NEXT_PUBLIC_API_VERSION}/${body.id}/instances`
+    );
+
+    return res.status(200).json(data);
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json(JSON.stringify(err));
+  }
+};
+
+export default handler;
