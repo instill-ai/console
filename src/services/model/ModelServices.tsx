80722d766a2b8a5a3d75fb4610cfdfb28aff0a1b
pochun-chiu <pochun.chiu@instill.tech>
2022-05-21 08:23:07 +0800

    feat: add useModel and useModelInstance

--- src/services/model/ModelServices.tsx
+++ src/services/model/ModelServices.tsx
@@ -1,3 +1,40 @@
-export const useModel = () => {
-  return "placeholder";
+// ###################################################################
+// #                                                                 #
+// # Model                                                           #
+// #                                                                 #
+// ###################################################################
+
+import { listModelInstancesQuery, listModelsQuery } from "@/lib/instill";
+import { useQuery } from "react-query";
+
+export const useModels = () => {
+  return useQuery(["models"], async () => {
+    const models = await listModelsQuery();
+    return Promise.resolve(models);
+  });
+};
+
+// ###################################################################
+// #                                                                 #
+// # Model Instance                                                  #
+// #                                                                 #
+// ###################################################################
+
+export const useModeInstances = (modelId: string[]) => {
+  return useQuery(
+    ["modelInstances"],
+    async () => {
+      const modelInstances = [];
+
+      for (const id of modelId) {
+        const instances = await listModelInstancesQuery(id);
+        modelInstances.push(...instances);
+      }
+
+      return Promise.resolve(modelInstances);
+    },
+    {
+      enabled: !!modelId,
+    }
+  );
 };
