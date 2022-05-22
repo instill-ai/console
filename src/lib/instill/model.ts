62aaed8c0938db3b49ba276f965af2c56afb01f6
pochun-chiu <pochun.chiu@instill.tech>
2022-05-22 09:27:23 +0800

    feat: add listModelDefinitions

--- src/lib/instill/model.ts
+++ src/lib/instill/model.ts
@@ -55,6 +55,36 @@ export const listModelsQuery = async (): Promise<Model[]> => {
   }
 };
 
+export type ModelDefinition = {
+  name: string;
+  uid: string;
+  id: string;
+  title: string;
+  documentation_url: string;
+  icon: string;
+  model_spec: Record<string, any>;
+  model_instance_spec: Record<string, any>;
+  create_time: string;
+  update_time: string;
+};
+
+export type ListModelDefinitionsResponse = {
+  model_definitions: ModelDefinition[];
+  next_page_token: string;
+  total_size: string;
+};
+
+export const listModelDefinitions = async (): Promise<ModelDefinition[]> => {
+  try {
+    const { data } = await axios.get<ListModelDefinitionsResponse>(
+      "/api/model/list-model-definitions"
+    );
+    return Promise.resolve(data.model_definitions);
+  } catch (err) {
+    return Promise.reject(err);
+  }
+};
+
 // ###################################################################
 // #                                                                 #
 // # Model Instance                                                  #
@@ -71,7 +101,7 @@ export type ModelInstance = {
   name: string;
   uid: string;
   id: string;
-  state: string;
+  state: ModelState;
   task: string;
   model_definition: string;
   configuration: string;
