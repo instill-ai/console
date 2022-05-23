84019bb331fb6028a76a307bafda280a636c8940
pochun-chiu <pochun.chiu@instill.tech>
2022-05-23 14:05:40 +0800

    feat: add Nullable type

--- src/types/general.ts
+++ src/types/general.ts
@@ -3,3 +3,5 @@ import { ConnectorState, ModelState, PipelineState } from "@/lib/instill";
 export type State = PipelineState | ConnectorState | ModelState;
 
 export type Mode = "sync" | "async" | "unspecific";
+
+export type Nullable<T> = T | null;
