85cd1e6e099651624607df3efedf4c0457e3e6ec
pochun-chiu <pochun.chiu@instill.tech>
2022-05-23 10:13:15 +0800

    feat: add deploy-model endpoint

---
+++ src/pages/api/model/deploy-model.tsx
@@ -0,0 +1,30 @@
+import axios from "axios";
+import { NextApiRequest, NextApiResponse } from "next";
+import { CreateModelResponse } from "@/lib/instill";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { method, body } = req;
+
+  if (method !== "POST") {
+    res.setHeader("Allow", ["POST"]);
+    res.status(405).end(`Method ${method} Not Allowed`);
+  }
+
+  if (!body) {
+    return res.status(500).json("Create model payload not provided");
+  }
+
+  try {
+    const { data } = await axios.post<CreateModelResponse>(
+      `${process.env.NEXT_PUBLIC_MODEL_API_ENDPOINT}/${process.env.NEXT_PUBLIC_API_VERSION}/${body.name}:deploy`,
+      body
+    );
+
+    return res.status(200).json(data);
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json(JSON.stringify(err));
+  }
+};
+
+export default handler;
