13038aa49fb2cc7cb39828f22e13780d7e7b559f
pochun-chiu <pochun.chiu@instill.tech>
2022-05-23 15:24:29 +0800

    feat: add full UseExistingModelFlow

--- src/components/forms/CreatePipelineForm/SetupModelStep/UseExistingModelFlow.tsx
+++ src/components/forms/CreatePipelineForm/SetupModelStep/UseExistingModelFlow.tsx
@@ -1,4 +1,12 @@
-import { FC, useEffect, useMemo, useRef, useState } from "react";
+import {
+  Dispatch,
+  FC,
+  SetStateAction,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
 import { useFormikContext } from "formik";
 import { SingleSelectOption } from "@instill-ai/design-system";
 
@@ -6,32 +14,56 @@ import { PrimaryButton } from "@/components/ui/Buttons";
 import useOnScreen from "@/hooks/useOnScreen";
 import { SingleSelect } from "../../../formik";
 import { StepNumberState, Values } from "../CreatePipelineForm";
+import { useAllModeInstances } from "@/services/model/ModelServices";
 
-export type UseExistingModelFlowProps = StepNumberState;
+export type UseExistingModelFlowProps = StepNumberState & {
+  setModelCreated: Dispatch<SetStateAction<boolean>>;
+  modelCreated: boolean;
+};
 
-const UseExistingModelFlow: FC<UseExistingModelFlowProps> = () => {
-  const [modelOptions, setModelOptions] = useState<SingleSelectOption[] | null>(
-    null
-  );
+const UseExistingModelFlow: FC<UseExistingModelFlowProps> = ({
+  setModelCreated,
+  modelCreated,
+}) => {
+  const [modelInstanceOptions, setModelInstanceOptions] = useState<
+    SingleSelectOption[] | null
+  >(null);
 
   const { values } = useFormikContext<Values>();
 
   const flowRef = useRef<HTMLDivElement>(null);
   const flowIsOnScreen = useOnScreen(flowRef);
+  const modelInstances = useAllModeInstances();
 
   useEffect(() => {
-    if (!flowIsOnScreen) return;
+    if (!flowIsOnScreen || !modelInstances.isSuccess) return;
 
-    setTimeout(() => setModelOptions([]), 3000);
-  }, [flowIsOnScreen]);
+    console.log(modelInstances);
+
+    const onlineModelInstances = modelInstances.data.filter(
+      (e) => e.state === "STATE_ONLINE"
+    );
+
+    setModelInstanceOptions(
+      onlineModelInstances.map((e) => {
+        const instanceNameList = e.name.split("/");
+        const modelId = instanceNameList[2];
+
+        return {
+          label: `${modelId}/${e.id}`,
+          value: e.id,
+        };
+      })
+    );
+  }, [flowIsOnScreen, modelInstances.isSuccess]);
 
   const canUseExistingModel = useMemo(() => {
-    if (!values.model.existing.name) {
+    if (!values.model.existing.id) {
       return false;
     }
 
     return true;
-  }, [values.model.existing.name]);
+  }, [values.model.existing.id]);
 
   return (
     <div ref={flowRef} className="flex flex-1 flex-col gap-y-5 p-5">
@@ -41,17 +73,19 @@ const UseExistingModelFlow: FC<UseExistingModelFlowProps> = () => {
       <SingleSelect
         name="model.existing.name"
         instanceId="existing-model-name"
-        disabled={false}
+        disabled={modelCreated ? true : false}
         readOnly={false}
-        options={modelOptions ? modelOptions : []}
+        options={modelInstanceOptions ? modelInstanceOptions : []}
         required={true}
         description={"Setup Guide"}
         label="Source type"
+        menuPlacement="auto"
+        defaultValue={null}
       />
       <PrimaryButton
         position="ml-auto"
         type="button"
-        disabled={canUseExistingModel ? false : true}
+        disabled={modelCreated ? true : canUseExistingModel ? false : true}
       >
         Use model
       </PrimaryButton>
