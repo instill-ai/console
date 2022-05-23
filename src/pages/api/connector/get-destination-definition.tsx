af65a0e4900a2c2bca0ff1c1d9603a263891e8e3
pochun-chiu <pochun.chiu@instill.tech>
2022-05-23 17:37:14 +0800

    feat: add get-destination-definition api route

---
+++ src/pages/api/connector/get-destination-definition.tsx
@@ -0,0 +1,29 @@
+import { GetDestinationDefinitionResponse } from "@/lib/instill";
+import axios from "axios";
+import { NextApiRequest, NextApiResponse } from "next";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { method, body } = req;
+
+  if (method !== "POST") {
+    res.setHeader("Allow", ["POST"]);
+    res.status(405).end(`Method ${method} Not Allowed`);
+  }
+
+  if (!body.id) {
+    return res.status(500).json("Destination id not provided");
+  }
+
+  try {
+    const { data } = await axios.get<GetDestinationDefinitionResponse>(
+      `${process.env.NEXT_PUBLIC_CONNECTOR_API_ENDPOINT}/${process.env.NEXT_PUBLIC_API_VERSION}/${body.id}`
+    );
+
+    return res.status(200).json(data);
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json(JSON.stringify(err));
+  }
+};
+
+export default handler;
