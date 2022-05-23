7879d340608094ccaf58d924c0b412a830e1d551
pochun-chiu <pochun.chiu@instill.tech>
2022-05-23 16:20:28 +0800

    feat: add create-destination api route

---
+++ src/pages/api/connector/create-destination.tsx
@@ -0,0 +1,30 @@
+import { CreateDestinationResponse } from "@/lib/instill";
+import axios from "axios";
+import { NextApiRequest, NextApiResponse } from "next";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { method, body } = req;
+
+  if (method !== "POST") {
+    res.setHeader("Allow", ["POST"]);
+    res.status(405).end(`Method ${method} Not Allowed`);
+  }
+
+  if (!body) {
+    return res.status(500).json("Body not provided");
+  }
+
+  try {
+    const { data } = await axios.post<CreateDestinationResponse>(
+      `${process.env.NEXT_PUBLIC_CONNECTOR_API_ENDPOINT}/${process.env.NEXT_PUBLIC_API_VERSION}/destination-connectors`,
+      body
+    );
+
+    return res.status(200).json(data);
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json(JSON.stringify(err));
+  }
+};
+
+export default handler;
