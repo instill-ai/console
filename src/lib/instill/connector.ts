20c386f96e001a9cfb980e9cbdf14e1922f6d352
pochun-chiu <pochun.chiu@instill.tech>
2022-05-25 16:10:25 +0800

    fix: rollback connector with definition

--- src/lib/instill/connector.ts
+++ src/lib/instill/connector.ts
@@ -106,11 +106,19 @@ export type Source = {
   name: string;
   uid: string;
   id: string;
+  source_connector_definition: string;
+  connector: Connector;
+};
+
+export type SourceWithDefinition = {
+  name: string;
+  uid: string;
+  id: string;
   source_connector_definition: ConnectorDefinition;
   connector: Connector;
 };
 
-export type SourceWithPipeline = Source & {
+export type SourceWithPipelines = Source & {
   pipelines: Pipeline[];
 };
 
@@ -232,6 +240,14 @@ export const getDestinationDefinitionQuery = async (
 // ###################################################################
 
 export type Destination = {
+  name: string;
+  uid: string;
+  id: string;
+  destination_connector_definition: string;
+  connector: Connector;
+};
+
+export type DestinationWithDefinition = {
   name: string;
   uid: string;
   id: string;
