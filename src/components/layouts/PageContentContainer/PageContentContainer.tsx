e79e9c769b74d244a940216b671be45329e7a979
pochun-chiu <pochun.chiu@instill.tech>
2022-05-26 11:02:04 +0800

    feat: adjust general layout

--- src/components/layouts/PageContentContainer/PageContentContainer.tsx
+++ src/components/layouts/PageContentContainer/PageContentContainer.tsx
@@ -2,7 +2,7 @@ import { FC } from "react";
 
 const PageContentContainer: FC = ({ children }) => {
   return (
-    <div className="flex min-h-full flex-col px-[138px] pt-10 pb-[100px]">
+    <div className="flex min-h-full min-w-[927px] flex-col px-[138px] pt-10 pb-[100px]">
       {children}
     </div>
   );
