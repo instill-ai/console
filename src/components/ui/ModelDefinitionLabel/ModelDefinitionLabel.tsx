ad3463e0784b552162c09a834254d2fe293326ac
pochun-chiu <pochun.chiu@instill.tech>
2022-05-27 19:44:07 +0800

    feat: add marginBottom into ModelDefinitionLabel and make modeDefinition nullable

--- src/components/ui/ModelDefinitionLabel/ModelDefinitionLabel.tsx
+++ src/components/ui/ModelDefinitionLabel/ModelDefinitionLabel.tsx
@@ -1,10 +1,17 @@
-import { GitHubIcon, LocalUploadIcon } from "@instill-ai/design-system";
 import { FC } from "react";
+import cn from "clsx";
 
-export type ModelDefinitionLabelProps = { modelDefinition: string };
+import { Nullable } from "@/types/general";
+import { GitHubIcon, LocalUploadIcon } from "@instill-ai/design-system";
+
+export type ModelDefinitionLabelProps = {
+  modelDefinition: Nullable<string>;
+  marginBottom: Nullable<string>;
+};
 
 const ModelDefinitionLabel: FC<ModelDefinitionLabelProps> = ({
   modelDefinition,
+  marginBottom,
 }) => {
   const icon = {
     width: "w-[18px]",
@@ -31,11 +38,17 @@ const ModelDefinitionLabel: FC<ModelDefinitionLabelProps> = ({
     );
 
   return (
-    <div className="flex gap-x-2 px-2 py-[7px]">
-      {modelDefinitionIcon}
-      <p className="instill-text-small flex text-instillGrey90">
-        {modelDefinition === "model-definitions/github" ? "GitHub" : "Local"}
-      </p>
+    <div className={cn("flex gap-x-2 px-2 py-[7px]", marginBottom)}>
+      {modelDefinition ? (
+        <>
+          {modelDefinitionIcon}
+          <p className="instill-text-small flex text-instillGrey90">
+            {modelDefinition === "model-definitions/github"
+              ? "GitHub"
+              : "Local"}
+          </p>
+        </>
+      ) : null}
     </div>
   );
 };
