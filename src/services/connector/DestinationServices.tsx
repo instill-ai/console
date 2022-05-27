758e71042ec7fbef559d2c74c51a0e79185439df
pochun-chiu <pochun.chiu@instill.tech>
2022-05-27 09:15:42 +0800

    feat: add useDestinationWithPipelines

--- src/services/connector/DestinationServices.tsx
+++ src/services/connector/DestinationServices.tsx
@@ -1,11 +1,16 @@
 import {
   createDestinationMutation,
   CreateDestinationPayload,
+  DestinationWithDefinition,
+  DestinationWithPipelines,
+  getDestinationDefinitionQuery,
   getDestinationQuery,
   listDestinationDefinitionsQuery,
   listDestinationsQuery,
 } from "@/lib/instill";
+import { Nullable } from "@/types/general";
 import { useMutation, useQuery } from "react-query";
+import { usePipelines } from "../pipeline/PipelineServices";
 
 // ###################################################################
 // #                                                                 #
@@ -27,12 +32,66 @@ export const useDestinations = () => {
   });
 };
 
-export const useDestination = (destinationName: string) => {
-  const destinationId = destinationName.split("/")[1];
-  return useQuery(["destinations", destinationId], async () => {
+export const useDestination = (destinationName: Nullable<string>) => {
+  return useQuery(["destinations", destinationName], async () => {
+    if (!destinationName) {
+      return Promise.reject(new Error("invalid destination name"));
+    }
+
     const destination = await getDestinationQuery(destinationName);
-    return Promise.resolve(destination);
+    const destinationDefinition = await getDestinationDefinitionQuery(
+      destination.destination_connector_definition
+    );
+    const destinationWithDefinition: DestinationWithDefinition = {
+      ...destination,
+      destination_connector_definition: destinationDefinition,
+    };
+
+    return Promise.resolve(destinationWithDefinition);
   });
+};
+
+export const useDestinationWithPipelines = (
+  destinationName: Nullable<string>
+) => {
+  const pipelines = usePipelines(true);
+  const destination = useDestination(destinationName);
+  return useQuery(
+    ["destinations", destinationName, "with-pipelines"],
+    async () => {
+      if (!destinationName) {
+        return Promise.reject(new Error("invalid destination name"));
+      }
+
+      if (!destination.data) {
+        return Promise.reject(new Error("invalid destination data"));
+      }
+
+      if (!pipelines.data) {
+        return Promise.reject(new Error("invalid pipeline data"));
+      }
+
+      const targetPipelines = pipelines.data.filter(
+        (e) => e.recipe.destination.name === destinationName
+      );
+
+      const destinationWithPipelines: DestinationWithPipelines = {
+        ...destination.data,
+        pipelines: targetPipelines,
+      };
+
+      return Promise.resolve(destinationWithPipelines);
+    },
+    {
+      enabled: destinationName
+        ? destination.isSuccess
+          ? pipelines.isSuccess
+            ? true
+            : false
+          : false
+        : false,
+    }
+  );
 };
 
 // ###################################################################
