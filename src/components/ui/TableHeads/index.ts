864c5547a72b609f6452420dd763d9f00363feca
pochun-chiu <pochun.chiu@instill.tech>
2022-05-28 12:49:03 +0800

    feat: add ModelTableHead

--- src/components/ui/TableHeads/index.ts
+++ src/components/ui/TableHeads/index.ts
@@ -5,15 +5,19 @@ import type { ConnectorTableHeadProps } from "./ConnectorTableHead";
 import PipelineOverviewTableHead from "./PipelineOverviewTableHead";
 import type { PipelineOverviewTableHeadProps } from "./PipelineOverviewTableHead";
 import SourcePipelinesTableHead from "./SourcePipelinesTableHead";
+import ModelTableHead from "./ModelTableHead";
+import type { ModelTableHeadProps } from "./ModelTableHead";
 
 export {
   PipelinesTableHead,
   ConnectorTableHead,
   PipelineOverviewTableHead,
   SourcePipelinesTableHead,
+  ModelTableHead,
 };
 export type {
   PipelinesTableHeadProps,
   ConnectorTableHeadProps,
   PipelineOverviewTableHeadProps,
+  ModelTableHeadProps,
 };
