97287121dfdcf6e1eda135933b78526ae29330ad
Po-Chun, Chiu <pochun.chiu@instill.tech>
2022-05-31 20:13:24 +0800

    feat: complete CreateDestinationForm

--- src/components/forms/CreateDestinationForm/CreateDestinationForm.tsx
+++ src/components/forms/CreateDestinationForm/CreateDestinationForm.tsx
@@ -1,50 +1,155 @@
-import { FC } from "react";
-import { Form, Formik } from "formik";
-import { SingleSelect, TextField } from "../../formik/FormikField";
+import { FC, useEffect, useCallback, useState } from "react";
+import { Formik } from "formik";
+import { useRouter } from "next/router";
 
+import { SingleSelect } from "../../formik/FormikField";
 import { PrimaryButton } from "@/components/ui/Buttons";
-import { mockAsyncDataConnectionOptions } from "../MockData";
 import { FormBase } from "@/components/formik";
+import { ConnectorIcon } from "@/components/ui";
+import { SingleSelectOption } from "@instill-ai/design-system";
+import { CreateDestinationPayload, CreateSourcePayload } from "@/lib/instill";
+import { Nullable } from "@/types/general";
+import {
+  useCreateDestination,
+  useDestinations,
+} from "@/services/connector/DestinationServices";
+
+export type CreateDestinationFormValues = {
+  id: string;
+  definition: string;
+};
 
 const CreateDestinationForm: FC = () => {
+  const router = useRouter();
+
+  // ###################################################################
+  // #                                                                 #
+  // # 1 - Initialize the source definition                            #
+  // #                                                                 #
+  // ###################################################################
+  //
+  // A user can only have a http source and a grpc source
+
+  const [
+    syncDestinationDefinitionOptions,
+    setSyncDestinationDefinitionOptions,
+  ] = useState<SingleSelectOption[]>([]);
+  const [
+    selectedSyncDestinationDefinitionOption,
+    setSelectedSyncDestinationDefinitionOption,
+  ] = useState<Nullable<SingleSelectOption>>(null);
+
+  const destinations = useDestinations();
+  const createDestination = useCreateDestination();
+
+  useEffect(() => {
+    setSyncDestinationDefinitionOptions([
+      {
+        label: "gRPC",
+        value: "destination-grpc",
+        startIcon: (
+          <ConnectorIcon
+            iconName="grpc.svg"
+            iconColor="fill-instillGrey90"
+            iconHeight="h-[30px]"
+            iconWidth="w-[30px]"
+            iconPosition="my-auto"
+          />
+        ),
+      },
+      {
+        label: "HTTP",
+        value: "destination-http",
+        startIcon: (
+          <ConnectorIcon
+            iconName="http.svg"
+            iconColor="fill-instillGrey90"
+            iconHeight="h-[30px]"
+            iconWidth="w-[30px]"
+            iconPosition="my-auto"
+          />
+        ),
+      },
+    ]);
+  }, []);
+
+  const destinationDefinitionOnChange = useCallback(
+    (option: SingleSelectOption) => {
+      setSelectedSyncDestinationDefinitionOption(option);
+    },
+    []
+  );
+
   return (
     <Formik
-      initialValues={{ name: null, type: null }}
-      onSubmit={(values) => console.log(values)}
+      initialValues={{ id: null, definition: null }}
+      validate={(values) => {
+        const error: Partial<CreateDestinationFormValues> = {};
+
+        if (!values.definition) {
+          error.definition = "Required";
+        }
+
+        if (destinations.data?.find((e) => e.id === values.definition)) {
+          error.definition =
+            "You could only create one http and one grpc source. Check the setup guide for more information.";
+        }
+
+        return error;
+      }}
+      onSubmit={(values) => {
+        if (!values.definition || !destinations.isSuccess) return;
+
+        const payload: CreateDestinationPayload = {
+          id: values.definition,
+          destination_connector_definition: `destination-connector-definitions/${values.definition}`,
+          connector: {
+            configuration: "{}",
+          },
+        };
+
+        createDestination.mutate(payload, {
+          onSuccess: () => {
+            router.push("/destinations");
+          },
+        });
+      }}
     >
       {(formik) => {
         return (
           <FormBase marginBottom={null} gapY="gap-y-5" padding={null}>
-            <TextField
-              name="name"
+            {/* <TextField
+              name="id"
               label="Name"
-              description="Pick a name to help you identify this destination in Instill"
-              disabled={false}
+              description="Pick a name to help you identify this source in Instill"
+              disabled={allSyncSourceCreated ? true : false}
               readOnly={false}
               required={true}
               placeholder=""
               type="text"
               autoComplete="off"
-              error={null}
-              value={formik.values.name}
-            />
+              value={formik.values.id || ""}
+            /> */}
             <SingleSelect
-              name="type"
-              label="Destination type"
-              instanceId="destination-type"
+              name="definition"
+              label="Source type"
+              instanceId="source-type"
+              options={syncDestinationDefinitionOptions}
+              value={selectedSyncDestinationDefinitionOption}
+              onChangeCb={destinationDefinitionOnChange}
+              error={formik.errors.definition || null}
               disabled={false}
               readOnly={false}
-              options={mockAsyncDataConnectionOptions}
               required={true}
-              description={"Setup Guide"}
+              description="Setup Guide"
               menuPlacement="auto"
-              error={null}
             />
             <PrimaryButton
               type="submit"
               disabled={formik.isValid ? false : true}
+              position="ml-auto"
             >
-              Set up Destination
+              Set up Source
             </PrimaryButton>
           </FormBase>
         );
