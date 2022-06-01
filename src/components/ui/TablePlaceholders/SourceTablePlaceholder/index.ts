f06f68995cf25077872bb59060110e2c6a9d5d84
Po-Chun, Chiu <pochun.chiu@instill.tech>
2022-06-01 21:47:34 +0800

    feat: export TablePlaceholder props

--- src/components/ui/TablePlaceholders/SourceTablePlaceholder/index.ts
+++ src/components/ui/TablePlaceholders/SourceTablePlaceholder/index.ts
@@ -1,1 +1,2 @@
 export { default } from "./SourceTablePlaceholder";
+export * from "./SourceTablePlaceholder";
--- src/components/ui/TablePlaceholders/index.ts
+++ src/components/ui/TablePlaceholders/index.ts
@@ -1,8 +1,13 @@
 import DestinationTablePlaceholder from "./DestinationTablePlaceholder";
+import type { DestinationTablePlaceholderProps } from "./DestinationTablePlaceholder";
 import SourceTablePlaceholder from "./SourceTablePlaceholder";
+import type { SourceTablePlaceholderProps } from "./SourceTablePlaceholder";
 import PipelineTablePlaceholder from "./PipelineTablePlaceholder";
+import type { PipelineTablePlaceholderProps } from "./PipelineTablePlaceholder";
 import ModelTablePlaceholder from "./ModelTablePlaceholder";
+import type { ModelTablePlaceholderProps } from "./ModelTablePlaceholder";
 import TableLoadingPlaceholder from "./TableLoadingPlaceholder";
+import type { TableLoadingPlaceholderProps } from "./TableLoadingPlaceholder";
 
 export {
   DestinationTablePlaceholder,
@@ -10,4 +15,12 @@ export {
   PipelineTablePlaceholder,
   ModelTablePlaceholder,
   TableLoadingPlaceholder,
+};
+
+export type {
+  DestinationTablePlaceholderProps,
+  SourceTablePlaceholderProps,
+  PipelineTablePlaceholderProps,
+  ModelTablePlaceholderProps,
+  TableLoadingPlaceholderProps,
 };
