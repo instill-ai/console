b7508c85c48940e0a25f1c96da13d1c6f13990b2
Po-Chun, Chiu <pochun.chiu@instill.tech>
2022-06-01 21:40:15 +0800

    feat: add marginBottom into TableLoadingPlaceholder

--- src/components/ui/TablePlaceholders/TableLoadingPlaceholder/TableLoadingPlaceholder.tsx
+++ src/components/ui/TablePlaceholders/TableLoadingPlaceholder/TableLoadingPlaceholder.tsx
@@ -1,9 +1,18 @@
-import { NoBgSquareProgress } from "@instill-ai/design-system";
 import { FC } from "react";
+import cn from "clsx";
+import { NoBgSquareProgress } from "@instill-ai/design-system";
 
-const TableLoadingPlaceholder: FC = () => {
+import { Nullable } from "@/types/general";
+
+export type TableLoadingPlaceholderProps = {
+  marginBottom: Nullable<string>;
+};
+
+const TableLoadingPlaceholder: FC<TableLoadingPlaceholderProps> = ({
+  marginBottom,
+}) => {
   return (
-    <div className="flex h-[300px] w-full bg-white">
+    <div className={cn("flex h-[300px] w-full bg-white", marginBottom)}>
       <div className="m-auto flex flex-col gap-y-2.5">
         <div className="m-auto flex h-[72px] w-[72px] bg-instillBlue10">
           <NoBgSquareProgress
