ba210144cec88f518283d1248586fcdaad984525
Po-Chun, Chiu <pochun.chiu@instill.tech>
2022-06-01 16:14:49 +0800

    fix: fix model page loading state issue

--- src/pages/models/index.tsx
+++ src/pages/models/index.tsx
@@ -4,6 +4,7 @@ import { PageBase, PageContentContainer } from "@/components/layouts";
 import PageTitle from "@/components/ui/PageTitle";
 import ModelTable from "@/services/model/ModelTable";
 import { useModelsWithInstances } from "@/services/model/ModelServices";
+import { useMultiStageQueryLoadingState } from "@/services/useMultiStageQueryLoadingState";
 
 interface GetLayOutProps {
   page: ReactElement;
@@ -13,6 +14,14 @@ const ModelPage: FC & {
   getLayout?: FC<GetLayOutProps>;
 } = () => {
   const modelsWithInstances = useModelsWithInstances();
+
+  const isLoading = useMultiStageQueryLoadingState({
+    data: modelsWithInstances.data,
+    isError: modelsWithInstances.isError,
+    isSuccess: modelsWithInstances.isSuccess,
+    isLoading: modelsWithInstances.isLoading,
+  });
+
   return (
     <PageContentContainer>
       <PageTitle
@@ -31,7 +40,7 @@ const ModelPage: FC & {
       />
       <ModelTable
         models={modelsWithInstances.isSuccess ? modelsWithInstances.data : []}
-        isLoading={modelsWithInstances.isLoading}
+        isLoading={isLoading}
       />
     </PageContentContainer>
   );
