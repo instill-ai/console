565384677b88d3e670d98830288972a99bb1effb
Po-Chun, Chiu <pochun.chiu@instill.tech>
2022-06-01 10:58:27 +0800

    feat: complete UseExistingSourceFlow

--- src/components/forms/CreatePipelineForm/SetupSourceStep/UseExistingSourceFlow.tsx
+++ src/components/forms/CreatePipelineForm/SetupSourceStep/UseExistingSourceFlow.tsx
@@ -1,60 +1,113 @@
-import { FC, useState, useRef, useEffect, useMemo } from "react";
+import { FC, useState, useEffect, useMemo } from "react";
 import { useFormikContext } from "formik";
 import { SingleSelectOption } from "@instill-ai/design-system";
 
 import { PrimaryButton } from "@/components/ui/Buttons";
-import useOnScreen from "@/hooks/useOnScreen";
 import { SingleSelect } from "../../../formik";
-import { StepNumberState, Values } from "../CreatePipelineForm";
+import {
+  StepNumberState,
+  CreatePipelineFormValues,
+} from "../CreatePipelineForm";
+import { useSources } from "@/services/connector/SourceServices";
+import { ConnectorIcon } from "@/components/ui";
+import { Nullable } from "@/types/general";
 
 export type UseExistingSourceFlowProps = StepNumberState;
 
 const UseExistingSourceFlow: FC<UseExistingSourceFlowProps> = ({
-  maximumStepNumber,
   setStepNumber,
   stepNumber,
 }) => {
-  const [sourceOptions, setSourceOptions] = useState<
-    SingleSelectOption[] | null
-  >(null);
+  const { values, setFieldValue, errors } =
+    useFormikContext<CreatePipelineFormValues>();
 
-  const { values } = useFormikContext<Values>();
+  // ###################################################################
+  // #                                                                 #
+  // # 1 - Initialize the source definition and sources                #
+  // #                                                                 #
+  // ###################################################################
 
-  const flowRef = useRef<HTMLDivElement>(null);
-  const flowIsOnScreen = useOnScreen(flowRef);
+  const [sourceOptions, setSourceOptions] =
+    useState<Nullable<SingleSelectOption[]>>(null);
+
+  const sources = useSources();
 
   useEffect(() => {
-    if (!flowIsOnScreen) return;
+    if (!sources.isSuccess) return;
 
-    setTimeout(() => setSourceOptions([]), 3000);
-  }, [flowIsOnScreen]);
+    setSourceOptions(
+      sources.data.map((e) => {
+        return {
+          label: e.id,
+          value: e.id,
+          startIcon: (
+            <ConnectorIcon
+              iconName={e.source_connector_definition.connector_definition.icon}
+              iconColor="fill-instillGrey90"
+              iconHeight="h-[30px]"
+              iconWidth="w-[30px]"
+              iconPosition="my-auto"
+            />
+          ),
+        };
+      })
+    );
+  }, [sources.isSuccess]);
+
+  const selectedSourceOption = useMemo(() => {
+    if (!values.source.existing.id || !sourceOptions) return null;
+
+    return (
+      sourceOptions.find((e) => e.value === values.source.existing.id) || null
+    );
+  }, [values.source.existing.id]);
+
+  // ###################################################################
+  // #                                                                 #
+  // # 2 - Use existing source                                         #
+  // #                                                                 #
+  // ###################################################################
 
   const canUseExistingSource = useMemo(() => {
-    if (!values.dataSource.existing.name) {
+    if (!values.source.existing.id) {
       return false;
     }
 
     return true;
-  }, [values.dataSource.existing.name]);
+  }, [values.source.existing.id]);
 
   const handleUseExistingSource = () => {
+    const source = sources.data?.find(
+      (e) => e.id === values.source.existing.id
+    );
+
+    if (!source) return;
+
+    setFieldValue(
+      "source.existing.definition",
+      source.source_connector_definition.connector_definition.title
+    );
     setStepNumber(stepNumber + 1);
   };
 
   return (
-    <div ref={flowRef} className="flex flex-1 flex-col gap-y-5 p-5">
+    <div className="flex flex-1 flex-col gap-y-5 p-5">
       <h3 className="instill-text-h3 text-black">
         Select a existing online source
       </h3>
       <SingleSelect
-        name="dataSource.existing.name"
+        name="source.existing.name"
+        label="Source type"
         instanceId="existing-data-source-name"
+        description={"Setup Guide"}
+        options={sourceOptions ? sourceOptions : []}
+        value={selectedSourceOption}
+        error={errors.source?.existing?.id || null}
+        additionalOnChangeCb={null}
+        menuPlacement="auto"
         disabled={false}
         readOnly={false}
-        options={sourceOptions ? sourceOptions : []}
         required={true}
-        description={"Setup Guide"}
-        label="Source type"
       />
       <PrimaryButton
         position="ml-auto"
