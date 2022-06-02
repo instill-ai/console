be39eee9117496fa6a46d096586daf58223c9d31
Po-Chun, Chiu <pochun.chiu@instill.tech>
2022-06-02 16:49:37 +0800

    refactor: refactor StateOverview prop type

--- src/components/ui/StateOverview/StateOverview.tsx
+++ src/components/ui/StateOverview/StateOverview.tsx
@@ -1,10 +1,11 @@
+import { Nullable } from "@/types/general";
 import { FC, memo, ReactNode } from "react";
 import StateIndicator from "../StateIndicator";
 
 export type StateOverviewProps = {
-  errorCounts: number;
-  onlineCounts: number;
-  offlineCounts: number;
+  errorCounts: Nullable<number>;
+  onlineCounts: Nullable<number>;
+  offlineCounts: Nullable<number>;
 };
 
 const StateOverview: FC<StateOverviewProps> = ({
@@ -12,7 +13,7 @@ const StateOverview: FC<StateOverviewProps> = ({
   onlineCounts,
   offlineCounts,
 }) => {
-  const getStateItem = (icon: ReactNode, counts: number) => {
+  const getStateItem = (icon: ReactNode, counts: Nullable<number>) => {
     return (
       <div className="flex flex-row gap-x-[5px] py-[3px] pl-[3px] pr-[10px]">
         {icon}
