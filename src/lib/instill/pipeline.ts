643efa9437d0cbcfd40141e8007dce87f764a971
Po-Chun, Chiu <pochun.chiu@instill.tech>
2022-06-03 10:45:50 +0800

    feat: add active and deactive pipeline method

--- src/lib/instill/pipeline.ts
+++ src/lib/instill/pipeline.ts
@@ -299,3 +299,33 @@ export const updatePipelineMutation = async (
     return Promise.reject(err);
   }
 };
+
+export type ActivatePipelineResponse = {
+  pipeline: PipelineWithRawRecipe;
+};
+
+export const activatePipelineMutation = async (pipelineName: string) => {
+  try {
+    const { data } = await axios.post<ActivatePipelineResponse>(
+      `${process.env.NEXT_PUBLIC_PIPELINE_API_ENDPOINT}/${process.env.NEXT_PUBLIC_API_VERSION}/${pipelineName}:activate`
+    );
+    return Promise.resolve(data.pipeline);
+  } catch (err) {
+    return Promise.reject(err);
+  }
+};
+
+export type DeActivatePipelineResponse = {
+  pipeline: PipelineWithRawRecipe;
+};
+
+export const deActivatePipelineMutation = async (pipelineName: string) => {
+  try {
+    const { data } = await axios.post<DeActivatePipelineResponse>(
+      `${process.env.NEXT_PUBLIC_PIPELINE_API_ENDPOINT}/${process.env.NEXT_PUBLIC_API_VERSION}/${pipelineName}:deactivate`
+    );
+    return Promise.resolve(data.pipeline);
+  } catch (err) {
+    return Promise.reject(err);
+  }
+};
