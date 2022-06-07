90d5eeeebc22691f729d57484222ded9486c5bb8
Po-Chun, Chiu <pochun.chiu@instill.tech>
2022-06-07 14:47:25 +0800

    refactor: move User type into dedicated file

---
+++ src/lib/instill/mgmt/types.ts
@@ -0,0 +1,14 @@
+export type User = {
+  name: string;
+  uid: string;
+  email: string;
+  id: string;
+  company_name: string;
+  role: string;
+  usage_data_collection: boolean;
+  newsletter_subscription: boolean;
+  type: string;
+  create_time: string;
+  update_time: string;
+  cookie_token?: string;
+};
