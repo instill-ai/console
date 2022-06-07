30b1fb763dcc14061fa06cb617f5d1f524a97232
Po-Chun, Chiu <pochun.chiu@instill.tech>
2022-06-07 14:51:37 +0800

    refactor: move User query and mutation into dedicated file

--- src/lib/instill/mgmt/index.ts
+++ src/lib/instill/mgmt/index.ts
@@ -1,1 +1,7 @@
 import type { User } from "./types";
+
+import type { UpdateUserResponse } from "./mutations";
+import { updateLocalUserMutation } from "./mutations";
+
+import type { GetUserResponse } from "./queries";
+import { getUserQuery } from "./queries";
---
+++ src/lib/instill/mgmt/queries.ts
@@ -0,0 +1,18 @@
+import axios from "axios";
+import { User } from "./types";
+
+export type GetUserResponse = {
+  user: User;
+};
+
+export const getUserQuery = async (userName: string): Promise<User> => {
+  try {
+    const { data } = await axios.get<GetUserResponse>(
+      `${process.env.NEXT_PUBLIC_MGMT_API_ENDPOINT}/${process.env.NEXT_PUBLIC_API_VERSION}/${userName}`
+    );
+
+    return Promise.resolve(data.user);
+  } catch (err) {
+    return Promise.reject(err);
+  }
+};
