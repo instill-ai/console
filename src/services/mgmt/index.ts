31f5d3abe72a736b2b89af1f0ad56b516facd045
Po-Chun, Chiu <pochun.chiu@instill.tech>
2022-06-07 13:38:15 +0800

    feat: add useTrackingToken hook

---
+++ src/services/mgmt/index.ts
@@ -0,0 +1,3 @@
+import useTrackingToken from "./useTrackingToken";
+
+export { useTrackingToken };
---
+++ src/services/mgmt/useTrackingToken.ts
@@ -0,0 +1,38 @@
+import { User } from "@/lib/instill/mgmt";
+import { useQuery, useQueryClient } from "react-query";
+import { v4 as uuidv4 } from "uuid";
+import { useUpdateUser } from "./MgmtServices";
+
+const useTrackingToken = () => {
+  const queryClient = useQueryClient();
+  const updateUser = useUpdateUser();
+
+  return useQuery(
+    ["user", "tracking"],
+    async () => {
+      const user = queryClient.getQueryData<User>(["user"]);
+
+      if (user) {
+        if (user.cookie_token) {
+          return Promise.resolve(user.cookie_token);
+        }
+      }
+
+      const newTrackingToken = uuidv4();
+
+      const newUser = await updateUser.mutateAsync({
+        name: "users/local-user",
+        cookie_token: newTrackingToken,
+      });
+
+      return Promise.resolve(newUser.cookie_token);
+    },
+    {
+      onSuccess: (newUserTrackingToken) => {
+        queryClient.setQueryData(["user", "tracking"], newUserTrackingToken);
+      },
+    }
+  );
+};
+
+export default useTrackingToken;
