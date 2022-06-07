6abc1b93692a25a29f7bf48957ad89ae8470574e
Po-Chun, Chiu <pochun.chiu@instill.tech>
2022-06-07 15:32:52 +0800

    refactor: move pipeline mutations into dedicated file

--- src/lib/instill/pipeline/index.ts
+++ src/lib/instill/pipeline/index.ts
@@ -13,6 +13,17 @@ import type { ListPipelinesResponse, GetPipelineResponse } from "./queries";
 
 import { listPipelinesQuery, getPipelineQuery } from "./queries";
 
+import type {
+  CreatePipelinePayload,
+  CreatePipelineResponse,
+  UpdatePipelinePayload,
+  UpdatePipelineResponse,
+
+  createPipelineMutation,
+  updatePipelineMutation,
+} from "./mutations";
+
+
 export type {
   PipelineMode,
   PipelineState,
@@ -22,6 +33,16 @@ export type {
   PipelineRecipe,
   ListPipelinesResponse,
   GetPipelineResponse,
+  CreatePipelinePayload,
+  CreatePipelineResponse,
+  UpdatePipelinePayload,
+  UpdatePipelineResponse,
 };
 
-export { mockPipelines, listPipelinesQuery, getPipelineQuery };
+export {
+  mockPipelines,
+  listPipelinesQuery,
+  getPipelineQuery,
+  createPipelineMutation,
+  updatePipelineMutation,
+};
