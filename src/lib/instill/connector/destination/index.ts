b3e506a0810140b85500161e990bdf2732400859
Po-Chun, Chiu <pochun.chiu@instill.tech>
2022-06-07 14:40:11 +0800

    refactor: move destination types into dedicated folder

---
+++ src/lib/instill/connector/destination/index.ts
@@ -0,0 +1,11 @@
+import {
+  Destination,
+  DestinationWithDefinition,
+  DestinationWithPipelines,
+} from "./types";
+
+export type {
+  Destination,
+  DestinationWithDefinition,
+  DestinationWithPipelines,
+};
