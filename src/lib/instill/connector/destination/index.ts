b6e54b12b5b5fccaefd4e68d22c38567def4d9e2
Po-Chun, Chiu <pochun.chiu@instill.tech>
2022-06-07 14:43:58 +0800

    refactor: move destination query to dedicated file

--- src/lib/instill/connector/destination/index.ts
+++ src/lib/instill/connector/destination/index.ts
@@ -4,8 +4,33 @@ import {
   DestinationWithPipelines,
 } from "./types";
 
+import type {
+  ListDestinationDefinitionsResponse,
+  GetDestinationDefinitionResponse,
+  ListDestinationsResponse,
+  GetDestinationResponse,
+} from "./queries";
+
+import {
+  listDestinationDefinitionsQuery,
+  getDestinationDefinitionQuery,
+  listDestinationsQuery,
+  getDestinationQuery,
+} from "./queries";
+
+export {
+  listDestinationDefinitionsQuery,
+  getDestinationDefinitionQuery,
+  listDestinationsQuery,
+  getDestinationQuery,
+};
+
 export type {
   Destination,
   DestinationWithDefinition,
   DestinationWithPipelines,
+  ListDestinationDefinitionsResponse,
+  GetDestinationDefinitionResponse,
+  ListDestinationsResponse,
+  GetDestinationResponse,
 };
