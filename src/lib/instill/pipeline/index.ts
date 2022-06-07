8dc4c9a17ec72dfefd4d7be6d61df2f3ce448683
Po-Chun, Chiu <pochun.chiu@instill.tech>
2022-06-07 15:15:53 +0800

    refactor: move pipeline types into dedicated file

--- src/lib/instill/pipeline/index.ts
+++ src/lib/instill/pipeline/index.ts
@@ -1,3 +1,21 @@
 import { mockPipelines } from "./mocks";
 
+import type {
+  PipelineMode,
+  PipelineState,
+  Pipeline,
+  PipelineWithRawRecipe,
+  RawPipelineRecipe,
+  PipelineRecipe,
+} from "./types";
+
 export { mockPipelines };
+
+export type {
+  PipelineMode,
+  PipelineState,
+  Pipeline,
+  PipelineWithRawRecipe,
+  RawPipelineRecipe,
+  PipelineRecipe,
+};
