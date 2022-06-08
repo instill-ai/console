7bf6c3c1f187d96bc4a0199118e2efea7328d7f3
Po-Chun, Chiu <pochun.chiu@instill.tech>
2022-06-08 16:00:01 +0800

    feat: add amplitude function

---
+++ src/lib/amplitude.ts
@@ -0,0 +1,49 @@
+import { Nullable } from "@/types/general";
+import { init, setUserId, track } from "@amplitude/analytics-browser";
+
+export type AmplitudeEventProperties = {
+  type: "navigation" | "critical_action";
+};
+
+export type AmplitudeEvent =
+  // Navigtion
+  | "hit_pipelines_page"
+  | "hit_pipeline_page"
+  | "hit_create_pipeline_page"
+  | "hit_models_page"
+  | "hit_model_page"
+  | "hit_create_model_page"
+  | "hit_sources_page"
+  | "hit_source_page"
+  | "hit_create_source_page"
+  | "hit_destinations_page"
+  | "hit_destination_page"
+  | "hit_create_destination_page"
+  | "hit_onboarding_page"
+
+  // Critical event
+  | "create_model"
+  | "delete_model"
+  | "create_pipeline"
+  | "delete_pipeline"
+  | "create_destination"
+  | "delete_destination"
+  | "create_source"
+  | "delete_source";
+
+export const initAmplitude = (userId: Nullable<string>) => {
+  if (process.env.NEXT_PUBLIC_AMPLITUDE_KEY) {
+    init(process.env.NEXT_PUBLIC_AMPLITUDE_KEY, userId ? userId : undefined);
+  }
+};
+
+export const setAmplitudeUserId = (userId: string) => {
+  setUserId(userId);
+};
+
+export const sendAmplitudeData = (
+  eventType: AmplitudeEvent,
+  eventProperties: AmplitudeEventProperties
+) => {
+  track(eventType, eventProperties);
+};
