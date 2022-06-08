f0dbc119a5e52f7c373a65470cb101eace41135a
Po-Chun, Chiu <pochun.chiu@instill.tech>
2022-06-08 11:20:12 +0800

    refactor: move PipelinesTable into dedicated folder

--- src/components/ui/Tables/index.ts
+++ src/components/ui/Tables/index.ts
@@ -4,7 +4,14 @@ import type { SourcesTableProps } from "./SourcesTable";
 import SourcesTable from "./SourcesTable";
 import type { ModelsTableProps } from "./ModelsTable";
 import ModelsTable from "./ModelsTable";
+import type { PipelinesTableProps } from "./PipelinesTable";
+import PipelinesTable from "./PipelinesTable";
 
-export type { DestinationsTableProps, SourcesTableProps, ModelsTableProps };
+export type {
+  DestinationsTableProps,
+  SourcesTableProps,
+  ModelsTableProps,
+  PipelinesTableProps,
+};
 
-export { DestinationsTable, SourcesTable, ModelsTable };
+export { DestinationsTable, SourcesTable, ModelsTable, PipelinesTable };
