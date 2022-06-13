504b17b3a68afa48e3be60dea6b96d6da06660d7
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-06-13 15:43:59 +0800

    feat: setup server in jest

--- jest.setup.ts
+++ jest.setup.ts
@@ -7,4 +7,8 @@ import "@testing-library/jest-dom/extend-expect";
 
 // ref: https://nextjs.org/docs/basic-features/environment-variables#test-environment-variables
 import { loadEnvConfig } from "@next/env";
+import { server } from "mocks";
 loadEnvConfig(process.cwd());
+
+beforeAll(() => server.listen());
+afterAll(() => server.close());
