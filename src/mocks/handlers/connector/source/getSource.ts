385d790e9e7e1af2230261412e259317ed0045f1
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-06-14 13:22:34 +0800

    fix: fix msw url issue

--- src/mocks/handlers/connector/source/getSource.ts
+++ src/mocks/handlers/connector/source/getSource.ts
@@ -9,28 +9,32 @@ const getSourceHandler = rest.get<
   Record<string, never>,
   GetSourceParam,
   GetSourceResponse
->("/source-connectors/:sourceId", (req, res, ctx) => {
-  const { sourceId } = req.params;
-  return res(
-    ctx.json({
-      source_connector: {
-        name: "source-connectors/source-http",
-        uid: "3a98d0ce-b738-40c4-a5ab-6b359f00b3ba",
-        id: "source-http",
-        source_connector_definition: "source-connector-definitions/source-http",
-        connector: {
-          description: "",
-          configuration: "{}",
-          state: "STATE_CONNECTED",
-          tombstone: false,
-          user: "users/local-user",
-          create_time: "2022-06-14T05:14:47.801646Z",
-          update_time: "2022-06-14T05:14:47.803989Z",
-          org: "",
+>(
+  `${process.env.NEXT_PUBLIC_CONNECTOR_API_ENDPOINT}/${process.env.NEXT_PUBLIC_API_VERSION}/source-connectors/:sourceId`,
+  (req, res, ctx) => {
+    const { sourceId } = req.params;
+    return res(
+      ctx.json({
+        source_connector: {
+          name: "source-connectors/source-http",
+          uid: "3a98d0ce-b738-40c4-a5ab-6b359f00b3ba",
+          id: "source-http",
+          source_connector_definition:
+            "source-connector-definitions/source-http",
+          connector: {
+            description: "",
+            configuration: "{}",
+            state: "STATE_CONNECTED",
+            tombstone: false,
+            user: "users/local-user",
+            create_time: "2022-06-14T05:14:47.801646Z",
+            update_time: "2022-06-14T05:14:47.803989Z",
+            org: "",
+          },
         },
-      },
-    })
-  );
-});
+      })
+    );
+  }
+);
 
 export default getSourceHandler;
