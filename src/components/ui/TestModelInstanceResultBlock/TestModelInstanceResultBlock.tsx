dda0c6ed1bebb061ee48dafd7401ebd158b2d221
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-06-15 19:53:31 +0800

    feat: add TestModelInstanceResultBlock

---
+++ src/components/ui/TestModelInstanceResultBlock/TestModelInstanceResultBlock.tsx
@@ -0,0 +1,53 @@
+import { Dispatch, FC, SetStateAction, useState } from "react";
+import cn from "clsx";
+import { CodeBlock } from "react-code-blocks";
+
+export type TestModelInstanceResultBlockProps = {
+  result: string;
+  width: string;
+  blockIsOpen: boolean;
+  setBlockIsOpen: Dispatch<SetStateAction<boolean>>;
+};
+
+const TestModelInstanceResultBlock: FC<TestModelInstanceResultBlockProps> = ({
+  result,
+  width,
+  blockIsOpen,
+  setBlockIsOpen,
+}) => {
+  return (
+    <div className={cn("flex flex-col bg-white", width)}>
+      <div className="flex flex-row p-2.5">
+        <p className="instill-text-body my-auto mr-auto text-instillGrey90">
+          Testing result
+        </p>
+        <button
+          className="flex bg-instillGrey05 py-[5px] px-2.5"
+          onClick={() => {
+            setBlockIsOpen((prev) => !prev);
+          }}
+        >
+          {blockIsOpen ? "↑" : "↓"}
+        </button>
+      </div>
+      {blockIsOpen ? (
+        <div className="w-full bg-white">
+          <CodeBlock
+            text={result}
+            language="json"
+            theme="googlecode"
+            showLineNumbers={false}
+            customStyle={{
+              backgroundColor: "white",
+              fontFamily: "sans",
+              fontSize: "14px",
+              fontWeight: "400",
+            }}
+          />
+        </div>
+      ) : null}
+    </div>
+  );
+};
+
+export default TestModelInstanceResultBlock;
