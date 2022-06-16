20ca857c4b56a65a8ff8740f7c062f9b547c793c
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-06-16 17:01:47 +0800

    fix: fix jest config to ignore the e2e folder

--- jest.config.js
+++ jest.config.js
@@ -9,7 +9,7 @@ const createJestConfig = nextJest({
 const customJestConfig = {
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
   testEnvironment: "jest-environment-jsdom",
-  modulePathIgnorePatterns: ["<rootDir>/__tests__/e2e/"],
+  modulePathIgnorePatterns: ["<rootDir>/e2e/"],
   moduleNameMapper: {
     "^@/components/(.*)$": "<rootDir>/src/components/$1",
     "^@/utils/(.*)$": "<rootDir>/src/utils/$1",
