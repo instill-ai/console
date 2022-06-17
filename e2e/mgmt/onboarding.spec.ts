53750188789effdde929f7054ca47b682390d9dc
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-06-17 16:11:27 +0800

    feat: update onboarding.spec.ts

--- e2e/mgmt/onboarding.spec.ts
+++ e2e/mgmt/onboarding.spec.ts
@@ -23,19 +23,24 @@ test("should successfully fill in the onboarding form and submit", async ({
     `${process.env.NEXT_PUBLIC_MAIN_URL}/onboarding`
   );
 
+  // Fill in email
   await page.fill("text='Your email *'", "instill@instill.tech");
   const email = await page.inputValue("text='Your email *'");
   expect(email).toBe("instill@instill.tech");
 
+  // Fill in company
   await page.fill("text='Your company *'", "instill-ai");
+  const company = await page.inputValue("text='Your company *");
+  expect(company).toBe("instill-ai");
 
+  // Select manager
   const select = await page.waitForSelector("#react-select-role-input");
-
   await select.click();
-
   const option = await page.waitForSelector(":text('Manager')", {
     state: "attached",
   });
   await option.scrollIntoViewIfNeeded();
   await option.click();
+  const role = await page.inputValue("text='Your role *");
+  expect(role).toBe("Manager (who make decisions)");
 });
