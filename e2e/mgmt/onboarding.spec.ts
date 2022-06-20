31c56aae0f209cc5c924fd25c16a8adafa064e1b
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-06-20 20:13:06 +0800

    refactor: refactor onboarding e2e test

--- e2e/mgmt/onboarding.spec.ts
+++ e2e/mgmt/onboarding.spec.ts
@@ -3,8 +3,9 @@ import { test, expect } from "@playwright/test";
 test("first time enter user should go to the onboarding page", async ({
   page,
 }) => {
-  if (!process.env.NEXT_PUBLIC_MAIN_URL) return;
-
+  if (!process.env.NEXT_PUBLIC_MAIN_URL) {
+    throw new Error("env not found");
+  }
   await page.goto(process.env.NEXT_PUBLIC_MAIN_URL);
 
   await expect(page).toHaveURL(
@@ -15,80 +16,67 @@ test("first time enter user should go to the onboarding page", async ({
 test("should disable start button, if email input is not correct", async ({
   page,
 }) => {
-  if (!process.env.NEXT_PUBLIC_MAIN_URL) return;
+  if (!process.env.NEXT_PUBLIC_MAIN_URL) {
+    throw new Error("env not found");
+  }
 
-  await page.goto(process.env.NEXT_PUBLIC_MAIN_URL);
-
-  // Fill in email
-  await page.fill("text='Your email *'", "instill");
-
-  // Fill in company
-  await page.fill("text='Your company *'", "instill-ai");
+  await page.goto(`${process.env.NEXT_PUBLIC_MAIN_URL}/onboarding`);
+  await page.locator("#email").fill("instill");
+  await page.type("#company_name", "instill-ai");
 
   // Select role
-  const select = await page.waitForSelector("#react-select-role-input");
-  await select.click();
-  const option = await page.waitForSelector(":text('Manager')", {
-    state: "attached",
-  });
-  await option.scrollIntoViewIfNeeded();
-  await option.click();
+  await page.locator("#role").click();
+  await page.locator("#react-select-role-option-0").click();
 
-  await page.locator("test=Start").isDisabled();
+  const startButtonState = await page.locator("text=Start").isDisabled();
+  expect(startButtonState).toBeTruthy();
 });
 
 test("should disable start button, if other field is not filled in", async ({
   page,
 }) => {
-  if (!process.env.NEXT_PUBLIC_MAIN_URL) return;
+  if (!process.env.NEXT_PUBLIC_MAIN_URL) {
+    throw new Error("env not found");
+  }
 
-  await page.goto(process.env.NEXT_PUBLIC_MAIN_URL);
-  await page.fill("text='Your email *'", "instill@instill.tech");
-  await page.locator("test=Start").isDisabled();
+  await page.goto(`${process.env.NEXT_PUBLIC_MAIN_URL}/onboarding`);
+  await page.locator("#email").fill("instill");
+
+  const startButtonState = await page.locator("text=Start").isDisabled();
+  expect(startButtonState).toBeTruthy();
 });
 
 test("should successfully fill in the onboarding form and submit", async ({
   page,
   context,
 }) => {
-  if (!process.env.NEXT_PUBLIC_MAIN_URL) return;
+  if (!process.env.NEXT_PUBLIC_MAIN_URL) {
+    throw new Error("env not found");
+  }
 
-  await page.goto(process.env.NEXT_PUBLIC_MAIN_URL);
-  await expect(page).toHaveURL(
-    `${process.env.NEXT_PUBLIC_MAIN_URL}/onboarding`
-  );
+  await page.goto(`${process.env.NEXT_PUBLIC_MAIN_URL}/onboarding`);
+  await page.locator("#email").fill("instill@gmail.com");
+  await page.type("#company_name", "instill-ai");
 
-  // Fill in email
-  await page.fill("text='Your email *'", "instill@instill.tech");
-
-  // Fill in company
-  await page.fill("text='Your company *'", "instill-ai");
-
-  // Select role - manager
-  const select = await page.waitForSelector("#react-select-role-input");
-  await select.click();
-  const option = await page.waitForSelector(":text('Manager')", {
-    state: "attached",
-  });
-  await option.scrollIntoViewIfNeeded();
-  await option.click();
+  // Select role
+  await page.locator("#role").click();
+  await page.locator("#react-select-role-option-0").click();
 
   // Accept newsletter subscription
-  await page.locator("#newsletter_subscription").click();
+  await page.locator("#newsletter_subscription").check();
+
+  await page.screenshot({ path: "test.png" });
 
   // Click Start and wait for navigation
-  await page.locator("test=Start").click();
+  await page.locator("text=Start").click();
   await page.waitForNavigation();
 
-  expect(page.url).toBe(`${process.env.NEXT_PUBLIC_MAIN_URL}/pipeline`);
+  expect(page.url()).toBe(`${process.env.NEXT_PUBLIC_MAIN_URL}/pipelines`);
 
   // Check cookie exist
   const cookies = await context.cookies();
   const instillCookies = cookies.find(
     (e) => e.name === process.env.NEXT_PUBLIC_INSTILL_AI_USER_COOKIE_NAME
   );
-  expect(instillCookies).not.toBeNull();
-  expect(
-    JSON.parse(instillCookies?.value as string).cookie_token
-  ).not.toBeNull();
+  expect(instillCookies).toBeDefined();
 });
