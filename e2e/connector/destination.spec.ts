48cdf78eabd881457ba8005b88482107465d47e8
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-06-22 23:10:32 +0800

    refactor: refactor destination spec

--- e2e/connector/destination.spec.ts
+++ e2e/connector/destination.spec.ts
@@ -1,18 +1,14 @@
 import { test, expect } from "@playwright/test";
 
 test("should create new destination", async ({ page }) => {
-  if (!process.env.NEXT_PUBLIC_MAIN_URL) {
-    throw new Error("env not found");
-  }
-
-  await page.goto(`${process.env.NEXT_PUBLIC_MAIN_URL}/destinations/create`);
+  await page.goto("/destinations/create");
 
   // Select destination type
   await page.locator("#destinationDefinition").click({ force: true });
-  await page.locator("#react-select-destination-definition-option-0").click();
+  await page.locator("#react-select-destinationDefinition-option-0").click();
 
   // Create the destination
-  await page.locator("text=Set up destination").click();
+  await page.locator("button", { hasText: "Set up destination" }).click();
   await page.waitForNavigation();
 
   expect(page.url()).toBe(`${process.env.NEXT_PUBLIC_MAIN_URL}/destinations`);
@@ -22,11 +18,7 @@ test("should create new destination", async ({ page }) => {
 });
 
 test("should navigate to destination details page", async ({ page }) => {
-  if (!process.env.NEXT_PUBLIC_MAIN_URL) {
-    throw new Error("env not found");
-  }
-
-  await page.goto(`${process.env.NEXT_PUBLIC_MAIN_URL}/destinations`);
+  await page.goto("/destinations");
   await page.locator("h3", { hasText: "destination-grpc" }).click();
 
   expect(page.url()).toBe(
