37043ff1c7d461fe4b1e68ec9e2908d3d595f3cf
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-06-23 13:56:27 +0800

    feat: add delete pipeline playwright function

--- e2e/common/pipeline.ts
+++ e2e/common/pipeline.ts
@@ -7,3 +7,24 @@ export const openPipelinesPage = async (page: Page) => {
 export const openCreatePipelinePage = async (page: Page) => {
   await page.goto("/pipelines/create");
 };
+
+export const deletePipeline = async (page: Page, pipelineId: string) => {
+  await page.goto(`/pipelines/${pipelineId}`);
+  await page.locator("button", { hasText: "Delete" }).click();
+  await page.locator("#confirmationCode").fill(pipelineId);
+
+  page.screenshot({ path: "test.png" });
+
+  await page.locator("role=dialog >> button:has-text('Delete')").click();
+
+  await Promise.all([
+    page.waitForResponse((response) => {
+      if (!process.env.NEXT_PUBLIC_PIPELINE_API_ENDPOINT) {
+        throw new Error("env not provided");
+      }
+      return response
+        .url()
+        .includes(process.env.NEXT_PUBLIC_PIPELINE_API_ENDPOINT);
+    }),
+  ]);
+};
