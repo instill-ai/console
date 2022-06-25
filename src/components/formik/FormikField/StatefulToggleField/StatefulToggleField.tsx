4c3ed7cbb3de5c36b4bb15319a4bb80bfee2a3a4
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-06-25 21:40:44 +0800

    feat: add StatefulToggleField

---
+++ src/components/formik/FormikField/StatefulToggleField/StatefulToggleField.tsx
@@ -0,0 +1,77 @@
+import { FC } from "react";
+import { Field, FieldProps } from "formik";
+import {
+  BasicToggleFieldProps,
+  StatefulToggleField,
+} from "@instill-ai/design-system";
+import { Nullable, State } from "@/types/general";
+
+export type ToggleFieldProps = Omit<BasicToggleFieldProps, "onChangeInput"> & {
+  name: string;
+  additionalOnChangeCb: Nullable<(value: boolean) => void>;
+  state: State;
+};
+
+const StatefulToggleFieldWrapper: FC<ToggleFieldProps & FieldProps> = ({
+  field,
+  form,
+  id,
+  defaultChecked,
+  additionalOnChangeCb,
+  additionalMessageOnLabel,
+  error,
+  ...props
+}) => {
+  const onChange = (_: string, value: boolean) => {
+    form.setFieldValue(field.name, value);
+    if (additionalOnChangeCb) {
+      additionalOnChangeCb(value);
+    }
+  };
+
+  return (
+    <StatefulToggleField
+      {...props}
+      id={id}
+      error={error}
+      onChangeInput={onChange}
+      defaultChecked={defaultChecked}
+      additionalMessageOnLabel={additionalMessageOnLabel}
+    />
+  );
+};
+
+const StatefulToggleFieldFormikWrapper: FC<ToggleFieldProps> = ({
+  id,
+  name,
+  disabled,
+  readOnly,
+  required,
+  description,
+  label,
+  additionalOnChangeCb,
+  additionalMessageOnLabel,
+  defaultChecked,
+  error,
+  state,
+}) => {
+  return (
+    <Field
+      id={id}
+      name={name}
+      component={StatefulToggleFieldWrapper}
+      disabled={disabled}
+      readOnly={readOnly}
+      required={required}
+      description={description}
+      additionalOnChangeCb={additionalOnChangeCb}
+      label={label}
+      additionalMessageOnLabel={additionalMessageOnLabel}
+      defaultChecked={defaultChecked}
+      error={error}
+      state={state}
+    />
+  );
+};
+
+export default StatefulToggleFieldFormikWrapper;
