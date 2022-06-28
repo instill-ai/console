34882b6dd10b195ad07e9187739cee30146c2195
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-06-28 11:02:46 +0800

    feat: separate shell command

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,14 +16,12 @@ test -n "$MODEL_API_ENDPOINT"
 echo "API_VERSION=$API_VERSION"
 test -n "$API_VERSION"
 
-
-find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | \
-xargs -0 sed -i "s#NEXT_PUBLIC_DISABLE_USAGE_COLLECTION#$DISABLE_USAGE_COLLECTION#g" | \
-xargs -0 sed -i "s#NEXT_PUBLIC_MGMT_API_ENDPOINT#$MGMT_API_ENDPOINT#g" | \
-xargs -0 sed -i "s#NEXT_PUBLIC_PIPELINE_API_ENDPOINT#$PIPELINE_API_ENDPOINT#g" | \
-xargs -0 sed -i "s#NEXT_PUBLIC_CONNECTOR_API_ENDPOINT#$CONNECTOR_API_ENDPOINT#g" | \
-xargs -0 sed -i "s#NEXT_PUBLIC_MODEL_API_ENDPOINT#$MODEL_API_ENDPOINT#g" | \
-xargs -0 sed -i "s#NEXT_PUBLIC_API_VERSION#$API_VERSION#g" | \
+find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#NEXT_PUBLIC_DISABLE_USAGE_COLLECTION#$DISABLE_USAGE_COLLECTION#g"
+find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#NEXT_PUBLIC_MGMT_API_ENDPOINT#$MGMT_API_ENDPOINT#g"
+find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#NEXT_PUBLIC_PIPELINE_API_ENDPOINT#$PIPELINE_API_ENDPOINT#g"
+find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#NEXT_PUBLIC_CONNECTOR_API_ENDPOINT#$CONNECTOR_API_ENDPOINT#g"
+find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#NEXT_PUBLIC_MODEL_API_ENDPOINT#$MODEL_API_ENDPOINT#g"
+find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#NEXT_PUBLIC_API_VERSION#$API_VERSION#g"
 
 echo "Starting Nextjs"
 exec "$@"
