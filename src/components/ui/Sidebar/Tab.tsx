71184f7e2b4c42b6a1520734d7e8911e3e593848
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-06-28 22:43:14 +0800

    refactor: refactor sidebar icon and hover style

--- src/components/ui/Sidebar/Tab.tsx
+++ src/components/ui/Sidebar/Tab.tsx
@@ -1,15 +1,102 @@
 import Link from "next/link";
 import { FC, ReactElement } from "react";
 import cn from "clsx";
+import {
+  DataDestinationIcon,
+  DataSourceIcon,
+  GearIcon,
+  ModelIcon,
+  PipelineIcon,
+  ResourceIcon,
+} from "@instill-ai/design-system";
 
-interface TabProps {
-  icon: ReactElement;
+export type TabProps = {
   tabName: string;
-  link: string;
+  link:
+    | "/sources"
+    | "/pipelines"
+    | "/destinations"
+    | "/models"
+    | "/setting"
+    | "/resources";
   isCollapsed: boolean;
-}
+  isCurrent: boolean;
+};
 
-const Tab: FC<TabProps> = ({ icon, tabName, link, isCollapsed }) => {
+const Tab: FC<TabProps> = ({ tabName, link, isCollapsed, isCurrent }) => {
+  let icon: ReactElement;
+
+  const iconColor = cn(
+    isCurrent ? "fill-instillBlue50" : "fill-instillGrey30",
+    "group-hover:fill-instillBlue10"
+  );
+  const iconWidth = "w-[30px]";
+  const iconHeight = "h-[30px]";
+  const iconPosition = isCollapsed ? "my-auto" : "my-auto";
+
+  switch (link) {
+    case "/sources":
+      icon = (
+        <DataSourceIcon
+          color={iconColor}
+          width={iconWidth}
+          height={iconHeight}
+          position={iconPosition}
+        />
+      );
+      break;
+    case "/destinations":
+      icon = (
+        <DataDestinationIcon
+          color={iconColor}
+          width={iconWidth}
+          height={iconHeight}
+          position={iconPosition}
+        />
+      );
+      break;
+    case "/models":
+      icon = (
+        <ModelIcon
+          color={iconColor}
+          width={iconWidth}
+          height={iconHeight}
+          position={iconPosition}
+        />
+      );
+      break;
+    case "/pipelines":
+      icon = (
+        <PipelineIcon
+          color={iconColor}
+          width={iconWidth}
+          height={iconHeight}
+          position={iconPosition}
+        />
+      );
+      break;
+    case "/resources":
+      icon = (
+        <ResourceIcon
+          color={iconColor}
+          width={iconWidth}
+          height={iconHeight}
+          position={iconPosition}
+        />
+      );
+      break;
+    case "/setting":
+      icon = (
+        <GearIcon
+          color={iconColor}
+          width={iconWidth}
+          height={iconHeight}
+          position={iconPosition}
+        />
+      );
+      break;
+  }
+
   return (
     <Link href={link}>
       <a
@@ -22,7 +109,12 @@ const Tab: FC<TabProps> = ({ icon, tabName, link, isCollapsed }) => {
       >
         <div className="px-1"> {icon}</div>
         {isCollapsed ? null : (
-          <p className="my-auto text-base leading-[28px] text-instillGrey30 group-hover:text-instillBlue10">
+          <p
+            className={cn(
+              "my-auto text-base leading-[28px] group-hover:text-instillBlue10",
+              isCurrent ? "text-instillBlue50" : "text-instillGrey30"
+            )}
+          >
             {tabName}
           </p>
         )}
