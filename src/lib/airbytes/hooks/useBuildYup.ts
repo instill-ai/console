d4c6eaf1ecb2787c976217b39323cc2e30601ac6
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-07-05 00:01:14 +0800

    feat: add useBuildYup

---
+++ src/lib/airbytes/hooks/useBuildYup.ts
@@ -0,0 +1,18 @@
+import { Nullable } from "@/types/general";
+import { useMemo } from "react";
+import airbyteSchemaToYup from "../airbyteSchemaToYup";
+import { AirbyteJsonSchema, SelectedItemMap } from "../types";
+
+const useBuildYup = (
+  jsonSchema: Nullable<AirbyteJsonSchema>,
+  selectedItemMap: SelectedItemMap
+) => {
+  const yup = useMemo(() => {
+    if (!jsonSchema) return null;
+    return airbyteSchemaToYup(jsonSchema, selectedItemMap);
+  }, [jsonSchema, selectedItemMap]);
+
+  return yup;
+};
+
+export default useBuildYup;
