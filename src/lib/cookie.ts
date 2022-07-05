e39a619ace812f71c27dc20f44834f76cb2e2424
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-07-05 15:35:29 +0800

    feat: let cookie lib accept nullable domain name

--- src/lib/cookie.ts
+++ src/lib/cookie.ts
@@ -1,11 +1,12 @@
 import { NextApiResponse } from "next";
 import { serialize } from "cookie";
+import { Nullable } from "@/types/general";
 
 export const setCookie = (
   res: NextApiResponse,
   value: string,
   key: string,
-  domain: string,
+  domain: Nullable<string>,
   maxAge: number,
   httOnly: boolean
 ) => {
@@ -16,7 +17,7 @@ export const setCookie = (
     secure: process.env.NODE_ENV === "production" ? true : false,
     path: "/",
     sameSite: "lax",
-    domain: domain,
+    domain: domain ? domain : undefined,
   });
 
   res.setHeader("Set-Cookie", cookie);
