6b0452840332d741e293911b06ea40fe2d77c9bd
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-07-05 11:16:34 +0800

    refactor: refactor CreateDestinationForm

--- src/components/forms/connector/destination/CreateDestinationForm/CreateDestinationForm.tsx
+++ src/components/forms/connector/destination/CreateDestinationForm/CreateDestinationForm.tsx
@@ -1,36 +1,91 @@
-import { FC, useEffect, useCallback, useState } from "react";
-import { Formik } from "formik";
+import { FC, useCallback, useMemo, useState } from "react";
 import { useRouter } from "next/router";
 import {
   BasicProgressMessageBox,
+  BasicSingleSelect,
+  BasicTextField,
   SingleSelectOption,
 } from "@instill-ai/design-system";
 
-import { TextField } from "../../../../formik/FormikField";
-import { FormikFormBase } from "@/components/formik";
-import { ConnectorIcon, PrimaryButton } from "@/components/ui";
+import { PrimaryButton } from "@/components/ui";
 import { CreateDestinationPayload } from "@/lib/instill";
 import { Nullable } from "@/types/general";
-import { useCreateDestination, useDestinations } from "@/services/connector";
+import {
+  useCreateDestination,
+  useDestinationDefinitions,
+} from "@/services/connector";
 import { useAmplitudeCtx } from "context/AmplitudeContext";
-import { sendAmplitudeData } from "@/lib/amplitude";
+import { FormBase } from "@/components/forms/commons";
+import {
+  AirbyteFormErrors,
+  AirbyteFormValues,
+  useBuildYup,
+} from "@/lib/airbytes";
+import { AirbyteDestinationFields } from "@/lib/airbytes/components";
 
-export type CreateDestinationFormValues = {
-  id: string;
-  definition: string;
-};
+export type CreateDestinationFormValues = AirbyteFormValues;
+export type CreateDestinationFormErrors = AirbyteFormErrors;
 
 const CreateDestinationForm: FC = () => {
   const router = useRouter();
   const { amplitudeIsInit } = useAmplitudeCtx();
+  const [createDestinationValues, setCreateDestinationFormValues] =
+    useState<CreateDestinationFormValues>({
+      id: null,
+      definition: null,
+    });
+  const [createDestinationErrors, setCreateDestinationErrors] =
+    useState<CreateDestinationFormErrors>({
+      id: null,
+      definition: null,
+    });
 
   // ###################################################################
   // #                                                                 #
   // # 1 - Initialize the destination definition                       #
   // #                                                                 #
   // ###################################################################
-  //
-  // A user can only have a http destination and a grpc destination
+
+  const destinationDefinitions = useDestinationDefinitions();
+
+  const destinationOptions = useMemo(() => {
+    if (!destinationDefinitions.isSuccess) return [];
+
+    const options: SingleSelectOption[] = [];
+
+    for (const definition of destinationDefinitions.data) {
+      options.push({
+        label: definition.id,
+        value: definition.name,
+      });
+    }
+
+    return options;
+  }, [destinationDefinitions.isSuccess, destinationDefinitions.data]);
+
+  const selectedDestinationOption = useMemo(() => {
+    if (!createDestinationValues?.definition || !destinationOptions) {
+      return null;
+    }
+
+    return (
+      destinationOptions.find(
+        (e) => e.value === createDestinationValues.definition
+      ) || null
+    );
+  }, [createDestinationValues?.definition, destinationOptions]);
+
+  const selectedDestinationDefinition = useMemo(() => {
+    if (!destinationDefinitions.isSuccess || !createDestinationValues) {
+      return null;
+    }
+
+    return (
+      destinationDefinitions.data.find(
+        (e) => e.name === createDestinationValues.definition
+      ) ?? null
+    );
+  }, [destinationDefinitions.isSuccess, createDestinationValues?.definition]);
 
   // ###################################################################
   // #                                                                 #
@@ -44,53 +99,86 @@ const CreateDestinationForm: FC = () => {
 
   const createDestination = useCreateDestination();
 
-  const onSubmitHandler = useCallback(
-    (values) => {
-      if (!values.destinationDefinition || !destinations.isSuccess) return;
-
-      const payload: CreateDestinationPayload = {
-        id: values.destinationDefinition,
-        destination_connector_definition: `destination-connector-definitions/${values.destinationDefinition}`,
-        connector: {
-          configuration: "{}",
-        },
-      };
-
-      setIsCreatingDestination(true);
-
-      createDestination.mutate(payload, {
-        onSuccess: () => {
-          setIsCreatingDestination(false);
-          if (amplitudeIsInit) {
-            sendAmplitudeData("create_destination", {
-              type: "critical_action",
-              process: "destination",
-            });
-          }
-          router.push("/destinations");
-        },
-        onError: (error) => {
-          if (error instanceof Error) {
-            setCreateDestinationError(error.message);
-            setIsCreatingDestination(false);
-          } else {
-            setCreateDestinationError(
-              "Something went wrong when deploying model"
-            );
-            setIsCreatingDestination(false);
-          }
-        },
-      });
-    },
-    [amplitudeIsInit, router, createDestination, destinations.isSuccess]
+  const validateSchema = useBuildYup(
+    selectedDestinationDefinition?.connector_definition.spec
+      .connection_specification ?? null,
+    null
   );
 
+  const submitHandler = useCallback(async () => {
+    if (
+      !createDestinationValues.id ||
+      !createDestinationValues.definition ||
+      !validateSchema
+    ) {
+      return;
+    }
+
+    console.log(createDestinationValues, validateSchema);
+
+    try {
+      validateSchema.validateSync(createDestinationValues);
+    } catch (err) {
+      console.log(err);
+      return;
+    }
+
+    console.log("pass");
+
+    const { id, definition, description, ...configuration } =
+      createDestinationValues;
+
+    const payload: CreateDestinationPayload = {
+      id: createDestinationValues.id as string,
+      destination_connector_definition: `destination-connector-definitions/${createDestinationValues.definition}`,
+      connector: {
+        description: createDestinationValues.description as string,
+        configuration: JSON.stringify(configuration),
+      },
+    };
+
+    // setIsCreatingDestination(true);
+
+    // createDestination.mutate(payload, {
+    //   onSuccess: () => {
+    //     setIsCreatingDestination(false);
+    //     if (amplitudeIsInit) {
+    //       sendAmplitudeData("create_destination", {
+    //         type: "critical_action",
+    //         process: "destination",
+    //       });
+    //     }
+    //     router.push("/destinations");
+    //   },
+    //   onError: (error) => {
+    //     if (error instanceof Error) {
+    //       setCreateDestinationError(error.message);
+    //       setIsCreatingDestination(false);
+    //     } else {
+    //       setCreateDestinationError(
+    //         "Something went wrong when deploying model"
+    //       );
+    //       setIsCreatingDestination(false);
+    //     }
+    //   },
+    // });
+  }, [amplitudeIsInit, router, createDestination, validateSchema]);
+
+  const updateFieldValues = useCallback((field: string, value: any) => {
+    setCreateDestinationFormValues((prev) => {
+      return {
+        ...prev,
+        [field]: value,
+      };
+    });
+  }, []);
+
   return (
-    <FormikFormBase marginBottom={null} gapY="gap-y-5" padding={null}>
-      <TextField
+    <FormBase marginBottom={null} gapY="gap-y-5" padding={null}>
+      <BasicTextField
         id="id"
-        name="id"
         label="ID"
+        key="id"
         additionalMessageOnLabel={null}
         description="Pick a name to help you identify this destination in Instill"
         disabled={false}
@@ -99,9 +187,31 @@ const CreateDestinationForm: FC = () => {
         placeholder=""
         type="text"
         autoComplete="off"
-        value={formik.values.id || ""}
+        value={(createDestinationValues?.id as string) ?? null}
         error={null}
-        additionalOnChangeCb={null}
+        onChangeInput={(id, value) => updateFieldValues(id, value)}
+      />
+      <BasicSingleSelect
+        id="definition"
+        key="definition"
+        instanceId="definition"
+        menuPlacement="auto"
+        label="Destination type"
+        additionalMessageOnLabel={null}
+        description={""}
+        disabled={false}
+        readOnly={false}
+        required={false}
+        error={null}
+        value={selectedDestinationOption}
+        options={destinationOptions}
+        onChangeInput={(id, option) => updateFieldValues(id, option?.value)}
+      />
+      <AirbyteDestinationFields
+        selectedDestinationDefinition={selectedDestinationDefinition}
+        fieldValues={createDestinationValues}
+        setFieldValues={setCreateDestinationFormValues}
+        fieldErrors={createDestinationErrors}
       />
       <div className="flex flex-row">
         {createDestinationError ? (
@@ -115,14 +225,14 @@ const CreateDestinationForm: FC = () => {
         ) : null}
         <PrimaryButton
           type="submit"
-          disabled={formik.isValid ? false : true}
+          disabled={false}
           position="ml-auto my-auto"
-          onClickHandler={null}
+          onClickHandler={() => submitHandler()}
         >
           Set up destination
         </PrimaryButton>
       </div>
-    </FormikFormBase>
+    </FormBase>
   );
 };
 
