b3a6bcda57a599bfa1d64ef240838051233376b0
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-07-06 15:11:48 +0800

    feat: add setter and test

--- src/lib/dot/index.test.ts
+++ src/lib/dot/index.test.ts
@@ -23,3 +23,93 @@ describe("getter", () => {
     expect(dot.getter(obj, "foo.aar", "no!")).toBe("no!");
   });
 });
+
+describe("setter", () => {
+  it("sets flat value", () => {
+    const obj = { foo: "bar" };
+    dot.setter(obj, "flat", "value");
+    expect(obj).toEqual({ foo: "bar", flat: "value" });
+  });
+
+  it("removes flat value", () => {
+    const obj = { foo: "bar" };
+    dot.setter(obj, "foo", undefined);
+    expect(obj).toEqual({});
+  });
+
+  it("sets nested value", () => {
+    const obj = { x: "y" };
+    dot.setter(obj, "foo.bar", "hi");
+    expect(obj).toEqual({ x: "y", foo: { bar: "hi" } });
+  });
+
+  it("updates nested value", () => {
+    const obj = { x: "y", foo: { bar: "a" } };
+    dot.setter(obj, "foo.bar", "b");
+    expect(obj).toEqual({ x: "y", foo: { bar: "b" } });
+  });
+
+  it("removes nested value", () => {
+    const obj = { x: "y", foo: { bar: "a" } };
+    dot.setter(obj, "foo.bar", undefined);
+    expect(obj).toEqual({ x: "y", foo: {} });
+    expect(obj.foo).not.toHaveProperty("bar");
+  });
+
+  it("updates deep nested value", () => {
+    const obj = { x: "y", twofoldly: { foo: { bar: "a" } } };
+    dot.setter(obj, "twofoldly.foo.bar", "b");
+    expect(obj).toEqual({ x: "y", twofoldly: { foo: { bar: "b" } } });
+  });
+
+  it("removes deep nested value", () => {
+    const obj = { x: "y", twofoldly: { foo: { bar: "a" } } };
+    dot.setter(obj, "twofoldly.foo.bar", undefined);
+    expect(obj).toEqual({ x: "y", twofoldly: { foo: {} } });
+    expect(obj.twofoldly.foo).not.toHaveProperty("bar");
+  });
+
+  it("sets new array", () => {
+    const obj = { x: "y" };
+    dot.setter(obj, "foo.0", "bar");
+    expect(obj).toEqual({ x: "y", foo: ["bar"] });
+  });
+
+  it("updates nested array value", () => {
+    const obj = { x: "y", foo: ["bar"] };
+    dot.setter(obj, "foo.0", "bar");
+    expect(obj).toEqual({ x: "y", foo: ["bar"] });
+  });
+
+  it("adds new item to nested array", () => {
+    const obj = { x: "y", foo: ["bar"] };
+    dot.setter(obj, "foo.1", "bar2");
+    expect(obj).toEqual({ x: "y", foo: ["bar", "bar2"] });
+  });
+
+  it("sticks to object with int key when defined", () => {
+    const obj = { x: "y", foo: { 0: "a" } };
+    dot.setter(obj, "foo.0", "b");
+    expect(obj).toEqual({ x: "y", foo: { 0: "b" } });
+  });
+
+  // We are currently don't support bracket path
+
+  // it("supports bracket path", () => {
+  //   const obj = { x: "y" };
+  //   dot.setter(obj, "nested[0]", "value");
+  //   expect(obj).toEqual({ x: "y", nested: ["value"] });
+  // });
+
+  it("supports path containing key of the object", () => {
+    const obj = { x: "y" };
+    dot.setter(obj, "a.x.c", "value");
+    expect(obj).toEqual({ x: "y", a: { x: { c: "value" } } });
+  });
+
+  it("can convert primitives to objects before setting", () => {
+    const obj = { x: [{ y: true }] };
+    dot.setter(obj, "x.0.y.z", true);
+    expect(obj).toEqual({ x: [{ y: { z: true } }] });
+  });
+});
--- src/lib/dot/index.ts
+++ src/lib/dot/index.ts
@@ -1,33 +1,80 @@
 /* eslint-disable  @typescript-eslint/no-explicit-any */
 
-export type DotKey = string | string[];
+export type DotPath = string | string[];
 
-const getter = (obj: any, key: DotKey, defaultValue?: any): any => {
-  const path = toPath(key);
+/**
+ * Get value with given path
+ */
 
-  if (typeof obj === "undefined") return defaultValue;
-
-  const firstKey = path.shift();
-
-  if (!firstKey) return obj;
-
-  return getter(obj[firstKey], path, defaultValue);
-
-  // const path = toPath(key);
-  // let num = 0;
-  // while (obj && num < path.length) {
-  //   obj = obj[path[num++]];
-  // }
-  // return obj === undefined ? defaultValue : obj;
+const getter = (obj: any, path: DotPath, defaultValue?: any): any => {
+  path = toPath(path);
+  let index = 0;
+  while (obj && index < path.length) {
+    obj = obj[path[index++]];
+  }
+  return obj === undefined ? defaultValue : obj;
 };
 
-// const setter = (key: string) => {};
+/**
+ * Set value with given path
+ */
+
+const setter = (obj: any, path: DotPath, value: any) => {
+  if (!isObject) return obj;
+  path = toPath(path);
+
+  let index = -1;
+  const length = path.length;
+  const lastIndex = length - 1;
+  let nested = obj;
+
+  while (nested != null && ++index < length) {
+    const key = path[index];
+    let newValue = value;
+
+    if (index !== lastIndex) {
+      const objValue = nested[key];
+      newValue = isObject(objValue)
+        ? objValue
+        : isInteger(path[index + 1])
+        ? []
+        : {};
+    }
+
+    if (newValue === undefined) {
+      delete nested[key];
+    } else {
+      nested[key] = newValue;
+    }
+
+    nested = nested[key];
+  }
+};
 
 export default {
   getter,
+  setter,
 };
 
-const toPath = (key: DotKey): string[] => {
-  if (Array.isArray(key)) return key;
-  return key.split(".");
+const toPath = (path: DotPath): string[] => {
+  if (Array.isArray(path)) return path;
+  return path.split(".");
+};
+
+/**
+ * Checks if `value` is the object
+ * (e.g. arrays, functions, objects, regexes, `new Number(0)`, and `new String('')`)
+ */
+
+const isObject = (value: any) => {
+  const type = typeof value;
+  return value !== null && (type === "object" || type === "function");
+};
+
+/**
+ * Checks if `value` is the integer
+ */
+
+const isInteger = (value: any): boolean => {
+  return String(Math.floor(Number(value))) === value;
 };
