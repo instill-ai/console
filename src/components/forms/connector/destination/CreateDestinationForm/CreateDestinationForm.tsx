e3a7b1ed6c7f7a8c93d018b23af979e287b9a19e
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-07-28 21:25:48 +0800

    fix: fix http and grpc destination not correctly setup issue

--- src/components/forms/connector/destination/CreateDestinationForm/CreateDestinationForm.tsx
+++ src/components/forms/connector/destination/CreateDestinationForm/CreateDestinationForm.tsx
@@ -107,6 +107,12 @@ const CreateDestinationForm: FC<CreateDestinationFormProps> = ({
       return null;
     }
 
+    console.log(
+      destinationDefinitions.data.find(
+        (e) => e.name === fieldValues.definition
+      ) ?? null
+    );
+
     return (
       destinationDefinitions.data.find(
         (e) => e.name === fieldValues.definition
@@ -117,6 +123,44 @@ const CreateDestinationForm: FC<CreateDestinationFormProps> = ({
     destinationDefinitions.data,
     fieldValues?.definition,
   ]);
+
+  // Instill Ai provided connector HTTP and gRPC can only have default id destination-http and destination-grpc
+  // We need to make sure user have proper instruction on this issue.
+
+  const canSetIdField = useMemo(() => {
+    if (!selectedDestinationDefinition) return true;
+
+    if (
+      selectedDestinationDefinition.connector_definition.docker_repository ===
+        "instill-ai/destination-grpc" ||
+      selectedDestinationDefinition.connector_definition.docker_repository ===
+        "instill-ai/destination-http"
+    ) {
+      return false;
+    } else {
+      return true;
+    }
+  }, [selectedDestinationDefinition]);
+
+  const defaultId = useMemo(() => {
+    if (!selectedDestinationDefinition) return null;
+
+    if (
+      selectedDestinationDefinition.connector_definition.docker_repository ===
+      "instill-ai/destination-grpc"
+    ) {
+      return "destination-grpc";
+    }
+
+    if (
+      selectedDestinationDefinition.connector_definition.docker_repository ===
+      "instill-ai/destination-http"
+    ) {
+      return "destination-http";
+    }
+
+    return null;
+  }, [selectedDestinationDefinition]);
 
   // ###################################################################
   // #                                                                 #
@@ -164,7 +208,9 @@ const CreateDestinationForm: FC<CreateDestinationFormProps> = ({
     if (!airbyteYup) return null;
 
     return yup.object({
-      id: yup.string().required(),
+      id: canSetIdField
+        ? yup.string().required()
+        : yup.string().nullable().notRequired(),
       configuration: airbyteYup,
     });
   }, [airbyteYup]);
@@ -202,16 +248,53 @@ const CreateDestinationForm: FC<CreateDestinationFormProps> = ({
 
     setFieldErrors(null);
 
-    const payload: CreateDestinationPayload = {
-      id: fieldValues.id as string,
-      destination_connector_definition: `destination-connector-definitions/${
-        fieldValues.definition as string
-      }`,
-      connector: {
-        description: fieldValues.description as string,
-        configuration: fieldValues.configuration as AirbyteFieldValues,
-      },
-    };
+    let payload = {} as CreateDestinationPayload;
+
+    if (
+      selectedDestinationDefinition?.connector_definition.docker_repository ===
+      "instill-ai/destination-grpc"
+    ) {
+      payload = {
+        id: "destination-grpc",
+        destination_connector_definition: `destination-connector-definitions/${
+          fieldValues.definition as string
+        }`,
+        connector: {
+          description: fieldValues.description as string,
+          configuration:
+            (fieldValues.configuration as AirbyteFieldValues) ?? {},
+        },
+      };
+    } else if (
+      selectedDestinationDefinition?.connector_definition.docker_repository ===
+      "instill-ai/destination-http"
+    ) {
+      payload = {
+        id: "destination-http",
+        destination_connector_definition: `destination-connector-definitions/${
+          fieldValues.definition as string
+        }`,
+        connector: {
+          description: fieldValues.description as string,
+          configuration:
+            (fieldValues.configuration as AirbyteFieldValues) ?? {},
+        },
+      };
+    } else {
+      payload = {
+        id: fieldValues.id as string,
+        destination_connector_definition: `destination-connector-definitions/${
+          fieldValues.definition as string
+        }`,
+        connector: {
+          description: fieldValues.description as string,
+          configuration:
+            (fieldValues.configuration as AirbyteFieldValues) ?? {},
+        },
+      };
+    }
+
+    console.log(payload);
 
     setMessageBoxState(() => ({
       activate: true,
@@ -269,6 +352,7 @@ const CreateDestinationForm: FC<CreateDestinationFormProps> = ({
     fieldValues,
     setResult,
     setStepNumber,
+    selectedDestinationDefinition,
   ]);
 
   const updateFieldValues = useCallback((field: string, value: string) => {
@@ -289,16 +373,29 @@ const CreateDestinationForm: FC<CreateDestinationFormProps> = ({
           key="id"
           description="Pick a name to help you identify this destination in Instill"
           required={true}
-          value={fieldValues ? (fieldValues.id as string) ?? null : null}
+          disabled={canSetIdField ? false : true}
+          additionalMessageOnLabel={
+            canSetIdField
+              ? null
+              : `${selectedDestinationOption?.label} destination's id can only be ${defaultId}`
+          }
+          value={
+            canSetIdField
+              ? fieldValues
+                ? (fieldValues.id as string) ?? null
+                : null
+              : defaultId
+          }
           error={fieldErrors ? (fieldErrors.id as string) ?? null : null}
           onChangeInput={(id, value) => updateFieldValues(id, value)}
         />
+
         <BasicTextArea
           id="description"
           label="Description"
           key="description"
           description="Fill with a short description of your data destination"
-          required={true}
+          required={false}
           error={
             fieldErrors ? (fieldErrors.description as string) ?? null : null
           }
