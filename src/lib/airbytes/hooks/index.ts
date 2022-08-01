04ed96c9883dc1beb23553d54e110a322ef32295
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-08-01 23:08:34 +0800

    feat: add useAirbyteFieldValues

--- src/lib/airbytes/hooks/index.ts
+++ src/lib/airbytes/hooks/index.ts
@@ -1,5 +1,11 @@
 import useBuildAirbyteFields from "./useBuildAirbyteFields";
 import useBuildAirbyteYup from "./useBuildAirbyteYup";
 import useAirbyteFormTree from "./useAirbyteFormTree";
+import useAirbyteFieldValues from "./useAirbyteFieldValues";
 
-export { useBuildAirbyteFields, useBuildAirbyteYup, useAirbyteFormTree };
+export {
+  useBuildAirbyteFields,
+  useBuildAirbyteYup,
+  useAirbyteFormTree,
+  useAirbyteFieldValues,
+};
