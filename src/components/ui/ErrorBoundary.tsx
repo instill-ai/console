a474c38a0ceb78c43fef0290455089548d7e4c14
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-08-18 12:55:28 +0800

    feat: add getDerivedStateFromError

--- src/components/ui/ErrorBoundary.tsx
+++ src/components/ui/ErrorBoundary.tsx
@@ -18,6 +18,13 @@ class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
     errorInfo: null,
   };
 
+  static getDerivedStateFromError(error: Error) {
+    // This lifecycle is invoked after an error has been thrown by a descendant component.
+    // It receives the error that was thrown as a parameter and should return a value to update state.
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true, error };
+  }
+
   public componentDidCatch(error: Error, errorInfo: ErrorInfo) {
     console.error("Uncaught error:", error, errorInfo);
     this.setState({ error, errorInfo });
