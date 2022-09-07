f9c494e29b437f661c9507f1ef6a3d691a1c3d38
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-09-07 13:18:26 +0800

    feat: Implement onboarding integration test (#271)
    Because
    
    - #270
    
    This commit
    
    - Implement more integration test related to onboarding

---
+++ integration-test/onboarding.spec.ts
@@ -0,0 +1,105 @@
+import { test, expect } from "@playwright/test";
+import { removeRegisteredUser } from "./common/mgmt";
+
+test("first time enter user should go to the onboarding page", async ({
+  page,
+}) => {
+  await removeRegisteredUser();
+  await page.goto("/");
+  await expect(page).toHaveURL(
+    `${process.env.NEXT_PUBLIC_MAIN_URL}/onboarding`
+  );
+});
+
+test("should disable start button, if email input format is not correct", async ({
+  page,
+}) => {
+  await page.goto("/onboarding");
+  const emailField = page.locator("#email");
+  await emailField.fill("instill");
+  await expect(emailField).toHaveValue("instill");
+
+  const companyField = page.locator("#companyName");
+  await companyField.fill("instill-ai");
+  await expect(companyField).toHaveValue("instill-ai");
+
+  // Select role
+  await page.locator("#role").click({ force: true });
+  await page.locator("#react-select-role-option-0").click();
+
+  const startButtonState = await page.locator("text=Start").isDisabled();
+  expect(startButtonState).toBeTruthy();
+});
+
+test("should disable start button, if email format is not correct", async ({
+  page,
+}) => {
+  await page.goto("/onboarding");
+  await page.locator("#email").fill("instill");
+  const startButtonState = await page.locator("text=Start").isDisabled();
+  expect(startButtonState).toBeTruthy();
+});
+
+test("should disable start button, if fields are not filled in", async ({
+  page,
+}) => {
+  await page.goto("/onboarding");
+  expect(await page.locator("text=Start").isDisabled()).toBeTruthy();
+
+  await page.locator("#email").fill("instill@instill.tech");
+  expect(await page.locator("text=Start").isDisabled()).toBeTruthy();
+
+  await page.type("#companyName", "instill-ai");
+  expect(await page.locator("text=Start").isDisabled()).toBeTruthy();
+});
+
+test("should successfully fill in the onboarding form and submit", async ({
+  page,
+  context,
+  browserName,
+}) => {
+  await removeRegisteredUser();
+  await page.goto("/onboarding");
+
+  const emailField = page.locator("#email");
+  await emailField.fill("instill@gmail.com");
+  await expect(emailField).toHaveValue("instill@gmail.com");
+
+  const companyField = page.locator("#companyName");
+  await companyField.fill("instill-ai");
+  await expect(companyField).toHaveValue("instill-ai");
+
+  // Select role
+  await page.locator("#role").click({ force: true });
+  await page.locator("#react-select-role-option-0").click();
+  await expect(page.locator("data-testid=role-selected-option")).toHaveText(
+    "Manager (who makes decisions)"
+  );
+
+  // Accept newsletter subscription
+  await page.locator("#newsletterSubscription").check();
+
+  const startButton = page.locator("text=Start");
+  const startButtonState = await startButton.isDisabled();
+  expect(startButtonState).not.toBeTruthy();
+
+  // Click Start and wait for navigation
+  await Promise.all([
+    // It is important to call waitForNavigation before click to set up waiting.
+    page.waitForNavigation(),
+    // Clicking the link will indirectly cause a navigation.
+    startButton.click(),
+  ]);
+
+  expect(page.url()).toBe(`${process.env.NEXT_PUBLIC_MAIN_URL}/pipelines`);
+
+  // Check cookie exist
+  // Safari have issue when setting up cookies.
+  if (browserName !== "webkit") {
+    const cookies = await context.cookies();
+    const instillCookies = cookies.find(
+      (e) => e.name === process.env.NEXT_PUBLIC_INSTILL_AI_USER_COOKIE_NAME
+    );
+    expect(instillCookies).toBeDefined();
+  }
+});
--- playwright.config.ts
+++ playwright.config.ts
@@ -13,9 +13,9 @@ if (!process.env.NEXT_PUBLIC_MAIN_URL) {
  * See https://playwright.dev/docs/test-configuration.
  */
 const config: PlaywrightTestConfig = {
-  testDir: "./e2e",
+  testDir: "./integration-test",
   /* Maximum time one test can run for. */
-  timeout: 30 * 1000,
+  timeout: 10 * 1000,
   expect: {
     /**
      * Maximum time expect() should wait for the condition to be met.
@@ -28,9 +28,9 @@ const config: PlaywrightTestConfig = {
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 2 : 1,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
