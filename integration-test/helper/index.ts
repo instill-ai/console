f8f250df71b9b7d2d0a67559fe44c0a2a4b2b734
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-09-16 09:58:20 +0800

    feat: add addInstillCookie helper function

--- integration-test/helper/index.ts
+++ integration-test/helper/index.ts
@@ -1,2 +1,16 @@
+import { BrowserContext } from "@playwright/test";
+
 export const delay = (ms: number) =>
   new Promise((resolve) => setTimeout(resolve, ms));
+
+export const addInstillCookie = async (context: BrowserContext) => {
+  await context.addCookies([
+    {
+      name: "instill-ai-user",
+      domain: process.env.NEXT_PUBLIC_MAIN_URL?.split(":")[0],
+      path: "/",
+      value: JSON.stringify({ cookie_token: "instill-token" }),
+      httpOnly: process.env.NODE_ENV === "production" ? true : false,
+    },
+  ]);
+};
