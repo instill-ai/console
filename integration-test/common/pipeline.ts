2a17a85bf7f4035793708c515059884dab4261a0
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-09-18 10:39:26 +0800

    feat: add expectToDeletePipeline

--- integration-test/common/pipeline.ts
+++ integration-test/common/pipeline.ts
@@ -1,4 +1,4 @@
-import { Page } from "@playwright/test";
+import { Page, expect } from "@playwright/test";
 
 export const openPipelinesPage = async (page: Page) => {
   await page.goto("/pipelines");
@@ -12,19 +12,63 @@ export const openCreatePipelinePage = async (page: Page) => {
   await page.goto("/pipelines/create");
 };
 
-export const deletePipeline = async (page: Page, pipelineId: string) => {
-  await page.goto(`/pipelines/${pipelineId}`);
-  await page.locator("button", { hasText: "Delete" }).click();
-  await page.locator("#confirmationCode").fill(pipelineId);
-  await page.locator("role=dialog >> button:has-text('Delete')").click();
+export const expectToDeletePipeline = async (
+  page: Page,
+  pipelineId: string
+) => {
+  // Should enable open delete pipeline modal button
+  const openDeleteModelModalButton = page.locator("button", {
+    hasText: "Delete",
+  });
+  expect(await openDeleteModelModalButton.isEnabled()).toBeTruthy();
+
+  // Should open delete pipeline modal
+  const deleteResourceModal = page.locator("data-testid=delete-resource-modal");
+
   await Promise.all([
-    page.waitForResponse((response) => {
-      if (!process.env.NEXT_PUBLIC_PIPELINE_BACKEND_BASE_URL) {
-        throw new Error("env not provided");
-      }
-      return response
-        .url()
-        .includes(process.env.NEXT_PUBLIC_PIPELINE_BACKEND_BASE_URL);
-    }),
+    openDeleteModelModalButton.click(),
+    deleteResourceModal.isVisible(),
   ]);
+
+  // Should have proper delete pipeline modal titme
+  const modalTitle = deleteResourceModal.locator("h2", {
+    hasText: "Delete This Pipeline",
+  });
+  await expect(modalTitle).toHaveCount(1);
+
+  // Should have correct confirmation code
+  const confirmationCode = deleteResourceModal.locator("label", {
+    hasText: `Please type "${pipelineId}" to confirm.`,
+  });
+  await expect(confirmationCode).toHaveCount(1);
+
+  // Should disable delete pipeline button
+  const deleteButton = deleteResourceModal.locator("button", {
+    hasText: "Delete",
+  });
+  expect(await deleteButton.isDisabled()).toBeTruthy();
+
+  // Should enable cancel deletion button
+  const cancelButton = deleteResourceModal.locator("button", {
+    hasText: "Cancel",
+  });
+  expect(await cancelButton.isEnabled()).toBeTruthy();
+
+  // Should input confirmation code and enable delete button
+  const confirmationCodeInput =
+    deleteResourceModal.locator("#confirmationCode");
+
+  await Promise.all([
+    confirmationCodeInput.fill(pipelineId),
+    deleteButton.isEnabled(),
+  ]);
+  expect(await deleteButton.isEnabled()).toBeTruthy();
+
+  // Should delete pipeline and navigate to pipelines page
+  await Promise.all([page.waitForNavigation(), deleteButton.click()]);
+  expect(page.url()).toEqual(`${process.env.NEXT_PUBLIC_MAIN_URL}/pipelines`);
+
+  // Should not display deleted pipeline
+  const modelItemTitle = page.locator("h3", { hasText: pipelineId });
+  await expect(modelItemTitle).toHaveCount(0);
 };
