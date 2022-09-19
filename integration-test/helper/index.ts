0e0af27bf71d122414109ebc41f6613a65872f40
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-09-19 15:08:32 +0800

    feat: add initialNavigationTo helper

--- integration-test/helper/index.ts
+++ integration-test/helper/index.ts
@@ -1,9 +1,10 @@
-import { BrowserContext } from "@playwright/test";
+import { BrowserContext, expect, Page } from "@playwright/test";
+import { addRegisteredUser } from "../common/mgmt";
 
 export const delay = (ms: number) =>
   new Promise((resolve) => setTimeout(resolve, ms));
 
-export const addInstillCookie = async (context: BrowserContext) => {
+export const addUserCookie = async (context: BrowserContext) => {
   await context.addCookies([
     {
       name: "instill-ai-user",
@@ -13,4 +14,18 @@ export const addInstillCookie = async (context: BrowserContext) => {
       httpOnly: process.env.NODE_ENV === "production" ? true : false,
     },
   ]);
+
+  const newCookies = await context.cookies();
+  const instillAiUser = newCookies.find((e) => e.name === "instill-ai-user");
+  expect(instillAiUser).toBeDefined();
+};
+
+export const initialNavigationTo = async (page: Page, path: string) => {
+  await addRegisteredUser();
+  await page.goto(path, { waitUntil: "networkidle" });
+  expect(page.url()).toBe(
+    path[0] === "/"
+      ? `${process.env.NEXT_PUBLIC_MAIN_URL}${path}`
+      : `${process.env.NEXT_PUBLIC_MAIN_URL}/${path}`
+  );
 };
