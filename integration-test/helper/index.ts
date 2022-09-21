1c3b1214f79e7e83a760473ed273e9e9cd0e15ab
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-09-21 20:27:29 +0800

    feat: add expectToSelectReactSelectOption

--- integration-test/helper/index.ts
+++ integration-test/helper/index.ts
@@ -1,4 +1,4 @@
-import { BrowserContext, expect } from "@playwright/test";
+import { BrowserContext, expect, Locator, Page } from "@playwright/test";
 
 export const delay = (ms: number) =>
   new Promise((resolve) => setTimeout(resolve, ms));
@@ -17,4 +17,21 @@ export const addUserCookie = async (context: BrowserContext) => {
   const newCookies = await context.cookies();
   const instillAiUser = newCookies.find((e) => e.name === "instill-ai-user");
   expect(instillAiUser).toBeDefined();
+};
+
+export const expectToSelectReactSelectOption = async (
+  clickElement: Locator,
+  selectElement: Locator,
+  waitForElement?: Locator
+) => {
+  await clickElement.click({ force: true });
+
+  if (waitForElement) {
+    await Promise.all([
+      waitForElement.waitFor({ state: "visible" }),
+      selectElement.click(),
+    ]);
+  } else {
+    await selectElement.click();
+  }
 };
