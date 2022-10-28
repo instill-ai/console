689f50d3407f501563507a43bc26206cb8ee3c6f
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-10-28 22:25:12 +0800

    feat: disable html report (#297)
    In order to not block other CI tests, we disable HTML report of the playwright.

--- playwright.config.ts
+++ playwright.config.ts
@@ -34,7 +34,7 @@ const config: PlaywrightTestConfig = {
   /* Opt out of parallel tests on CI. */
   workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: process.env.CI ? "dot" : "html",
+  reporter: "dot",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     storageState: "playwright-state.json",
