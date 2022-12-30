050033e4bfe4498080a413fe5acfc99b37e9a53e
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-12-30 22:39:15 +0800

    refactor: refactor checkCreateModelOperationUntilDone to make it have only one functionality

--- src/lib/instill/model/helper.ts
+++ src/lib/instill/model/helper.ts
@@ -1,58 +1,21 @@
-import { Nullable } from "@/types/general";
-import { ProgressMessageBoxState } from "@instill-ai/design-system";
-import { Dispatch, SetStateAction } from "react";
-import { getModelOperationQuery, getModelQuery } from "./queries";
-import { Model } from "./types";
-import {
-  AmplitudeEvent,
-  AmplitudeEventProperties,
-  sendAmplitudeData,
-} from "@/lib/amplitude";
+import { getModelOperationQuery } from "./queries";
 
 export const checkCreateModelOperationUntilDone = async (
-  operationName: string,
-  modelName: string,
-  setModelCreated: Dispatch<SetStateAction<boolean>>,
-  setNewModel: Dispatch<SetStateAction<Nullable<Model>>>,
-  setCreateModelMessageBoxState: Dispatch<
-    SetStateAction<ProgressMessageBoxState>
-  >,
-  amplitudeIsInit: boolean
+  operationName: string
 ) => {
   try {
     const operation = await getModelOperationQuery(operationName);
     if (operation.done) {
-      const model = await getModelQuery(modelName);
-      setModelCreated(true);
-      setNewModel(model);
-      setCreateModelMessageBoxState({
-        activate: true,
-        status: "success",
-        description: null,
-        message: "Succeed.",
-      });
-
-      if (amplitudeIsInit) {
-        sendAmplitudeData("create_github_model", {
-          type: "critical_action",
-          process: "model",
-        });
-      }
-
       return Promise.resolve(true);
     } else {
-      setTimeout(
-        () =>
-          checkCreateModelOperationUntilDone(
-            operationName,
-            modelName,
-            setModelCreated,
-            setNewModel,
-            setCreateModelMessageBoxState,
-            amplitudeIsInit
-          ),
-        1500
-      );
+      return new Promise((resolve) => {
+        setTimeout(async () => {
+          const result = await checkCreateModelOperationUntilDone(
+            operationName
+          );
+          resolve(result);
+        }, 1500);
+      });
     }
   } catch (err) {
     return Promise.reject(err);
