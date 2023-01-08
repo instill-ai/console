f441db75d7bb0b3c0d3dd0064f4b20675a020921
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2023-01-08 18:07:45 +0800

    chore: remove unnecessary config at next.config.js

--- next.config.js
+++ next.config.js
@@ -5,11 +5,4 @@ module.exports = {
   swcMinify: true,
   reactStrictMode: true,
   output: "standalone",
-  webpackDevMiddleware: (config) => {
-    config.watchOptions = {
-      poll: 1000,
-      aggregateTimeout: 300,
-    };
-    return config;
-  },
 };
