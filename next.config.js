605fba01edaca6badafabc4c8f8e1d153bb0707a
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2023-01-18 21:20:08 +0800

    feat: add babelrc for storybook

--- next.config.js
+++ next.config.js
@@ -5,4 +5,9 @@ module.exports = {
   swcMinify: true,
   reactStrictMode: true,
   output: "standalone",
+
+  // We need to ignore the babelrc.json at the root, that is for storybook
+  experimental: {
+    forceSwcTransforms: true,
+  },
 };
