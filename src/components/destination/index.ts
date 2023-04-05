4d29a5a4566b7d3ee605e55c86d963d9b107ad19
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2023-04-05 17:50:42 +0800

    chore: delete unused components

--- src/components/destination/index.ts
+++
@@ -1,4 +0,0 @@
-export { ConfigureDestinationForm } from "./ConfigureDestinationForm";
-export type { ConfigureDestinationFormProps } from "./ConfigureDestinationForm";
-export { CreateDestinationForm } from "./CreateDestinationForm";
-export type { CreateDestinationFormProps } from "./CreateDestinationForm";
