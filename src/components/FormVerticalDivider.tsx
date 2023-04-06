55b1535d7eb7759224b3863d9936fe51b0c041e8
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2023-04-06 19:49:41 +0800

    chore: deprecate unused components

--- src/components/FormVerticalDivider.tsx
+++
@@ -1,8 +0,0 @@
-export const FormVerticalDivider = () => {
-  return (
-    <div className="relative mx-5 h-full w-6">
-      <p className="absolute top-5 z-10 bg-instillGrey05 text-instill-h3">OR</p>
-      <div className="absolute left-1/2 h-full border-r"></div>
-    </div>
-  );
-};
