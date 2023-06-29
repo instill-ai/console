ea7eaf1ece54f1d843dc6581040c93986d8ed870
Po-Chun Chiu <57251712+EiffelFly@users.noreply.github.com>
2023-06-29 16:09:26 +0800

    feat(model-hub): add model-hub page (#432)
    Because
    
    - We will need model-hub to display all the model we have
    
    This commit
    
    - add model-hub page

--- src/components/Sidebar/PipelineCollapsible.tsx
+++ src/components/Sidebar/PipelineCollapsible.tsx
@@ -1,34 +1,35 @@
 import cn from "clsx";
-import Link from "next/link";
 import { useState } from "react";
 import { useRouter } from "next/router";
 import { Icons } from "@instill-ai/design-system";
 import * as Collapsible from "@radix-ui/react-collapsible";
+import { SidebarLink } from "./SidebarLink";
 
-export const PipelineCollapsible = () => {
+export type PipelineCollapsibleProps = {
+  className?: string;
+};
+
+export const PipelineCollapsible = (props: PipelineCollapsibleProps) => {
+  const { className } = props;
   const [isOpen, setIsOpen] = useState(true);
   const router = useRouter();
 
   return (
-    <Collapsible.Root open={isOpen} onOpenChange={setIsOpen} className="w-full">
+    <Collapsible.Root
+      open={isOpen}
+      onOpenChange={setIsOpen}
+      className={cn("w-full", className)}
+    >
       <div className="relative">
-        <Link
+        <SidebarLink
           href="/pipelines"
-          className={cn(
-            "mb-2 flex flex-row items-center rounded-xs border border-transparent px-3 py-2 hover:bg-semantic-bg-base-bg",
-            {
-              "!border-semantic-accent-default border-opacity-100 bg-semantic-accent-bg":
-                router.pathname === "/pipelines",
-            }
-          )}
-        >
-          <div className="flex flex-row items-center space-x-3">
+          icon={
             <Icons.Pipeline className="h-6 w-6 stroke-semantic-fg-primary" />
-            <h4 className="text-semantic-fg-primary product-body-text-2-semibold">
-              Pipelines
-            </h4>
-          </div>
-        </Link>
+          }
+          name="Pipelines"
+          hightlighted={router.pathname.split("/")[1] === "pipelines"}
+          className="px-3"
+        />
         <Collapsible.Trigger className="absolute right-3 top-1/2 z-30 ml-auto -translate-y-1/2">
           {isOpen ? (
             <Icons.ChevronUp className="h-6 w-6 stroke-semantic-fg-primary" />
@@ -37,52 +38,30 @@ export const PipelineCollapsible = () => {
           )}
         </Collapsible.Trigger>
       </div>
-      <Collapsible.Content className="flex flex-col space-y-1">
-        <Link
+      <Collapsible.Content className="flex flex-col">
+        <SidebarLink
           href="/sources"
-          className={cn(
-            "flex flex-row items-center space-x-2 rounded-xs border border-transparent py-2 pl-12 pr-3 hover:bg-semantic-bg-base-bg",
-            {
-              "!border-semantic-accent-default border-opacity-100 bg-semantic-accent-bg":
-                router.pathname === "/sources",
-            }
-          )}
-        >
-          <Icons.Database01 className="h-6 w-6 stroke-semantic-fg-primary" />
-          <p className="stroke-semantic-fg-primary product-body-text-2-semibold">
-            Sources
-          </p>
-        </Link>
-        <Link
-          href="/models"
-          className={cn(
-            "flex flex-row items-center space-x-2 rounded-xs border border-transparent py-2 pl-12 pr-3 hover:bg-semantic-bg-base-bg",
-            {
-              "!border-semantic-accent-default border-opacity-100 bg-semantic-accent-bg":
-                router.pathname === "/models",
-            }
-          )}
-        >
-          <Icons.Model className="h-6 w-6 stroke-semantic-fg-primary" />
-          <p className="stroke-semantic-fg-primary product-body-text-2-semibold">
-            Models
-          </p>
-        </Link>
-        <Link
+          icon={
+            <Icons.Database01 className="h-6 w-6 stroke-semantic-fg-primary" />
+          }
+          name="Sources"
+          hightlighted={router.pathname.split("/")[1] === "sources"}
+          className="pl-12 pr-3"
+        />
+        <SidebarLink
+          href="/ai"
+          icon={<Icons.Model className="h-6 w-6 stroke-semantic-fg-primary" />}
+          name="AI"
+          hightlighted={router.pathname.split("/")[1] === "ai"}
+          className="pl-12 pr-3"
+        />
+        <SidebarLink
           href="/destinations"
-          className={cn(
-            "flex flex-row items-center space-x-2 rounded-xs border border-transparent py-2 pl-12 pr-3 hover:bg-semantic-bg-base-bg",
-            {
-              "!border-semantic-accent-default border-opacity-100 bg-semantic-accent-bg":
-                router.pathname === "/destinations",
-            }
-          )}
-        >
-          <Icons.Box className="h-6 w-6 stroke-semantic-fg-primary" />
-          <p className="stroke-semantic-fg-primary product-body-text-2-semibold">
-            Destinations
-          </p>
-        </Link>
+          icon={<Icons.Box className="h-6 w-6 stroke-semantic-fg-primary" />}
+          name="Destinations"
+          hightlighted={router.pathname.split("/")[1] === "destinations"}
+          className="pl-12 pr-3"
+        />
       </Collapsible.Content>
     </Collapsible.Root>
   );
