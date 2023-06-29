70f2beb3024460807d3f7943a55cbbdc6b09f5d9
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2023-06-29 16:12:32 +0800

    chore: clean up debug code

--- src/pages/sources/index.tsx
+++ src/pages/sources/index.tsx
@@ -1,13 +1,9 @@
-import { FC, ReactElement, useEffect } from "react";
+import { FC, ReactElement } from "react";
 import {
   useConnectorsWithPipelines,
   SourcesTable,
   useCreateUpdateDeleteResourceGuard,
   useWatchConnectors,
-  Nullable,
-  createInstillAxiosClient,
-  Connector,
-  getQueryString,
 } from "@instill-ai/toolkit";
 
 import { PageTitle, PageHead, Topbar, Sidebar, PageBase } from "@/components";
@@ -31,12 +27,6 @@ const SourcePage: FC & {
     accessToken: null,
   });
 
-  console.log(
-    sources.data,
-    sources.isSuccess,
-    sources.isSuccess ? sources.data.map((source) => source.name) : []
-  );
-
   const sourcesWatchState = useWatchConnectors({
     connectorType: "CONNECTOR_TYPE_SOURCE",
     enabled: sources.isSuccess,
@@ -50,17 +40,6 @@ const SourcePage: FC & {
     sources.isLoading || (sources.isSuccess && sources.data.length > 0)
       ? sourcesWatchState.isLoading
       : false;
-
-  // useEffect(() => {
-  //   listConnectorsQuery({
-  //     pageSize: 10,
-  //     nextPageToken: null,
-  //     accessToken: null,
-  //     filter: null,
-  //   }).then((res) => {
-  //     console.log(res);
-  //   });
-  // }, []);
 
   /* -------------------------------------------------------------------------
    * Render
@@ -104,52 +83,3 @@ SourcePage.getLayout = (page) => {
     </PageBase>
   );
 };
-
-export type ListConnectorsResponse = {
-  connectors: Connector[];
-  next_page_token: string;
-  total_size: string;
-};
-
-export async function listConnectorsQuery({
-  pageSize,
-  nextPageToken,
-  accessToken,
-  filter,
-}: {
-  pageSize: Nullable<number>;
-  nextPageToken: Nullable<string>;
-  accessToken: Nullable<string>;
-  filter: Nullable<string>;
-}) {
-  try {
-    const client = createInstillAxiosClient(accessToken, "vdp");
-    const connectors: Connector[] = [];
-
-    const queryString = getQueryString(
-      `/connectors?view=VIEW_BASIC`,
-      pageSize,
-      nextPageToken,
-      filter
-    );
-
-    const { data } = await client.get<ListConnectorsResponse>(queryString);
-
-    connectors.push(...data.connectors);
-
-    if (data.next_page_token) {
-      connectors.push(
-        ...(await listConnectorsQuery({
-          pageSize,
-          accessToken,
-          nextPageToken: data.next_page_token,
-          filter,
-        }))
-      );
-    }
-
-    return Promise.resolve(connectors);
-  } catch (err) {
-    return Promise.reject(err);
-  }
-}
