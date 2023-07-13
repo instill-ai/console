c3c0ca31eb39b291bc0bb8ee6fc151c93592acf1
Naman Anand <iamnamananand996@gmail.com>
2023-07-13 14:23:00 +0530

    feat: namananand/ins 1280 consume new component from design system to console (#467)
    Because
    
    - consume new component from design system to console
    
    This commit
    
    - new component from design system to console

--- src/components/badge/Badge.tsx
+++ src/components/badge/Badge.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import cn from "clsx";
-import { Icons } from "@instill-ai/design-system";
+import { Icons, Tag } from "@instill-ai/design-system";
 import { getColor } from "@/lib/dashboard";
 
 type BadgeProps = {
@@ -13,28 +13,17 @@ export const Badge = (props: BadgeProps) => {
   const badgeColor = getColor(statusname);
 
   return (
-    <div className="BadgeWrap inline-flex flex-col items-start justify-start pb-2">
-      <div
-        className={cn(
-          `Tags inline-flex h-5 w-auto items-center justify-start gap-1 rounded-[100px] px-2 py-0.5 ${badgeColor.bgColor}`
-        )}
-      >
-        {change > 0 ? (
-          <Icons.ArrowUp className={cn(`h-4 w-4 stroke-${badgeColor.icon}`)} />
-        ) : (
-          <Icons.ArrowDown
-            className={cn(`h-4 w-4 stroke-${badgeColor.icon}`)}
-          />
-        )}
-
-        <div
-          className={cn(
-            `Label text-[12px] font-medium leading-none ${badgeColor.textColor}`
-          )}
-        >
-          {change}%
-        </div>
-      </div>
-    </div>
+    <Tag variant={badgeColor.variant} size="sm">
+      {change > 0 ? (
+        <Icons.ArrowUp
+          className={cn(`h-3 w-3 stroke-${badgeColor.iconColor}`)}
+        />
+      ) : (
+        <Icons.ArrowDown
+          className={cn(`h-3 w-3 stroke-${badgeColor.iconColor}`)}
+        />
+      )}
+      {change}%
+    </Tag>
   );
 };
--- src/components/cells/StateCell.tsx
+++ src/components/cells/StateCell.tsx
@@ -1,4 +1,5 @@
 import { getColor } from "@/lib/dashboard";
+import { Tag } from "@instill-ai/design-system";
 import { Nullable, ResourceState, StateIcon } from "@instill-ai/toolkit";
 import cn from "clsx";
 
@@ -13,23 +14,9 @@ export const StateCell = ({ state, width, name, padding }: StateCellProps) => {
   const badgeColor = getColor(state);
   return (
     <td>
-      <div className={cn("flex flex-row gap-x-2", width, padding)}>
-        <div className="flex">
-          <div
-            className={cn(
-              `Tags inline-flex h-5 items-center justify-start gap-1 rounded-[100px] ${badgeColor.bgColor} px-2 py-0.5`
-            )}
-          >
-            <div
-              className={cn(
-                `Label text-[12px] font-medium leading-none ${badgeColor.textColor}`
-              )}
-            >
-              {badgeColor.stateLabelName}
-            </div>
-          </div>
-        </div>
-      </div>
+      <Tag variant={badgeColor.variant} size={"sm"}>
+        {badgeColor.label}
+      </Tag>
     </td>
   );
 };
