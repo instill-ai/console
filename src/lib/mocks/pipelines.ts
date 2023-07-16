181d27bca97f608627a9b347d5708ebc2af85443
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2023-07-16 19:34:22 +0800

    refactor: remove unused components

--- src/lib/mocks/pipelines.ts
+++ src/lib/mocks/pipelines.ts
@@ -1,6 +1,6 @@
-import { PipelineTrigger } from "@/types";
+import { PipelineTriggerRecord } from "@/types";
 
-export const pipelines: PipelineTrigger[] = [
+export const pipelines: PipelineTriggerRecord[] = [
   {
     trigger_time: "2023-06-22T17:25:19Z",
     pipeline_trigger_id: "4b31a2b5-2c9a-4706-96f5-c347a9b06274",
@@ -8,6 +8,6 @@ export const pipelines: PipelineTrigger[] = [
     pipeline_uid: "9b841358-da64-44ab-8759-b8b42022a5fb",
     pipeline_mode: "MODE_ASYNC",
     compute_time_duration: 7.340086,
-    status: "completed",
+    status: "STATUS_COMPLETED",
   },
 ];
