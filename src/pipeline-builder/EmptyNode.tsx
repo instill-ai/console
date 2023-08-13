1d3eb6d338e7b2ab1db6261c6ad308fd3e4a7a69
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2023-08-14 00:18:08 +0200

    feat: update node styles

--- src/pipeline-builder/EmptyNode.tsx
+++ src/pipeline-builder/EmptyNode.tsx
@@ -1,6 +1,20 @@
-import { NodeProps } from "reactflow";
+import { Handle, NodeProps, Position } from "reactflow";
 import { NodeData } from "./type";
+import { Icons } from "@instill-ai/design-system";
 
 export const EmptyNode = ({ data, id }: NodeProps<NodeData>) => {
-  return <div></div>;
+  return (
+    <>
+      <div className="flex rounded-sm border border-dashed border-semantic-accent-default bg-semantic-accent-bg px-16 py-2">
+        <div className="my-auto flex flex-row gap-x-2 px-2 py-1.5">
+          <p className="text-semantic-accent-default product-button-button-3">
+            Add Connector
+          </p>
+          <Icons.Plus className="h-3 w-3 stroke-semantic-accent-default" />
+        </div>
+      </div>
+      <Handle type="target" position={Position.Left} id={id} />
+      <Handle type="source" position={Position.Right} id={id} />
+    </>
+  );
 };
