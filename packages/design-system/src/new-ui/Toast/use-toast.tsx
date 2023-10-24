e62e0220555982583e54cb492d8b22a95b6f54bd
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2023-10-24 20:02:21 +0800

    chore: expose Toast related function return type

--- packages/design-system/src/new-ui/Toast/use-toast.tsx
+++ packages/design-system/src/new-ui/Toast/use-toast.tsx
@@ -140,7 +140,13 @@ function dispatch(action: Action) {
 
 type Toast = Omit<ToasterToast, "id">;
 
-function toast({ ...props }: Toast) {
+export type ToastReturn = {
+  id: string;
+  dismiss: () => void;
+  update: (props: ToasterToast) => void;
+};
+
+function toast({ ...props }: Toast): ToastReturn {
   const id = genId();
 
   const update = (props: ToasterToast) =>
@@ -170,7 +176,12 @@ function toast({ ...props }: Toast) {
   };
 }
 
-function useToast() {
+export type UseToastReturn = State & {
+  toast: (props: Toast) => ToastReturn;
+  dismiss: (toastId?: string) => void;
+};
+
+function useToast(): UseToastReturn {
   const [state, setState] = React.useState<State>(memoryState);
 
   React.useEffect(() => {
