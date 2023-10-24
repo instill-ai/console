c80429cbd5f0dd9fb13f2994d433c7759ff011d1
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2023-10-24 13:45:24 +0800

    chore: move SuperRefineRules into lib/types/zod

---
+++ packages/toolkit/src/lib/type/zod.ts
@@ -0,0 +1,17 @@
+export type SuperRefineRule = {
+  key: string;
+  validator: (value: any) => SuperRefineRuleValidatorReturn;
+};
+
+export type SuperRefineRuleValidatorReturn =
+  | SuperRefineRuleValidatorValidReturn
+  | SuperRefineRuleValidatorInvalidReturn;
+
+export type SuperRefineRuleValidatorValidReturn = {
+  valid: true;
+};
+
+export type SuperRefineRuleValidatorInvalidReturn = {
+  valid: false;
+  error: string;
+};
