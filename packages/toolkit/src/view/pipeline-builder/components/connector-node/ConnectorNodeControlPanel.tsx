49eb6218e417d7e3303d4c3f5edcbe4734932ded
Po-Chun Chiu <57251712+EiffelFly@users.noreply.github.com>
2023-10-26 22:10:42 +0800

    feat(pipeline-builder): User can collapse/expand the component (#634)
    Because
    
    - User can collapse/expand the component
    
    This commit
    
    - User can collapse/expand the component

--- packages/toolkit/src/view/pipeline-builder/components/connector-node/ConnectorNodeControlPanel.tsx
+++ packages/toolkit/src/view/pipeline-builder/components/connector-node/ConnectorNodeControlPanel.tsx
@@ -7,12 +7,16 @@ export const ConnectorNodeControlPanel = ({
   handleDeleteNode,
   handleCopyNode,
   handleEditNode,
+  nodeIsCollapsed,
+  setNodeIsCollapsed,
   testModeEnabled,
 }: {
   componentType: PipelineComponentType;
   handleDeleteNode: () => void;
   handleCopyNode: () => void;
   handleEditNode: () => void;
+  nodeIsCollapsed: boolean;
+  setNodeIsCollapsed: React.Dispatch<React.SetStateAction<boolean>>;
   testModeEnabled: boolean;
 }) => {
   const [moreOptionsIsOpen, setMoreOptionsIsOpen] = React.useState(false);
@@ -36,6 +40,45 @@ export const ConnectorNodeControlPanel = ({
 
   return (
     <div className="flex flex-row gap-x-3">
+      <Tooltip.Provider>
+        <Tooltip.Root>
+          <Tooltip.Trigger asChild>
+            {/* 
+              eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex
+            */}
+            <span className="flex" tabIndex={0}>
+              <Button
+                className="!px-1 !py-1 !my-auto hover:!bg-semantic-bg-secondary"
+                variant="tertiaryGrey"
+                size="sm"
+                disabled={testModeEnabled}
+                onClick={(e) => {
+                  e.stopPropagation();
+                  setNodeIsCollapsed(!nodeIsCollapsed);
+                }}
+              >
+                {nodeIsCollapsed ? (
+                  <Icons.Plus className="w-4 h-4 stroke-semantic-fg-secondary" />
+                ) : (
+                  <Icons.Minus className="w-4 h-4 stroke-semantic-fg-secondary" />
+                )}
+              </Button>
+            </span>
+          </Tooltip.Trigger>
+          <Tooltip.Portal>
+            <Tooltip.Content className="!px-3 !py-2 rounded-sm !product-body-text-4-semibold bg-semantic-bg-primary">
+              {`${nodeIsCollapsed ? "Expand" : "Collapse"} component`}
+              <Tooltip.Arrow
+                className="fill-semantic-bg-primary"
+                offset={10}
+                width={9}
+                height={6}
+              />
+            </Tooltip.Content>
+          </Tooltip.Portal>
+        </Tooltip.Root>
+      </Tooltip.Provider>
+
       {/* 
         Control - open right panel
       */}
