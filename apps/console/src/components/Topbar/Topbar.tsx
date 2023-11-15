51cd8a83db9458e779dc71264e96a3830dd2e15f
Naman Anand <iamnamananand996@gmail.com>
2023-11-16 00:51:18 +0530

    chore: add profile page

--- apps/console/src/components/Topbar/Topbar.tsx
+++ apps/console/src/components/Topbar/Topbar.tsx
@@ -107,105 +107,111 @@ export const Topbar = (props: TopbarProps) => {
         <div className="h-10 w-10 rounded-lg my-auto flex items-center justify-center cursor-pointer">
           <Icons.SearchLg className="h-5 w-5 stroke-slate-500" />
         </div>
-        <DropdownMenu.Root>
-          <DropdownMenu.Trigger asChild>
-            <div className="h-10 w-10 rounded-lg bg-semantic-bg-secondary my-auto flex items-center justify-center cursor-pointer">
-              <Icons.User02 className="h-5 w-5 stroke-slate-500" />
-            </div>
-          </DropdownMenu.Trigger>
-          <DropdownMenu.Content
-            className="w-[240px] !rounded-sm !px-0 !py-2"
-            side="bottom"
-            align="end"
-          >
-            <DropdownMenu.Item className="!px-3 !py-2.5 !product-body-text-3-medium cursor-pointer gap-x-3 my-auto hover:!text-black">
+
+        {user.isSuccess ? (
+          <DropdownMenu.Root>
+            <DropdownMenu.Trigger asChild>
               <div className="h-10 w-10 rounded-lg bg-semantic-bg-secondary my-auto flex items-center justify-center cursor-pointer">
                 <Icons.User02 className="h-5 w-5 stroke-slate-500" />
               </div>
-              <div className="flex flex-col">
-                <h3 className="product-body-text-3-medium">User Name</h3>
-                <p className="text-semantic-fg-secondary product-body-text-4-regular">
-                  dani.sosa@instill.tech
-                </p>
-              </div>
-            </DropdownMenu.Item>
-            <Separator orientation="horizontal" />
-            <DropdownMenu.Item className="!px-3 !py-2.5 !product-body-text-3-medium cursor-pointer hover:!text-black">
-              <Link href="/settings" className="flex gap-x-2">
-                <Icons.User02 className="h-4 w-4 stroke-semantic-fg-disabled my-auto" />
-                <div className="my-auto">View profile</div>
-              </Link>
-            </DropdownMenu.Item>
-            <DropdownMenu.Item className="!px-3 !py-2.5 !product-body-text-3-medium cursor-pointer hover:!text-black">
-              <Link href="/settings" className="flex gap-x-2">
-                <Icons.Gear01 className="h-4 w-4 stroke-semantic-fg-disabled my-auto" />
-                <div className="my-auto">Settings</div>
-              </Link>
-            </DropdownMenu.Item>
-            <Separator orientation="horizontal" />
-            <DropdownMenu.Item className="!px-3 !py-2.5 !product-body-text-3-medium cursor-pointer my-auto hover:!text-black">
-              <div className="flex flex-row gap-x-2">
-                <div className="my-auto">
-                  <Icons.Plus className="h-4 w-4 stroke-semantic-fg-disabled" />
+            </DropdownMenu.Trigger>
+            <DropdownMenu.Content
+              className="w-[240px] !rounded-sm !px-0 !py-2"
+              side="bottom"
+              align="end"
+            >
+              <DropdownMenu.Item className="!px-3 !py-2.5 !product-body-text-3-medium cursor-pointer gap-x-3 my-auto hover:!text-black">
+                <div className="h-10 w-10 rounded-lg bg-semantic-bg-secondary my-auto flex items-center justify-center cursor-pointer">
+                  <Icons.User02 className="h-5 w-5 stroke-slate-500" />
                 </div>
-                <div className="my-auto">Create Organisation</div>
-                <div className="my-auto">
-                  <Tag variant="lightBlue" size="sm">
-                    Team
-                  </Tag>
+                <div className="flex flex-col">
+                  <h3 className="product-body-text-3-medium">User Name</h3>
+                  <p className="text-semantic-fg-secondary product-body-text-4-regular">
+                    dani.sosa@instill.tech
+                  </p>
                 </div>
-              </div>
-            </DropdownMenu.Item>
-            <Separator orientation="horizontal" />
-            <DropdownMenu.Item className="!px-3 !py-2.5 !product-body-text-3-medium cursor-pointer gap-x-2 my-auto hover:!text-black">
-              <Icons.LayersTwo01 className="h-4 w-4 stroke-semantic-fg-disabled" />
-              Changelog
-            </DropdownMenu.Item>
-            <DropdownMenu.Item className="!px-3 !py-2.5 !product-body-text-3-medium cursor-pointer hover:!text-black">
-              <Link
-                href="https://github.com/instill-ai/community"
-                className="flex gap-x-2"
-              >
-                <Icons.MessageSmileSquare className="h-4 w-4 stroke-semantic-fg-disabled my-auto" />
-                <div className="my-auto">Community</div>
-              </Link>
-            </DropdownMenu.Item>
-            <DropdownMenu.Item className="!px-3 !py-2.5 !product-body-text-3-medium cursor-pointer gap-x-2 my-auto hover:!text-black">
-              <Icons.HelpCircle className="h-4 w-4 stroke-semantic-fg-disabled" />
-              Support
-            </DropdownMenu.Item>
-            <DropdownMenu.Item className="!px-3 !py-2.5 !product-body-text-3-medium cursor-pointer hover:!text-black">
-              <Link
-                href="https://www.instill.tech/docs"
-                className="gap-x-2 flex"
-              >
-                <Icons.File05 className="h-4 w-4 stroke-semantic-fg-disabled my-auto" />
-                <div className="my-auto">Documentation</div>
-              </Link>
-            </DropdownMenu.Item>
-            <DropdownMenu.Item className="!px-3 !py-2.5 !product-body-text-3-medium cursor-pointer hover:!text-black">
-              <Link
-                href="https://github.com/instill-ai/vdp"
-                className="flex gap-x-2"
-              >
-                <GitHubIcon
-                  color="fill-semantic-fg-disabled"
-                  height="h-[20px]"
-                  position="my-auto"
-                  width="w-[20px]"
-                />
-                <div className="my-auto">Github</div>
-              </Link>
-            </DropdownMenu.Item>
-            <Separator orientation="horizontal" />
-            <DropdownMenu.Item className="!px-3 !py-2.5 !product-body-text-3-medium cursor-pointer hover:!text-black">
-              <Link href="/logout" className="flex gap-x-2">
-                <Icons.Logout01 className=" my-auto h-4 w-4 stroke-semantic-fg-disabled" />
-                <div className="my-auto">Log out</div>
-              </Link>
-            </DropdownMenu.Item>
-          </DropdownMenu.Content>
-        </DropdownMenu.Root>
+              </DropdownMenu.Item>
+              <Separator orientation="horizontal" />
+              <DropdownMenu.Item className="!px-3 !py-2.5 !product-body-text-3-medium cursor-pointer hover:!text-black">
+                <Link
+                  href={`/${user.data.id}/profile`}
+                  className="flex gap-x-2"
+                >
+                  <Icons.User02 className="h-4 w-4 stroke-semantic-fg-disabled my-auto" />
+                  <div className="my-auto">View profile</div>
+                </Link>
+              </DropdownMenu.Item>
+              <DropdownMenu.Item className="!px-3 !py-2.5 !product-body-text-3-medium cursor-pointer hover:!text-black">
+                <Link href="/settings" className="flex gap-x-2">
+                  <Icons.Gear01 className="h-4 w-4 stroke-semantic-fg-disabled my-auto" />
+                  <div className="my-auto">Settings</div>
+                </Link>
+              </DropdownMenu.Item>
+              <Separator orientation="horizontal" />
+              <DropdownMenu.Item className="!px-3 !py-2.5 !product-body-text-3-medium cursor-pointer my-auto hover:!text-black">
+                <div className="flex flex-row gap-x-2">
+                  <div className="my-auto">
+                    <Icons.Plus className="h-4 w-4 stroke-semantic-fg-disabled" />
+                  </div>
+                  <div className="my-auto">Create Organisation</div>
+                  <div className="my-auto">
+                    <Tag variant="lightBlue" size="sm">
+                      Team
+                    </Tag>
+                  </div>
+                </div>
+              </DropdownMenu.Item>
+              <Separator orientation="horizontal" />
+              <DropdownMenu.Item className="!px-3 !py-2.5 !product-body-text-3-medium cursor-pointer gap-x-2 my-auto hover:!text-black">
+                <Icons.LayersTwo01 className="h-4 w-4 stroke-semantic-fg-disabled" />
+                Changelog
+              </DropdownMenu.Item>
+              <DropdownMenu.Item className="!px-3 !py-2.5 !product-body-text-3-medium cursor-pointer hover:!text-black">
+                <Link
+                  href="https://github.com/instill-ai/community"
+                  className="flex gap-x-2"
+                >
+                  <Icons.MessageSmileSquare className="h-4 w-4 stroke-semantic-fg-disabled my-auto" />
+                  <div className="my-auto">Community</div>
+                </Link>
+              </DropdownMenu.Item>
+              <DropdownMenu.Item className="!px-3 !py-2.5 !product-body-text-3-medium cursor-pointer gap-x-2 my-auto hover:!text-black">
+                <Icons.HelpCircle className="h-4 w-4 stroke-semantic-fg-disabled" />
+                Support
+              </DropdownMenu.Item>
+              <DropdownMenu.Item className="!px-3 !py-2.5 !product-body-text-3-medium cursor-pointer hover:!text-black">
+                <Link
+                  href="https://www.instill.tech/docs"
+                  className="gap-x-2 flex"
+                >
+                  <Icons.File05 className="h-4 w-4 stroke-semantic-fg-disabled my-auto" />
+                  <div className="my-auto">Documentation</div>
+                </Link>
+              </DropdownMenu.Item>
+              <DropdownMenu.Item className="!px-3 !py-2.5 !product-body-text-3-medium cursor-pointer hover:!text-black">
+                <Link
+                  href="https://github.com/instill-ai/vdp"
+                  className="flex gap-x-2"
+                >
+                  <GitHubIcon
+                    color="fill-semantic-fg-disabled"
+                    height="h-[20px]"
+                    position="my-auto"
+                    width="w-[20px]"
+                  />
+                  <div className="my-auto">Github</div>
+                </Link>
+              </DropdownMenu.Item>
+              <Separator orientation="horizontal" />
+              <DropdownMenu.Item className="!px-3 !py-2.5 !product-body-text-3-medium cursor-pointer hover:!text-black">
+                <Link href="/logout" className="flex gap-x-2">
+                  <Icons.Logout01 className=" my-auto h-4 w-4 stroke-semantic-fg-disabled" />
+                  <div className="my-auto">Log out</div>
+                </Link>
+              </DropdownMenu.Item>
+            </DropdownMenu.Content>
+          </DropdownMenu.Root>
+        ) : null}
       </div>
     </div>
   );
