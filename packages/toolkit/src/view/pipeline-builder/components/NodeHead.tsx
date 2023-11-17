78670a72c6ce4c1f19dcd91517d7fa33a9b87564
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2023-11-17 16:43:59 +0800

    feat: add NodeHead

---
+++ packages/toolkit/src/view/pipeline-builder/components/NodeHead.tsx
@@ -0,0 +1,16 @@
+import cn from "clsx";
+import * as React from "react";
+
+export const NodeHead = ({
+  nodeIsCollapsed,
+  children,
+}: {
+  nodeIsCollapsed: boolean;
+  children: React.ReactNode;
+}) => {
+  return (
+    <div className={cn("flex w-full flex-row", { "mb-6": !nodeIsCollapsed })}>
+      {children}
+    </div>
+  );
+};
