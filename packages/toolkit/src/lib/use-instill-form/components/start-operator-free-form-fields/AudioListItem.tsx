a5b0d1498aefa2e4c3e24bbf288cb90911607af7
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2023-11-23 20:44:03 +0800

    feat: update audio list item

--- packages/toolkit/src/lib/use-instill-form/components/start-operator-free-form-fields/AudioListItem.tsx
+++ packages/toolkit/src/lib/use-instill-form/components/start-operator-free-form-fields/AudioListItem.tsx
@@ -1,8 +1,15 @@
 import * as React from "react";
 import * as Progress from "@radix-ui/react-progress";
 import { Nullable } from "../../../type";
+import { Icons } from "@instill-ai/design-system";
 
-export const AudioListItem = ({ file }: { file: Nullable<File> }) => {
+export const AudioListItem = ({
+  file,
+  onDelete,
+}: {
+  file: Nullable<File>;
+  onDelete: () => void;
+}) => {
   const [progress, setProgress] = React.useState(0);
 
   if (!file) {
@@ -31,7 +38,28 @@ export const AudioListItem = ({ file }: { file: Nullable<File> }) => {
           style={{ transform: `translateX(-${100 - progress}%)` }}
         />
       </Progress.Root>
-      <div className="absolute bottom-0 left-0 right-0 top-0 flex flex-row"></div>
+      <div className="absolute bottom-0 left-0 right-0 top-0 flex flex-row">
+        <Icons.Recording03 className="mr-2 h-5 w-5 stroke-semantic-fg-secondary" />
+        <p className="w-[180px] truncate text-semantic-fg-primary product-body-text-3-regular">
+          {file.name}
+        </p>
+        <div className="ml-auto flex flex-row">
+          <button
+            onClick={() => onDelete()}
+            className="hover:bg-semantic-bg-secondary"
+            type="button"
+          >
+            <Icons.X className="h-4 w-4 stroke-semantic-fg-secondary" />
+          </button>
+          <button
+            onClick={() => onDelete()}
+            className="hover:bg-semantic-bg-secondary"
+            type="button"
+          >
+            <Icons.X className="h-4 w-4 stroke-semantic-fg-secondary" />
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
