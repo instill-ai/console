cd9b5cf66544fc9dd638987e3e66cb8184e5d018
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2023-11-24 00:21:26 +0800

    feat: add start input types icon

---
+++ packages/design-system/src/new-ui/Icons/FilePlus02.tsx
@@ -0,0 +1,25 @@
+import * as React from "react";
+import { IconBase, IconBaseProps } from "./IconBase";
+
+export const FilePlus02 = React.forwardRef<
+  SVGSVGElement,
+  Omit<IconBaseProps, "viewBox" | "children">
+>((props, ref) => {
+  const { className, ...passThrough } = props;
+  return (
+    <IconBase
+      {...passThrough}
+      ref={ref}
+      viewBox="0 0 24 24"
+      className={className}
+    >
+      <path
+        d="M20 10.5V6.8C20 5.11984 20 4.27976 19.673 3.63803C19.3854 3.07354 18.9265 2.6146 18.362 2.32698C17.7202 2 16.8802 2 15.2 2H8.8C7.11984 2 6.27976 2 5.63803 2.32698C5.07354 2.6146 4.6146 3.07354 4.32698 3.63803C4 4.27976 4 5.11984 4 6.8V17.2C4 18.8802 4 19.7202 4.32698 20.362C4.6146 20.9265 5.07354 21.3854 5.63803 21.673C6.27976 22 7.11984 22 8.8 22H12M14 11H8M10 15H8M16 7H8M18 21V15M15 18H21"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </IconBase>
+  );
+});
+FilePlus02.displayName = "IconFilePlus02";
