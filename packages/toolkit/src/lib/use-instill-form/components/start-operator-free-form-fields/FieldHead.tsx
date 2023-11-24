32e46ef8d55dd2f72baa10c8ab737d1d160b1a65
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2023-11-24 15:10:07 +0800

    feat: user can be better hinted when building field on start operator

--- packages/toolkit/src/lib/use-instill-form/components/start-operator-free-form-fields/FieldHead.tsx
+++ packages/toolkit/src/lib/use-instill-form/components/start-operator-free-form-fields/FieldHead.tsx
@@ -1,6 +1,6 @@
 import { useShallow } from "zustand/react/shallow";
 import { InstillStore, Nullable, useInstillStore } from "../../..";
-import { Icons, Tooltip } from "@instill-ai/design-system";
+import { Icons, Tag, Tooltip } from "@instill-ai/design-system";
 
 const selector = (store: InstillStore) => ({
   isOwner: store.isOwner,
@@ -21,32 +21,92 @@ export const FieldHead = ({
   const { isOwner, currentVersion } = useInstillStore(useShallow(selector));
   return (
     <div className="flex flex-row items-center justify-between">
-      <Tooltip.Provider>
-        <Tooltip.Root>
-          <Tooltip.Trigger asChild>
-            <div className="my-auto line-clamp-1 w-1/2 font-sans text-sm font-semibold text-semantic-fg-primary">
-              {title}
-            </div>
-          </Tooltip.Trigger>
-          <Tooltip.Portal>
-            <Tooltip.Content
-              sideOffset={5}
-              side="top"
-              className="max-w-[300px] rounded-sm !border-none bg-semantic-bg-primary !px-3 !py-2"
-            >
-              <p className="break-words text-semantic-fg-secondary !product-body-text-4-semibold">
+      <div className="flex w-1/2 flex-1 flex-row gap-x-2">
+        <Tooltip.Provider>
+          <Tooltip.Root>
+            <Tooltip.Trigger asChild>
+              <div className="my-auto line-clamp-1 max-w-[120px] font-sans text-sm font-semibold text-semantic-fg-primary">
                 {title}
-              </p>
-              <Tooltip.Arrow
-                className="fill-semantic-bg-primary"
-                offset={10}
-                width={9}
-                height={6}
-              />
-            </Tooltip.Content>
-          </Tooltip.Portal>
-        </Tooltip.Root>
-      </Tooltip.Provider>
+              </div>
+            </Tooltip.Trigger>
+            <Tooltip.Portal>
+              <Tooltip.Content
+                sideOffset={5}
+                side="top"
+                className="max-w-[300px] rounded-sm !border-none bg-semantic-bg-primary !px-3 !py-2"
+              >
+                <p className="break-words text-semantic-fg-secondary !product-body-text-4-semibold">
+                  {title}
+                </p>
+                <Tooltip.Arrow
+                  className="fill-semantic-bg-primary"
+                  offset={10}
+                  width={9}
+                  height={6}
+                />
+              </Tooltip.Content>
+            </Tooltip.Portal>
+          </Tooltip.Root>
+        </Tooltip.Provider>
+        <Tooltip.Provider>
+          <Tooltip.Root>
+            <Tooltip.Trigger asChild>
+              <Icons.HelpCircle className="my-auto h-[14px] w-[14px] cursor-pointer stroke-semantic-fg-secondary" />
+            </Tooltip.Trigger>
+            <Tooltip.Portal>
+              <Tooltip.Content
+                sideOffset={5}
+                side="top"
+                className="w-[300px] rounded-sm !border-none bg-semantic-bg-primary !px-3 !py-2"
+              >
+                <div className="flex flex-col gap-y-2">
+                  <div className="flex flex-row gap-x-2">
+                    <div className="pt-0.5">
+                      <Icons.InfoCircle className="mb-auto h-4 w-4 stroke-semantic-fg-secondary" />
+                    </div>
+                    <div className="flex flex-col gap-y-2">
+                      <p className="m-auto text-semantic-fg-secondary product-body-text-3-semibold">
+                        You can use{" "}
+                        <Tag
+                          variant="lightBlue"
+                          size="sm"
+                          className="!rounded !px-2 !py-0.5"
+                        >{`{${path}}`}</Tag>{" "}
+                        to reference this field&rsquo;s value.
+                      </p>
+                    </div>
+                  </div>
+                  <div className="flex flex-row gap-x-2">
+                    <div className="pt-0.5">
+                      <Icons.InfoCircle className="mb-auto h-4 w-4 stroke-semantic-fg-secondary" />
+                    </div>
+                    <div className="flex flex-col gap-y-2">
+                      <p className="m-auto text-semantic-fg-secondary product-body-text-3-semibold">
+                        You can also use{" "}
+                        <Tag
+                          variant="lightBlue"
+                          size="sm"
+                          className="!rounded !px-2 !py-0.5"
+                        >{`{{${path}}}`}</Tag>{" "}
+                        to build the template which use this value. For example
+                      </p>
+                      <p className="rounded border border-semantic-bg-line bg-semantic-bg-base-bg px-2 py-1 text-semantic-fg-secondary product-body-text-3-regular">
+                        {`This is a template use this value, {{start.${title}}}`}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <Tooltip.Arrow
+                  className="fill-semantic-bg-primary"
+                  offset={10}
+                  width={9}
+                  height={6}
+                />
+              </Tooltip.Content>
+            </Tooltip.Portal>
+          </Tooltip.Root>
+        </Tooltip.Provider>
+      </div>
       {currentVersion === "latest" && isOwner ? (
         <div className="my-auto flex flex-row gap-x-2">
           <button
