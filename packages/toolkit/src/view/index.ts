4b284afd00b38b77827e7fa193a7ed19296b6372
Naman Anand <iamnamananand996@gmail.com>
2023-11-30 19:16:51 +0530

    chore: remove organization component from toolkit

--- packages/toolkit/src/view/index.ts
+++ packages/toolkit/src/view/index.ts
@@ -5,7 +5,6 @@ export * from "./dashboard";
 export * from "./data";
 export * from "./mgmt";
 export * from "./model";
-export * from "./organizations";
 export * from "./pipeline";
 export * from "./pipeline-builder";
 export * from "./resource";
