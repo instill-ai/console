eab3a8fb5e777fafe9545dbf34585447492d84d0
Naman Anand <iamnamananand996@gmail.com>
2023-12-05 18:51:34 +0530

    chore: add user list query

---
+++ packages/toolkit/src/lib/react-query-service/mgmt/useUsers.ts
@@ -0,0 +1,39 @@
+import { useQuery } from "@tanstack/react-query";
+import { getUserQuery, listUsersQuery, type User } from "../../vdp-sdk";
+import type { Nullable } from "../../type";
+import { env } from "../../utility";
+
+export const useUsers = ({
+  accessToken,
+  enabled,
+  retry,
+}: {
+  accessToken: Nullable<string>;
+  enabled: boolean;
+  /**
+   * - Default is 3
+   * - Set to false to disable retry
+   */
+  retry?: false | number;
+}) => {
+  return useQuery(
+    ["users"],
+    async () => {
+      if (!accessToken) {
+        return Promise.reject(new Error("accessToken not provided"));
+      }
+
+      const users = await listUsersQuery({
+        pageSize: env("NEXT_PUBLIC_QUERY_PAGE_SIZE"),
+        nextPageToken: null,
+        accessToken,
+      });
+
+      return Promise.resolve(users);
+    },
+    {
+      enabled,
+      retry: retry === false ? false : retry ? retry : 3,
+    }
+  );
+};
