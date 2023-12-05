5b5bf88a0b464569fafaaf7113d62cdc434c4775
Naman Anand <iamnamananand996@gmail.com>
2023-12-05 23:29:55 +0530

    chore(design-system): add user plus icon (#768)
    Because
    
    - add user plus icon
    
    This commit
    
    - add user plus icon

---
+++ packages/design-system/src/new-ui/Icons/UserPlus01.tsx
@@ -0,0 +1,25 @@
+import * as React from "react";
+import { IconBase, IconBaseProps } from "./IconBase";
+
+export const UserPlus01 = React.forwardRef<
+  SVGSVGElement,
+  Omit<IconBaseProps, "viewBox" | "children">
+>((props, ref) => {
+  const { className, ...passThrough } = props;
+  return (
+    <IconBase
+      {...passThrough}
+      ref={ref}
+      viewBox="0 0 24 24"
+      className={className}
+    >
+      <path
+        d="M12 15.5H7.5C6.10444 15.5 5.40665 15.5 4.83886 15.6722C3.56045 16.06 2.56004 17.0605 2.17224 18.3389C2 18.9067 2 19.6044 2 21M19 21V15M16 18H22M14.5 7.5C14.5 9.98528 12.4853 12 10 12C7.51472 12 5.5 9.98528 5.5 7.5C5.5 5.01472 7.51472 3 10 3C12.4853 3 14.5 5.01472 14.5 7.5Z"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </IconBase>
+  );
+});
+UserPlus01.displayName = "IconUserPlus01";
