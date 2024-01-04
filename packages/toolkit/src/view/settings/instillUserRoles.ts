785a0b0860c5a63eae4d8804f0f0b29187603f00
Po Chun Chiu <57251712+EiffelFly@users.noreply.github.com>
2024-01-04 19:11:47 +0800

    chore: add Marketing into InstillUserRoles (#861)
    Because
    
    - add Marketing into InstillUserRoles
    
    This commit
    
    - add Marketing into InstillUserRoles

--- packages/toolkit/src/view/settings/instillUserRoles.ts
+++ packages/toolkit/src/view/settings/instillUserRoles.ts
@@ -2,44 +2,12 @@ import { SelectOption } from "@instill-ai/design-system";
 
 export const instillUserRoles: SelectOption[] = [
   {
-    label: "HR & Legal",
-    value: "HR & Legal",
-  },
-  {
-    label: "Product & Design",
-    value: "Product & Design",
-  },
-  {
     label: "Creative Production",
     value: "Creative Production",
   },
   {
     label: "Customer Service",
     value: "Customer Service",
-  },
-  {
-    label: "Operations",
-    value: "Operations",
-  },
-  {
-    label: "Finance",
-    value: "Finance",
-  },
-  {
-    label: "IT & Support",
-    value: "IT & Support",
-  },
-  {
-    label: "Manufacturing",
-    value: "Manufacturing",
-  },
-  {
-    label: "Sales & Account Management",
-    value: "Sales & Account Management",
-  },
-  {
-    label: "Leadership/Management",
-    value: "Leadership/Management",
   },
   {
     label: "Data/AI - AI Researcher",
@@ -64,5 +32,41 @@ export const instillUserRoles: SelectOption[] = [
   {
     label: "Data/AI - Hobbyist",
     value: "Data/AI - Hobbyist",
+  },
+  {
+    label: "Finance",
+    value: "Finance",
+  },
+  {
+    label: "HR & Legal",
+    value: "HR & Legal",
+  },
+  {
+    label: "IT & Support",
+    value: "IT & Support",
+  },
+  {
+    label: "Leadership/Management",
+    value: "Leadership/Management",
+  },
+  {
+    label: "Marketing",
+    value: "Marketing",
+  },
+  {
+    label: "Manufacturing",
+    value: "Manufacturing",
+  },
+  {
+    label: "Operations",
+    value: "Operations",
+  },
+  {
+    label: "Product & Design",
+    value: "Product & Design",
+  },
+  {
+    label: "Sales & Account Management",
+    value: "Sales & Account Management",
   },
 ];
