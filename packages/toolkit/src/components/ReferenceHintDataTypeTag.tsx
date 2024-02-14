0c6ef4eaada75e47af236f30828f01d49ad0b70e
Po Chun Chiu <57251712+EiffelFly@users.noreply.github.com>
2024-02-15 00:18:34 +0800

    feat(pipeline-builder):  implement the new array data type label design (#957)
    Because
    
    - We have new label design for array type. To better visualize the
    different array and non-array type
    
    This commit
    
    -  implement the new array data type label design

--- packages/toolkit/src/components/ReferenceHintDataTypeTag.tsx
+++ packages/toolkit/src/components/ReferenceHintDataTypeTag.tsx
@@ -3,14 +3,32 @@ import cn from "clsx";
 export const ReferenceHintDataTypeTag = ({
   className,
   label,
+  isArray,
 }: {
   className?: string;
   label: string;
+  isArray?: boolean;
 }) => {
-  return (
+  return isArray ? (
     <div
       className={cn(
-        "flex h-5 flex-row items-center gap-x-1 rounded bg-semantic-success-bg px-2 py-1",
+        "flex flex-row gap-x-[3px] rounded bg-[#D1FAED] px-[6px] py-[1.5px]",
+        className
+      )}
+    >
+      <p className="my-auto align-middle font-sans text-[11px] font-medium text-semantic-success-default	">
+        Array
+      </p>
+      <div className="flex flex-row items-center rounded bg-semantic-success-bg	px-[6px] py-[1.5px]">
+        <p className="my-auto align-middle font-sans text-[9px] font-medium text-semantic-success-default">
+          {label}
+        </p>
+      </div>
+    </div>
+  ) : (
+    <div
+      className={cn(
+        "flex h-5 flex-row items-center rounded bg-semantic-success-bg px-2 py-1",
         className
       )}
     >
