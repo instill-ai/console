86dfb2080be6a8cf36cbf8c7f99a5eae108a8f42
Naman Anand <iamnamananand996@gmail.com>
2024-03-27 19:00:32 +0530

    fix: update name

--- packages/toolkit/src/components/top-bar/Topbar.tsx
+++ packages/toolkit/src/components/top-bar/Topbar.tsx
@@ -32,7 +32,7 @@ export const topbarItems = [
   {
     pathName: "models",
     icon: <Icons.Cube01 className="h-6 w-6 stroke-semantic-fg-primary" />,
-    name: "Model",
+    name: "Models",
   },
   {
     pathName: "dashboard",
