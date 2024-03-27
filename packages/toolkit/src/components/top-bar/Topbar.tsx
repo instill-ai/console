ed67569b06ab04ea78d087197274cc93ac1537d0
Naman Anand <iamnamananand996@gmail.com>
2024-03-27 19:34:09 +0530

    fix: docs url and navbar update (#1042)
    Because
    
    - docs url and navbar update
    
    This commit
    
    - docs url and navbar update

--- packages/toolkit/src/components/top-bar/Topbar.tsx
+++ packages/toolkit/src/components/top-bar/Topbar.tsx
@@ -32,7 +32,7 @@ export const topbarItems = [
   {
     pathName: "models",
     icon: <Icons.Cube01 className="h-6 w-6 stroke-semantic-fg-primary" />,
-    name: "Model Hub",
+    name: "Models",
   },
   {
     pathName: "dashboard",
