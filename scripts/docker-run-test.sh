1cc5582522d772f13c3050f71d481e3e6038ad08
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2024-03-28 08:50:49 +0800

    feat: upload test result as artifact

--- scripts/docker-run-test.sh
+++ scripts/docker-run-test.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 # Because we store the script within a shell script file, you need to grant permission like `sudo chmod 755 './scripts/docker-build.sh'` at the root.
 
-docker run --rm -t \
-  --network instill-network \
-  --entrypoint /bin/bash \
-  -e NEXT_PUBLIC_CONSOLE_BASE_URL='http://console:3000' \
+docker build --build-arg TEST_USER='root' -f Dockerfile.playwright -t console-playwright:latest .
+docker run -t --rm \
   -e NEXT_PUBLIC_GENERAL_API_VERSION=v1beta \
   -e NEXT_PUBLIC_MODEL_API_VERSION=v1alpha \
-  -e NEXT_PUBLIC_API_GATEWAY_URL='http://api-gateway:8080' \
-  -e NEXT_SERVER_API_GATEWAY_URL='http://api-gateway:8080' \
-  -e NEXT_PUBLIC_INSTILL_AI_USER_COOKIE_NAME='instill-ai-user' \
-  -e NEXT_PUBLIC_DISABLE_CREATE_UPDATE_DELETE_RESOURCE=false \
-  -e NEXT_PUBLIC_SET_SECURE_COOKIE=false \
-  --name instill-console-integration \
-  instill-console-integration
+  -e NEXT_PUBLIC_CONSOLE_EDITION=local-ce:test \
+  -e NEXT_PUBLIC_CONSOLE_BASE_URL=http://console:3000 \
+  -e NEXT_PUBLIC_BASE_API_GATEWAY_URL=http://api-gateway:8080 \
+  -e NEXT_SERVER_BASE_API_GATEWAY_URL=http://api-gateway:8080 \
+  -e NEXT_PUBLIC_SELF_SIGNED_CERTIFICATION=false \
+  -e NEXT_PUBLIC_INSTILL_AI_USER_COOKIE_NAME=instill-ai-user \
+  --network instill-network \
+  --entrypoint /bin/bash \
+  --name console-integration-test \
+  console-playwright:latest
