daf19433ed16b7df4a245d965f90e019334cab1e
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2024-03-28 11:18:17 +0800

    chore: rollback comment change

--- apps/console/integration-test/main.test.ts
+++ apps/console/integration-test/main.test.ts
@@ -4,5 +4,5 @@ import { shouldUnmarshalJSONInput } from "./tests/should-unmarshal.test";
 import { shouldEditAndCreateStartAndEndOperatorField } from "./tests/should-edit-create-start-end-field.test";
 
 test.describe(shouldChangeComponentID);
-// test.describe(shouldUnmarshalJSONInput);
-// test.describe(shouldEditAndCreateStartAndEndOperatorField);
+test.describe(shouldUnmarshalJSONInput);
+test.describe(shouldEditAndCreateStartAndEndOperatorField);
