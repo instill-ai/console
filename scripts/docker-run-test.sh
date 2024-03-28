e8d0ec2014b029c38d564d9749943961b1385149
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2024-03-28 09:22:49 +0800

    fix: fix artifact

--- scripts/docker-run-test.sh
+++ scripts/docker-run-test.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # Because we store the script within a shell script file, you need to grant permission like `sudo chmod 755 './scripts/docker-build.sh'` at the root.
 
-docker build --build-arg TEST_USER='root' -f Dockerfile.playwright -t console-playwright:latest .
 docker run -t --rm \
   -e NEXT_PUBLIC_GENERAL_API_VERSION=v1beta \
   -e NEXT_PUBLIC_MODEL_API_VERSION=v1alpha \
