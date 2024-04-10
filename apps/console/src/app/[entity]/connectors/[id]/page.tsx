8914128dd761b675cba8c68c4f19915a35cde375
Naman Anand <iamnamananand996@gmail.com>
2024-04-10 13:59:37 +0530

    chore: update

--- apps/console/src/app/[entity]/connectors/[id]/page.tsx
+++ apps/console/src/app/[entity]/connectors/[id]/page.tsx
@@ -1,58 +1,18 @@
-import {
-  fetchNamespaceType,
-  fetchUserConnector,
-} from "@instill-ai/toolkit/server";
 import { ConnectorViewPageRender } from "./render";
 import { Metadata } from "next";
-import { cookies } from "next/headers";
-import { ConnectorWithDefinition, Nullable } from "@instill-ai/toolkit";
 
 type Props = {
   params: { id: string; entity: string };
 };
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
-  const cookieStore = cookies();
-  const authSessionCookie = cookieStore.get("instill-auth-session")?.value;
-
-  let accessToken: Nullable<string> = null;
-
-  if (authSessionCookie) {
-    accessToken = JSON.parse(authSessionCookie).access_token;
-  }
-
-  const entity = params.entity;
-  const id = params.id;
-
-  try {
-    const namespaceType = await fetchNamespaceType({
-      namespace: entity,
-      accessToken,
-    });
-
-    let connector: Nullable<ConnectorWithDefinition> = null;
-
-    connector = await fetchUserConnector({
-      connectorName:
-        namespaceType === "NAMESPACE_USER"
-          ? `users/${entity}/connectors/${id}`
-          : `organizations/${entity}/connectors/${id}`,
-      accessToken,
-    });
-
-    const metadata: Metadata = {
-      title: `Instill Core | ${connector.id}`,
-      description: connector.description,
-      openGraph: {
-        images: ["/instill-open-graph.png"],
-      },
-    };
-
-    return Promise.resolve(metadata);
-  } catch (error) {
-    console.log(error);
-    return Promise.reject(error);
-  }
+  const metadata: Metadata = {
+    title: `Instill Core | ${params.id}`,
+    openGraph: {
+      images: ["/instill-open-graph.png"],
+    },
+  };
+  return Promise.resolve(metadata);
 }
 
 export default async function Page() {
