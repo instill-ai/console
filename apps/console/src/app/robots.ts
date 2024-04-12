6150d2d0cf4aa2e019943d3c0b8eabbb98c40293
Naman Anand <iamnamananand996@gmail.com>
2024-04-12 15:10:45 +0530

    feat: add sitemap for pipelines, users and organisations (#1073)
    Because
    
    - add sitemap for pipelines, users and organisations
    
    This commit
    
    - add sitemap for pipelines, users and organisations

---
+++ apps/console/src/app/robots.ts
@@ -0,0 +1,12 @@
+import { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+      disallow: "/private/",
+    },
+    sitemap: "https://instill.tech/sitemap.xml",
+  };
+}
