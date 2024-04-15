9fedb1fda9387f4c4d15203ba541d8991b6b8b3b
Po Chun Chiu <57251712+EiffelFly@users.noreply.github.com>
2024-04-15 16:36:35 +0800

    fix: fix sitemap generation error block building process (#1082)
    Because
    
    - Sitemap generation un-expectly throw error cause the build process to
    fall
    
    This commit
    
    - fix sitemap generation error block building process

--- apps/console/src/app/sitemap.ts
+++ apps/console/src/app/sitemap.ts
@@ -3,32 +3,42 @@ import { MetadataRoute } from "next";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const sitemaps: MetadataRoute.Sitemap = [];
+  const defaultApiUrl = "https://api.instill.tech";
+  const defaultBaseUrl = "https://instill.tech";
 
-  const pipelinesUrl = `https://api.instill.tech/vdp/v1beta/pipelines?view=VIEW_FULL&page_size=10&visibility=VISIBILITY_PUBLIC`;
+  const pipelinesUrl = `${
+    process.env.NEXT_PUBLIC_API_GATEWAY_URL || defaultApiUrl
+  }/vdp/v1beta/pipelines?page_size=100`;
   let nextToken = "";
   const allPipelines: Pipeline[] = [];
 
   let continueFetching = true;
   while (continueFetching) {
-    const pipelineResponse = await fetch(
-      nextToken ? `${pipelinesUrl}&page_token=${nextToken}` : pipelinesUrl,
-    );
-    if (!pipelineResponse.ok) {
-      throw new Error("Network response was not ok.");
-    }
-    const { pipelines, next_page_token } = await pipelineResponse.json();
-    allPipelines.push(...pipelines);
+    try {
+      const pipelineResponse = await fetch(
+        nextToken ? `${pipelinesUrl}&page_token=${nextToken}` : pipelinesUrl,
+      );
 
-    if (!next_page_token) {
+      if (pipelineResponse.ok) {
+        const { pipelines, next_page_token } = await pipelineResponse.json();
+        allPipelines.push(...pipelines);
+
+        if (!next_page_token) {
+          continueFetching = false;
+        } else {
+          nextToken = next_page_token;
+        }
+      } else {
+        continueFetching = false;
+      }
+    } catch (error) {
       continueFetching = false;
-    } else {
-      nextToken = next_page_token;
     }
   }
 
   allPipelines.map((pipeline: Pipeline) => {
     sitemaps.push({
-      url: `https://instill.tech/${
+      url: `${process.env.NEXT_PUBLIC_CONSOLE_BASE_URL || defaultBaseUrl}/${
         pipeline.owner_name.split("/")[1]
       }/pipelines/${pipeline.id}`,
       lastModified: new Date(),
@@ -37,39 +47,53 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     });
   });
 
-  const usersUrl = `https://api.instill.tech/core/v1beta/users`;
+  const usersUrl = `${
+    process.env.NEXT_PUBLIC_API_GATEWAY_URL || defaultApiUrl
+  }/core/v1beta/users`;
 
-  const userResponse = await fetch(usersUrl);
-  if (!userResponse.ok) {
-    throw new Error("Network response was not ok.");
+  try {
+    const usersResponse = await fetch(usersUrl);
+
+    if (usersResponse.ok) {
+      const { users } = await usersResponse.json();
+
+      users.map((user: User) => {
+        sitemaps.push({
+          url: `${process.env.NEXT_PUBLIC_CONSOLE_BASE_URL}/${user?.id}`,
+          lastModified: new Date(),
+          changeFrequency: "daily",
+          priority: 1,
+        });
+      });
+    }
+  } catch (error) {
+    console.error(error);
   }
-  const { users } = await userResponse.json();
 
-  users.map((user: User) => {
-    sitemaps.push({
-      url: `https://instill.tech/${user?.id}`,
-      lastModified: new Date(),
-      changeFrequency: "daily",
-      priority: 1,
-    });
-  });
+  const organizationsUrl = `${
+    process.env.NEXT_PUBLIC_API_GATEWAY_URL || defaultApiUrl
+  }/core/v1beta/organizations`;
 
-  const organizationsUrl = `https://api.instill.tech/core/v1beta/organizations`;
+  try {
+    const organizationsResponse = await fetch(organizationsUrl);
 
-  const organizationResponse = await fetch(organizationsUrl);
-  if (!organizationResponse.ok) {
-    throw new Error("Network response was not ok.");
+    if (organizationsResponse.ok) {
+      const { organizations } = await organizationsResponse.json();
+
+      organizations.map((organization: Organization) => {
+        sitemaps.push({
+          url: `${process.env.NEXT_PUBLIC_CONSOLE_BASE_URL || defaultBaseUrl}/${
+            organization?.id
+          }`,
+          lastModified: new Date(),
+          changeFrequency: "daily",
+          priority: 1,
+        });
+      });
+    }
+  } catch (error) {
+    console.error(error);
   }
-  const { organizations } = await organizationResponse.json();
-
-  organizations.map((organization: Organization) => {
-    sitemaps.push({
-      url: `https://instill.tech/${organization?.id}`,
-      lastModified: new Date(),
-      changeFrequency: "daily",
-      priority: 1,
-    });
-  });
 
   return sitemaps;
 }
