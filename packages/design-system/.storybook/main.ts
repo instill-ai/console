751e2663995e9965e4ce155953030f4ed65b6f89
Po Chun Chiu <57251712+EiffelFly@users.noreply.github.com>
2024-04-29 16:06:33 +0800

    fix: fix deps that have issues (#1107)
    Because
    
    - This PR fix the issues that are alerted by the deps bot
    
    This commit
    
    - fix deps that have issues

--- packages/design-system/.storybook/main.ts
+++ packages/design-system/.storybook/main.ts
@@ -1,24 +1,28 @@
+import { dirname, join } from "path";
 import type { StorybookConfig } from "@storybook/react-webpack5";
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   staticDirs: ["../public"],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/addon-a11y",
+    getAbsolutePath("@storybook/addon-links"),
+    getAbsolutePath("@storybook/addon-essentials"),
+    getAbsolutePath("@storybook/addon-interactions"),
+    getAbsolutePath("@storybook/addon-a11y"),
+    getAbsolutePath("@chromatic-com/storybook"),
   ],
   framework: {
-    name: "@storybook/react-webpack5",
+    name: getAbsolutePath("@storybook/nextjs"),
     options: {},
   },
   docs: {
     autodocs: true,
   },
-  core: {
-    builder: "@storybook/builder-webpack5",
-  },
+  core: {},
 };
 
 export default config;
+
+function getAbsolutePath(value: string): any {
+  return dirname(require.resolve(join(value, "package.json")));
+}
