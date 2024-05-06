6b5cf757094407e280d6e41b27d5ad6f937fd82e
Alexander Petcoglo <mail@rckt.cc>
2024-05-06 14:26:25 +0300

    implemented UI for model deletion

--- packages/toolkit/src/view/model/ModelsList.tsx
+++ packages/toolkit/src/view/model/ModelsList.tsx
@@ -1,17 +1,20 @@
 "use client";
 
+import { QueryObserverResult, RefetchOptions } from "@tanstack/react-query";
 import { CardModel } from "../../components/card-model/CardModel";
-import { Model } from "../../lib";
+import { Model, Nullable } from "../../lib";
 
 export type ModelsListProps = {
   models: Model[];
+  accessToken: Nullable<string>;
+  onModelDelete: (options?: RefetchOptions | undefined) => Promise<QueryObserverResult<Model[], Error>>
   //modelsWatchState: ModelsWatchState;
   //isError: boolean;
   //isLoading: boolean;
 };
 
 export const ModelsList = (props: ModelsListProps) => {
-  const { models/* , modelsWatchState, isError, isLoading */ } = props;
+  const { models, accessToken, onModelDelete/* , modelsWatchState, isError, isLoading */ } = props;
 
   if (models.length === 0) {
     return null;
@@ -21,7 +24,7 @@ export const ModelsList = (props: ModelsListProps) => {
     <div className="mb-4 flex flex-col gap-y-4">
       {
         models.map((model, index) => {
-          return <CardModel model={model} key={index} />
+          return <CardModel model={model} key={index} accessToken={accessToken} onDelete={onModelDelete} />
         })
       }
     </div>
