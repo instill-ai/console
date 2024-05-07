c1e0f0b6384785cda4b69a21de4ddefffde7bbbf
Alexander Petcoglo <mail@rckt.cc>
2024-05-07 15:45:56 +0300

    models request filter and visibility params

--- packages/toolkit/src/lib/react-query-service/model/useModels.ts
+++ packages/toolkit/src/lib/react-query-service/model/useModels.ts
@@ -1,19 +1,33 @@
 import { useQuery } from "@tanstack/react-query";
 import { env } from "../../../server";
-import { listModelsQuery } from "../../vdp-sdk";
+import { Visibility, listModelsQuery } from "../../vdp-sdk";
 import type { Nullable } from "../../type";
 
 export function useModels({
   accessToken,
   enabled,
   retry,
+  filter,
+  visibility,
 }: {
   accessToken: Nullable<string>;
   enabled: boolean;
   retry?: false | number;
+  filter: Nullable<string>;
+  visibility: Nullable<Visibility>;
 }) {
+  const queryKey = ["models"];
+
+  if (filter) {
+    queryKey.push(filter);
+  }
+
+  if (visibility) {
+    queryKey.push(visibility);
+  }
+
   return useQuery({
-    queryKey: ["models"],
+    queryKey,
     queryFn: async () => {
       if (!accessToken) {
         return Promise.reject(new Error("accessToken not provided"));
@@ -23,6 +37,8 @@ export function useModels({
         pageSize: env("NEXT_PUBLIC_QUERY_PAGE_SIZE"),
         nextPageToken: null,
         accessToken,
+        filter,
+        visibility,
       });
 
       return Promise.resolve(models);
