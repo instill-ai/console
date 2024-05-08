92a3ca8b974d5b36ed50259776e445379f408d4f
Naman Anand <iamnamananand996@gmail.com>
2024-05-08 19:18:03 +0530

    chore: audio input update

--- packages/toolkit/src/lib/use-instill-form/components/trigger-request-form-fields/AudioField.tsx
+++ packages/toolkit/src/lib/use-instill-form/components/trigger-request-form-fields/AudioField.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import * as React from "react";
+import cn from "clsx";
 import { Form } from "@instill-ai/design-system";
 import { AutoFormFieldBaseProps, Nullable } from "../../..";
 import { readFileToBinary } from "../../../../view";
@@ -8,6 +9,7 @@ import { FieldHead } from "./FieldHead";
 import { UploadFileInput } from "./UploadFileInput";
 import { AudioListItem } from "./AudioListItem";
 import { StartOperatorFreeFormFieldBaseProps } from "../../types";
+import { Icons } from "@instill-ai/design-system";
 
 export const AudioField = ({
   mode,
@@ -46,26 +48,48 @@ export const AudioField = ({
               disabledReferenceHint={disabledReferenceHint}
             />
 
-            <div className="flex">
-              <Form.Control>
-                <UploadFileInput
-                  ref={inputRef}
-                  keyPrefix={keyPrefix}
-                  title="Upload audio"
-                  fieldKey={path}
-                  accept="audio/*"
-                  onChange={async (e) => {
-                    const file = e.target.files?.[0];
-                    if (file) {
-                      setAudioFile(file);
-                      const binary = await readFileToBinary(file);
-                      field.onChange(binary);
-                    }
-                  }}
-                  disabled={disabled}
-                />
-              </Form.Control>
-            </div>
+            <label
+              htmlFor={`upload-file-input-${path}-${keyPrefix}`}
+              className="cursor-pointer"
+            >
+              <div
+                key={`${path}-image-placeholder`}
+                className={cn(
+                  "flex w-full flex-col items-center justify-center",
+                  mode === "build"
+                    ? "h-[150px] bg-semantic-bg-secondary"
+                    : "h-[230px] rounded-sm bg-semantic-bg-base-bg"
+                )}
+              >
+                <div className="hidden">
+                  <Form.Control>
+                    <UploadFileInput
+                      ref={inputRef}
+                      keyPrefix={keyPrefix}
+                      title="Upload audio"
+                      fieldKey={path}
+                      accept="audio/*"
+                      onChange={async (e) => {
+                        const file = e.target.files?.[0];
+                        if (file) {
+                          setAudioFile(file);
+                          const binary = await readFileToBinary(file);
+                          field.onChange(binary);
+                        }
+                      }}
+                      disabled={disabled}
+                    />
+                  </Form.Control>
+                </div>
+                <Icons.Upload01 className="h-8 w-8 stroke-semantic-fg-secondary" />
+                <p className="mt-4 font-sans text-[12px] font-normal text-semantic-fg-primary">
+                  Drag-and-drop audio, or{" "}
+                  <span className="text-semantic-accent-default">
+                    browse computer
+                  </span>
+                </p>
+              </div>
+            </label>
             {audioFile ? (
               <AudioListItem
                 src={URL.createObjectURL(audioFile)}
--- packages/toolkit/src/lib/use-instill-form/components/trigger-request-form-fields/AudiosField.tsx
+++ packages/toolkit/src/lib/use-instill-form/components/trigger-request-form-fields/AudiosField.tsx
@@ -9,6 +9,7 @@ import { FieldHead } from "./FieldHead";
 import { UploadFileInput } from "./UploadFileInput";
 import { AudioListItem } from "./AudioListItem";
 import { StartOperatorFreeFormFieldBaseProps } from "../../types";
+import { Icons } from "@instill-ai/design-system";
 
 export const AudiosField = ({
   mode,
@@ -47,88 +48,114 @@ export const AudiosField = ({
               disabledReferenceHint={disabledReferenceHint}
             />
 
-            <div className="flex flex-row gap-x-1">
-              <Form.Control>
-                <UploadFileInput
-                  ref={inputRef}
-                  keyPrefix={keyPrefix}
-                  title="Upload audios"
-                  fieldKey={path}
-                  accept="audio/*"
-                  multiple={true}
-                  onChange={async (e) => {
-                    if (e.target.files && e.target.files.length > 0) {
-                      const files: File[] = [];
-                      const binaries: string[] = [];
-                      for (const file of e.target.files) {
-                        const binary = await readFileToBinary(file);
-                        files.push(file);
-                        binaries.push(binary);
-                      }
-                      field.onChange(binaries);
-                      setAudioFiles((prev) => [...prev, ...files]);
-                    }
-                  }}
-                  disabled={disabled}
-                />
-              </Form.Control>
-              {audioFiles.length > 0 ? (
-                <button
-                  type="button"
-                  className="flex cursor-pointer rounded-full bg-semantic-error-bg px-2 py-0.5 font-sans text-xs font-medium text-semantic-error-default hover:bg-semantic-error-bg-alt"
-                  onClick={() => {
-                    field.onChange([]);
-                    setAudioFiles([]);
-
-                    if (inputRef.current) {
-                      inputRef.current.value = "";
-                    }
-                  }}
-                >
-                  Delete all
-                </button>
-              ) : null}
-            </div>
-            {audioFiles.length > 0 ? (
-              <ScrollArea.Root
+            <label
+              htmlFor={`upload-file-input-${path}-${keyPrefix}`}
+              className="cursor-pointer"
+            >
+              <div
+                key={`${path}-image-placeholder`}
                 className={cn(
-                  "nowheel h-[216px] rounded-sm p-2",
+                  "flex w-full flex-col items-center justify-center",
                   mode === "build"
-                    ? "bg-semantic-bg-secondary"
-                    : "border border-semantic-bg-line"
+                    ? "h-[150px] bg-semantic-bg-secondary"
+                    : "h-[230px] rounded-sm bg-semantic-bg-base-bg"
                 )}
               >
-                <div className="flex h-full flex-col gap-y-2">
-                  {audioFiles.map((e, i) => (
-                    <AudioListItem
-                      key={`${path}-${e.name}-item`}
-                      name={e.name}
-                      src={URL.createObjectURL(e)}
-                      onDelete={async () => {
-                        const newFiles = audioFiles.filter(
-                          (_, index) => index !== i
-                        );
-
-                        const newBinaries: string[] = [];
-
-                        for (const file of newFiles) {
-                          const binary = await readFileToBinary(file);
-                          newBinaries.push(binary);
-                        }
-
-                        field.onChange(newBinaries);
-                        setAudioFiles(newFiles);
-
-                        // We directly remove the browser input value, we don't need it
-                        // and it may cause some surprise when user reupload the same file
-                        if (inputRef.current) {
-                          inputRef.current.value = "";
+                <div className="hidden">
+                  <Form.Control>
+                    <UploadFileInput
+                      ref={inputRef}
+                      keyPrefix={keyPrefix}
+                      title="Upload audios"
+                      fieldKey={path}
+                      accept="audio/*"
+                      multiple={true}
+                      onChange={async (e) => {
+                        if (e.target.files && e.target.files.length > 0) {
+                          const files: File[] = [];
+                          const binaries: string[] = [];
+                          for (const file of e.target.files) {
+                            const binary = await readFileToBinary(file);
+                            files.push(file);
+                            binaries.push(binary);
+                          }
+                          field.onChange(binaries);
+                          setAudioFiles((prev) => [...prev, ...files]);
                         }
                       }}
+                      disabled={disabled}
                     />
-                  ))}
+                  </Form.Control>
                 </div>
-              </ScrollArea.Root>
+                <Icons.Upload01 className="h-8 w-8 stroke-semantic-fg-secondary" />
+                <p className="mt-4 font-sans text-[12px] font-normal text-semantic-fg-primary">
+                  Drag-and-drop audios, or{" "}
+                  <span className="text-semantic-accent-default">
+                    browse computer
+                  </span>
+                </p>
+              </div>
+            </label>
+
+            {audioFiles.length > 0 ? (
+              <React.Fragment>
+                <div className="flex flex-row gap-x-1">
+                  <button
+                    type="button"
+                    className="flex cursor-pointer rounded-full bg-semantic-error-bg px-2 py-0.5 font-sans text-xs font-medium text-semantic-error-default hover:bg-semantic-error-bg-alt"
+                    onClick={() => {
+                      field.onChange([]);
+                      setAudioFiles([]);
+
+                      if (inputRef.current) {
+                        inputRef.current.value = "";
+                      }
+                    }}
+                  >
+                    Delete all
+                  </button>
+                </div>
+
+                <ScrollArea.Root
+                  className={cn(
+                    "nowheel h-[216px] rounded-sm p-2",
+                    mode === "build"
+                      ? "bg-semantic-bg-secondary"
+                      : "border border-semantic-bg-line"
+                  )}
+                >
+                  <div className="flex h-full flex-col gap-y-2">
+                    {audioFiles.map((e, i) => (
+                      <AudioListItem
+                        key={`${path}-${e.name}-item`}
+                        name={e.name}
+                        src={URL.createObjectURL(e)}
+                        onDelete={async () => {
+                          const newFiles = audioFiles.filter(
+                            (_, index) => index !== i
+                          );
+
+                          const newBinaries: string[] = [];
+
+                          for (const file of newFiles) {
+                            const binary = await readFileToBinary(file);
+                            newBinaries.push(binary);
+                          }
+
+                          field.onChange(newBinaries);
+                          setAudioFiles(newFiles);
+
+                          // We directly remove the browser input value, we don't need it
+                          // and it may cause some surprise when user reupload the same file
+                          if (inputRef.current) {
+                            inputRef.current.value = "";
+                          }
+                        }}
+                      />
+                    ))}
+                  </div>
+                </ScrollArea.Root>
+              </React.Fragment>
             ) : null}
             <Form.Description className="!text-xs" text={description} />
             <Form.Message />
