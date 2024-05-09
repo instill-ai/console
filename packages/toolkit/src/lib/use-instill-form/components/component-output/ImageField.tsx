5baa67b3e1e9c2682057407d228fc7a9f2541fda
Naman Anand <iamnamananand996@gmail.com>
2024-05-10 05:20:21 +0530

    feat: add file input

--- packages/toolkit/src/lib/use-instill-form/components/component-output/ImageField.tsx
+++ packages/toolkit/src/lib/use-instill-form/components/component-output/ImageField.tsx
@@ -2,7 +2,9 @@
 
 import { Nullable } from "../../../type";
 import { ComponentOutputFieldBaseProps } from "../../types";
+import { DownloadButton } from "./DownloadButton";
 import { FieldRoot } from "./FieldRoot";
+import { ShareButton } from "./ShareButton";
 
 export type ImageFieldProps = {
   image: Nullable<string>;
@@ -14,8 +16,17 @@ export const ImageField = (props: ImageFieldProps) => {
   return (
     <FieldRoot title={title} fieldKey={`${title}-field`}>
       {!hideField && image ? (
-        <div className="flex w-full">
-          <img src={image} alt={`${title}`} className="object-contain" />
+        <div className="nodrag nowheel flex flex-col rounded-sm border border-semantic-bg-line">
+          <div className="flex flex-row rounded-t-[4px] border-b border-semantic-bg-line bg-[#F0F0F0] px-2 py-0.5">
+            <div className="ml-auto flex flex-row gap-x-1">
+              <DownloadButton className="my-auto" text={""} />
+            </div>
+          </div>
+          <div>
+            <div className="flex w-full flex-wrap gap-2">
+              <img src={image} alt={`${title}`} className="object-contain" />
+            </div>
+          </div>
         </div>
       ) : null}
     </FieldRoot>
