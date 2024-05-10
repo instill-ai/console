c667074ec5838c5913e2cd508d56578036dea8a4
Alexander Petcoglo <mail@rckt.cc>
2024-05-10 19:12:02 +0300

    add region and hardware title getters, fix pagination

--- packages/design-system/src/ui-helpers/index.ts
+++ packages/design-system/src/ui-helpers/index.ts
@@ -1,3 +1,5 @@
 export { getModelInstanceTaskToolkit } from "./getModelInstanceTaskToolkit";
 export { getModelDefinitionToolkit } from "./getModelDefinitionToolkit";
 export { getPipelineModeToolkit } from "./getPipelineModeToolkit";
+export { getModelRegionToolkit } from './getModelRegionToolkit';
+export { getModelHardwareToolkit } from './getModelHardwareToolkit';
