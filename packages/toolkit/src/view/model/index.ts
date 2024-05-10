1eb08f0339e247e02907c3df96de2e8a75ab377d
Alexander Petcoglo <mail@rckt.cc>
2024-05-10 16:11:24 +0300

    add models list pagination to repo

--- packages/toolkit/src/view/model/index.ts
+++ packages/toolkit/src/view/model/index.ts
@@ -5,5 +5,4 @@ export * from "./ModelConfigurationFields";
 export * from "./ModelHubCreatePageMainView";
 export * from "./ModelHubListPageMainView";
 export * from "./ModelHubSettingPageMainView";
-export * from "./ModelsTable";
 export * from "./ModelTablePlaceholder";
