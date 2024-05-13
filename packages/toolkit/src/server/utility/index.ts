731bcb2bc78d344a13996693d8c19c3a1038321a
Alexander Petcoglo <mail@rckt.cc>
2024-05-13 11:21:15 +0300

    type fixes, files cleanup

--- packages/toolkit/src/server/utility/index.ts
+++ packages/toolkit/src/server/utility/index.ts
@@ -3,7 +3,6 @@ export * from "./getHumanReadableStringFromTime";
 export * from "./config";
 export * from "./handle";
 export * from "./groupBy";
-export * from "./determineModelState";
 export * from "./cookie";
 export * from "./generateRandomReadableName";
 export * from "./getCanvasTextWidth";
