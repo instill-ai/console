323097d0a803fedd03ea2b4bc4429c8f7ef5e3ff
thewbuk <57405495+thewbuk@users.noreply.github.com>
2024-05-13 14:42:43 +0100

    add link to view changelog button

--- packages/toolkit/src/components/LatestChangesCard.tsx
+++ packages/toolkit/src/components/LatestChangesCard.tsx
@@ -17,13 +17,15 @@ const LatestChangesCard = () => (
       April 24, 2024
     </button>
     <p>Introducing Secret Management for Connector Configuration</p>
-    <button
-      type="button"
-      className="my-auto text-semantic-accent-default product-button-button-2 hover:!underline flex justify-start mt-4"
-    >
-      View changelog
-      <Icons.ChevronRight className="my-auto h-4 w-4 stroke-semantic-accent-default" />
-    </button>
+    <a href="https://instill-ai.productlane.com/changelog" target="_blank" rel="noopener noreferrer">
+      <button
+        type="button"
+        className="my-auto text-semantic-accent-default product-button-button-2 hover:!underline flex justify-start mt-4"
+      >
+        View changelog
+        <Icons.ChevronRight className="my-auto h-4 w-4 stroke-semantic-accent-default" />
+      </button>
+    </a>
   </div>
 );
 
