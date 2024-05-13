1f871d4eac61cbb03dda328c961c54621f7a6ad9
thewbuk <57405495+thewbuk@users.noreply.github.com>
2024-05-13 16:36:44 +0100

    fix sorting

--- packages/toolkit/src/components/card-pipeline-hub/Body.tsx
+++ packages/toolkit/src/components/card-pipeline-hub/Body.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { Icons } from "@instill-ai/design-system";
-import { ReadOnlyPipelineBuilder } from "../../view";
 import { Pipeline } from "../../lib";
 import { useRouter } from "next/navigation";
 
