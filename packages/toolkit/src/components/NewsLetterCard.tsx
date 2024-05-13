ed91e6b0667f07f8ee582db8aa72f9ffcab42544
thewbuk <57405495+thewbuk@users.noreply.github.com>
2024-05-13 09:13:46 +0100

    add tags to cards, fix spacing and text styling

--- packages/toolkit/src/components/NewsLetterCard.tsx
+++ packages/toolkit/src/components/NewsLetterCard.tsx
@@ -3,18 +3,21 @@ import { ImageWithFallback } from "./ImageWithFallback";
 
 const NewsLetterCard = () => (
   <div className="flex flex-col gap-y-2 rounded-sm border border-semantic-bg-line p-2">
-    <h2 className="text-2xl font-bold">What's New?</h2>
+    <h2 className="text-2xl font-bold mb-4">What's New?</h2>
     <ImageWithFallback
-      src={`/icons/gcs.svg`}
+      src={`https://placehold.co/600x400`}
       width={32}
       height={32}
       alt={`test-icon`}
       fallbackImg={<Icons.Box className="h-8 w-8 stroke-semantic-fg-primary" />}
     />
-    <div className="inline-block rounded-sm bg-blue-500 px-2 py-1 font-bold text-white">
+    <button
+      type="button"
+      className="my-auto capitalize text-semantic-accent-default product-button-button-2 hover:!underline bg-blue-100 rounded-sm p-2 w-1/3"
+    >
       Tag
-    </div>
-    <p>Text goes here</p>
+    </button>
+    <p>May 2024: Dive in AI Building with Instill AI's Hackathon and Local Workshop</p>
   </div>
 );
 
