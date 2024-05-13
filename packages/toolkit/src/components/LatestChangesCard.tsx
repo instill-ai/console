c0e84a07c453d0bd81bd103f26366e7149baa764
thewbuk <57405495+thewbuk@users.noreply.github.com>
2024-05-13 09:13:46 +0100

    add tags to cards, fix spacing and text styling

--- packages/toolkit/src/components/LatestChangesCard.tsx
+++ packages/toolkit/src/components/LatestChangesCard.tsx
@@ -1,16 +1,29 @@
-import React from "react";
+import { Icons } from "@instill-ai/design-system";
 
 const LatestChangesCard = () => (
   <div className="mt-4 flex flex-col gap-y-2 rounded-sm border border-semantic-bg-line p-2">
-    <h2 className="text-2xl font-bold">Latest Changes</h2>
-    <div className="inline-block rounded-sm bg-blue-500 px-2 py-1 font-bold text-white">
-      Tag
-    </div>
-    <p>Text goes here</p>
-    <div className="inline-block rounded-sm bg-blue-500 px-2 py-1 font-bold text-white">
-      Tag
-    </div>
-    <p>Text goes here</p>
+    <h2 className="text-2xl font-bold mb-4">Latest Changes</h2>
+    <button
+      type="button"
+      className="my-auto text-semantic-accent-default product-button-button-2 hover:!underline bg-blue-100 rounded-sm p-2 w-2/3 capitalize"
+    >
+      April 24, 2024
+    </button>
+    <p>Introducing Secret Management for Connector Configuration</p>
+    <button
+      type="button"
+      className="my-auto  text-semantic-accent-default product-button-button-2 hover:!underline bg-blue-100 rounded-sm p-2 w-2/3 capitalize"
+    >
+      April 24, 2024
+    </button>
+    <p>Introducing Secret Management for Connector Configuration</p>
+    <button
+      type="button"
+      className="my-auto text-semantic-accent-default product-button-button-2 hover:!underline flex justify-start mt-4"
+    >
+      View changelog
+      <Icons.ChevronRight className="my-auto h-4 w-4 stroke-semantic-fg-disabled  text-semantic-accent-default" />
+    </button>
   </div>
 );
 
