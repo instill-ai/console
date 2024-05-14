d6415fd94519353c7e27b8d1f43c544abb6a8ab8
Alexander Petcoglo <mail@rckt.cc>
2024-05-14 13:38:05 +0300

    updated lint-staged config

--- packages/toolkit/.eslintrc.cjs
+++ packages/toolkit/.eslintrc.cjs
@@ -1,5 +1,10 @@
+var path = require("path");
+
 module.exports = {
   root: true,
   extends: ["@instill-ai/eslint-config-cortex"],
   ignorePatterns: [".eslintrc.cjs"],
+  parserOptions: {
+    project: path.join(__dirname, "tsconfig.json"),
+  },
 };
