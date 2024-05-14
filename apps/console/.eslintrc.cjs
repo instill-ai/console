d3abbafa5c98092f72eebfac301c7b0f4ea9b58f
Alexander Petcoglo <mail@rckt.cc>
2024-05-14 13:38:05 +0300

    updated lint-staged config

--- apps/console/.eslintrc.cjs
+++ apps/console/.eslintrc.cjs
@@ -1,5 +1,10 @@
+var path = require("path");
+
 module.exports = {
   root: true,
   extends: ["next/core-web-vitals"],
   ignorePatterns: [".eslintrc.cjs"],
+  parserOptions: {
+    project: path.join(__dirname, "tsconfig.json"),
+  },
 };
