f54106a0d1b7e4d60674ae7ea35a0a0195457307
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2024-05-14 16:29:03 +0800

    feat: support richer json overview in the pipeline-builder

--- packages/toolkit/src/lib/use-instill-form/components/component-output/ObjectField.tsx
+++ packages/toolkit/src/lib/use-instill-form/components/component-output/ObjectField.tsx
@@ -1,8 +1,11 @@
 "use client";
 
+import JsonView from "@uiw/react-json-view";
+import { CopyToClipboardButton } from "../../../../components";
 import { GeneralRecord, Nullable } from "../../../type";
 import { ComponentOutputFieldBaseProps } from "../../types";
 import { FieldRoot } from "./FieldRoot";
+import { customTheme } from "../../../react-json-view";
 
 export type ObjectFieldProps = {
   object: Nullable<GeneralRecord>;
@@ -14,10 +17,29 @@ export const ObjectField = (props: ObjectFieldProps) => {
   return (
     <FieldRoot title={title} fieldKey={`${title}-field`}>
       {object && !hideField ? (
-        <div className="flex w-full">
-          <pre className="min-h-[36px] w-full flex-1 whitespace-pre-wrap break-all text-semantic-fg-primary product-body-text-4-regular">
-            {JSON.stringify(object, null, 2)}
-          </pre>
+        <div className="relative flex w-full flex-col">
+          <CopyToClipboardButton
+            className="absolute right-2 top-2 !border-none !bg-transparent"
+            iconClassName="!stroke-semantic-bg-primary"
+            text={JSON.stringify(object, null, 2)}
+          />
+          <JsonView
+            value={object ?? {}}
+            style={{
+              ...customTheme,
+              padding: "12px",
+              borderRadius: "8px",
+              fontSize: "12px",
+              overflow: "auto",
+              maxHeight: "400px",
+            }}
+            enableClipboard={false}
+            displayObjectSize={false}
+            indentWidth={8}
+            shortenTextAfterLength={0}
+            displayDataTypes={false}
+            collapsed={3}
+          />
         </div>
       ) : null}
     </FieldRoot>
