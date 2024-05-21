7ea05a21294bb3e62d43d2da89c297b6dc62be65
Alexander Petcoglo <mail@rckt.cc>
2024-05-21 18:02:00 +0300

    update DataTable component

--- packages/toolkit/src/view/model/view-settings/ModelSettingsOverview.tsx
+++ packages/toolkit/src/view/model/view-settings/ModelSettingsOverview.tsx
@@ -1,11 +1,34 @@
-import { Model } from "../../../lib";
+import {
+  InstillStore,
+  Model,
+  useAppEntity,
+  useInstillStore,
+  useShallow,
+  useUserModelReadme,
+} from "../../../lib";
 
 export type ModelSettingsOverviewProps = {
   model?: Model;
 };
 
+const selector = (store: InstillStore) => ({
+  accessToken: store.accessToken,
+  enabledQuery: store.enabledQuery,
+});
+
 export const ModelSettingsOverview = ({
   model,
 }: ModelSettingsOverviewProps) => {
-  return <div>{model?.name}</div>;
+  const entityObject = useAppEntity();
+  const { accessToken, enabledQuery } = useInstillStore(useShallow(selector));
+
+  const modelReadme = useUserModelReadme({
+    modelName: model?.name || null,
+    enabled: enabledQuery && entityObject.isSuccess,
+    accessToken,
+  });
+
+  console.log(modelReadme);
+
+  return <div></div>;
 };
