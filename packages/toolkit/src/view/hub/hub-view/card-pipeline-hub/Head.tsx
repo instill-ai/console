1c82aacbacf2ba9bfe99b83fb02f7c39b62433f2
thewbuk <57405495+thewbuk@users.noreply.github.com>
2024-05-21 14:50:51 +0100

    transform display name if empty

--- packages/toolkit/src/view/hub/hub-view/card-pipeline-hub/Head.tsx
+++ packages/toolkit/src/view/hub/hub-view/card-pipeline-hub/Head.tsx
@@ -39,24 +39,37 @@ export const Head = ({
     return null;
   }, [pipeline]);
 
+    // Function to transform the ID into a more readable format
+    const transformID = (id: string) => {
+      return id
+        .split(/[-_]/) // Split the ID by dash and underscore
+        .map((part) => part.charAt(0).toUpperCase() + part.slice(1)) // Capitalize each part
+        .join(" "); // Join the parts with a space
+    };
+    
   const displayName = React.useMemo(() => {
     const owner = pipeline.owner;
-
+  
     if ("user" in owner) {
-      // Owner is of type user
       const userOwner = owner as UserOwner;
       const userID = userOwner.user.name.split("/")[1]; // Get the part after "/"
-      const ownerDisplayName = userOwner.user.profile?.display_name || userID;
-
-      return ownerDisplayName;
+  
+      if (userOwner.user.profile?.display_name?.trim()) {
+        return userOwner.user.profile.display_name;
+      } else {
+        // Transform userID if display_name is empty, null, or consists of only whitespace
+        return transformID(userID);
+      }
     } else if ("organization" in owner) {
-      // Owner is of type organization
       const orgOwner = owner as OrganizationOwner;
       const orgID = orgOwner.organization.id;
-      const orgDisplayName =
-        orgOwner.organization.profile?.display_name || orgID;
-
-      return orgDisplayName;
+  
+      if (orgOwner.organization.profile?.display_name?.trim()) {
+        return orgOwner.organization.profile.display_name;
+      } else {
+        // Transform orgID if display_name is empty, null, or consists of only whitespace
+        return transformID(orgID);
+      }
     }
   }, [pipeline.owner]);
 
