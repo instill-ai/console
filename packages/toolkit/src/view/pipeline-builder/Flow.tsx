64d6b8499eac03c6bb6e3f9692d17ac1f5465283
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2024-05-21 13:39:52 +0800

    fix: fix credit not show up in pipeline-builder

--- packages/toolkit/src/view/pipeline-builder/Flow.tsx
+++ packages/toolkit/src/view/pipeline-builder/Flow.tsx
@@ -75,7 +75,7 @@ export const Flow = React.forwardRef<HTMLDivElement, FlowProps>(
               </div>
             </div>
           ) : null}
-          {env("NEXT_PUBLIC_APP_ENV") !== "CLOUD" ? (
+          {env("NEXT_PUBLIC_APP_ENV") === "CLOUD" ? (
             <div className="absolute bottom-8 left-1/2 -translate-x-1/2">
               <RemainingCreditCTA ctaTargetHref="/subscribe" />
             </div>
