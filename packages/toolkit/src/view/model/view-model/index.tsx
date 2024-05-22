3e4d4c6bae50ba5bf21d9e832413f6e12ef8b875
Alexander Petcoglo <mail@rckt.cc>
2024-05-22 17:53:44 +0300

    updated readme editor, added readme editor to model overview

---
+++ packages/toolkit/src/view/model/view-model/index.tsx
@@ -0,0 +1,6 @@
+export * from "./ModelContentViewer";
+export * from "./ModelHead";
+export * from "./ModelVersions";
+export * from "./ModelSettingsEditForm";
+export * from "./ModelApi";
+export * from "./ModelOverview";
--- packages/toolkit/src/view/model/view-settings/index.tsx
+++
@@ -1,4 +0,0 @@
-export * from "./ModelSettingsContentViewer";
-export * from "./ModelSettingsHead";
-export * from "./ModelSettingsVersions";
-export * from "./ModelSettingsEditForm";
