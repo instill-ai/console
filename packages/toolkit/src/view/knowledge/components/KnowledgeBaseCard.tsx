4ea2d0080b62650d6c22f4fc728f6b8ce45d9aed
thewbuk <57405495+thewbuk@users.noreply.github.com>
2024-05-22 17:12:00 +0100

    feat styling cards

--- packages/toolkit/src/view/knowledge/components/KnowledgeBaseCard.tsx
+++ packages/toolkit/src/view/knowledge/components/KnowledgeBaseCard.tsx
@@ -2,27 +2,22 @@
 import { Icons } from "@instill-ai/design-system";
 
 type KnowledgeBaseCardProps = {
-  title: string;
-  description: string;
   onClick?: () => void;
 };
 
 export const KnowledgeBaseCard = ({
-  title,
-  description,
   onClick,
 }: KnowledgeBaseCardProps) => {
   return (
     <div
-      className="flex h-[175px] cursor-pointer flex-col gap-y-2.5 rounded border border-semantic-bg-line bg-[#F8F9FC] p-2.5"
+      className="flex h-[175px] cursor-pointer flex-col gap-y-2.5 rounded-md border border-semantic-bg-line bg-semantic-bg-base-bg p-3 w-[360px]"
       onClick={onClick}
     >
       <div className="flex items-center gap-x-2">
         <Icons.Plus className="h-4 w-4 stroke-semantic-fg-secondary" />
-        <h3 className="text-base font-medium text-black">{title}</h3>
+        <h3 className="  text-semantic-fg-primary">Create Knowledge</h3>
       </div>
-      <div className="h-px w-full bg-white" />
-      <p className="text-sm text-[rgba(0,0,0,0.7)]">{description}</p>
+      <p className="product-body-text-3-regular  text-semantic-fg-secondary">Import your own text data or write data in real-time via Webhook for LLM context enhancement.</p>
     </div>
   );
 };
