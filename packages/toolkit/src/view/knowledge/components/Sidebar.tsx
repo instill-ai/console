9fe6389e2f649a32f630a793c9e583418eb5a5e7
thewbuk <57405495+thewbuk@users.noreply.github.com>
2024-05-22 22:57:32 +0100

    feat: start delete popup

--- packages/toolkit/src/view/knowledge/components/Sidebar.tsx
+++ packages/toolkit/src/view/knowledge/components/Sidebar.tsx
@@ -5,10 +5,10 @@ export const Sidebar = () => {
     const [activeTab, setActiveTab] = React.useState<Nullable<string>>("my-knowledge-bases");
 
     return (
-        <aside className="flex w-64 flex-col gap-y-4 bg-white p-4">
+        <aside className="flex flex-col p-4 w-38 gap-y-4">
             <div
                 className={`flex h-8 items-center gap-x-2 rounded px-3 text-sm font-semibold ${activeTab === "my-knowledge-bases"
-                    ? "bg-[#F0F5FF] text-[#1D5BD7]"
+                    ? "bg-semantic-accent-bg text-semantic-accent-hover"
                     : "text-semantic-fg-secondary"
                     }`}
                 onClick={() => setActiveTab("my-knowledge-bases")}
@@ -17,7 +17,7 @@ export const Sidebar = () => {
             </div>
             <div
                 className={`flex h-8 items-center gap-x-2 rounded px-3 text-sm font-semibold ${activeTab === "upload-explore"
-                    ? "bg-semantic-accent-bg text-semantic-accent-fg"
+                    ? "bg-semantic-accent-bg text-semantic-accent-hover"
                     : "text-semantic-fg-secondary"
                     }`}
                 onClick={() => setActiveTab("upload-explore")}
@@ -26,7 +26,7 @@ export const Sidebar = () => {
             </div>
             <div
                 className={`flex h-8 items-center gap-x-2 rounded px-3 text-sm font-semibold ${activeTab === "catalog-files"
-                    ? "bg-semantic-accent-bg text-semantic-accent-fg"
+                    ? "bg-semantic-accent-bg text-semantic-accent-hover"
                     : "text-semantic-fg-secondary"
                     }`}
                 onClick={() => setActiveTab("catalog-files")}
@@ -35,7 +35,7 @@ export const Sidebar = () => {
             </div>
             <div
                 className={`flex h-8 items-center gap-x-2 rounded px-3 text-sm font-semibold ${activeTab === "retrieve-test"
-                    ? "bg-semantic-accent-bg text-semantic-accent-fg"
+                    ? "bg-semantic-accent-bg text-semantic-accent-hover"
                     : "text-semantic-fg-secondary"
                     }`}
                 onClick={() => setActiveTab("retrieve-test")}
