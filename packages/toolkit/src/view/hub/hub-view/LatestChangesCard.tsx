ed3af0f23042b16b13a81acdc0927450db922211
thewbuk <57405495+thewbuk@users.noreply.github.com>
2024-05-24 11:42:51 +0100

    align dot with date

--- packages/toolkit/src/view/hub/hub-view/LatestChangesCard.tsx
+++ packages/toolkit/src/view/hub/hub-view/LatestChangesCard.tsx
@@ -43,11 +43,11 @@ export const LatestChangesCard = () => {
       </div>
       {changelogs.isSuccess && (
         <div className="relative">
-          <div className="absolute left-1.5 top-3 bottom-3 w-0.5 bg-semantic-node-disconnected-bg transform -translate-x-1/2" />
+          <div className="absolute left-1.5 top-4 bottom-4 w-0.5 bg-semantic-node-disconnected-bg transform -translate-x-1/2" />
           {changelogs.data.map((changelog: Changelog, index: number) => (
             <div key={changelog.id} className="mb-4 flex items-start">
               <div className="relative z-10 mr-2 flex-shrink-0 flex flex-col items-center">
-                <div className="h-3 w-3 rounded-full bg-semantic-node-disconnected-bg" />
+                <div className="h-3 w-3 rounded-full bg-semantic-node-disconnected-bg mt-1" /> 
               </div>
               <a
                 href={`https://instill-ai.productlane.com/changelog/${changelog.id}`}
