3ef5a56620b858f79cd3768e99b892ff27d855a9
thewbuk <57405495+thewbuk@users.noreply.github.com>
2024-05-24 11:37:17 +0100

    feat: change style of LatestChangesCard

--- packages/toolkit/src/view/hub/hub-view/LatestChangesCard.tsx
+++ packages/toolkit/src/view/hub/hub-view/LatestChangesCard.tsx
@@ -41,46 +41,52 @@ export const LatestChangesCard = () => {
       <div className="mb-4 font-bold product-headings-heading-3">
         Latest Changes
       </div>
-      {changelogs.isSuccess &&
-        changelogs.data.map((changelog: Changelog) => (
-          <a
-            href={`https://instill-ai.productlane.com/changelog/${changelog.id}`}
-            key={changelog.id}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="mb-4"
-          >
-            <div
-              className={cn(
-                buttonVariants({ variant: "secondaryColour", size: "md" }),
-                "pointer-events-none mb-1 w-min whitespace-nowrap rounded-sm px-2 py-2 capitalize"
-              )}
-            >
-              {changelog.date && new Date(changelog.date).getTime() !== 0
-                ? new Date(changelog.date).toLocaleDateString("en-US", {
-                    year: "numeric",
-                    month: "long",
-                    day: "numeric",
-                  })
-                : new Date(changelog.updatedAt).toLocaleDateString("en-US", {
-                    year: "numeric",
-                    month: "long",
-                    day: "numeric",
-                  })}
+      {changelogs.isSuccess && (
+        <div className="relative">
+          <div className="absolute left-1.5 top-3 bottom-3 w-0.5 bg-semantic-node-disconnected-bg" />
+          {changelogs.data.map((changelog: Changelog, index: number) => (
+            <div key={changelog.id} className="mb-4 flex items-start">
+              <div className="relative z-10 mr-2 flex flex-col items-center">
+                <div className="h-3 w-3 rounded-full  bg-semantic-node-disconnected-bg" />
+              </div>
+              <a
+                href={`https://instill-ai.productlane.com/changelog/${changelog.id}`}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <div
+                  className={
+                    "pointer-events-none mb-1 w-min whitespace-nowrap capitalize text-semantic-accent-default text-xs font-semibold"
+                  }
+                >
+                  {changelog.date && new Date(changelog.date).getTime() !== 0
+                    ? new Date(changelog.date).toLocaleDateString("en-US", {
+                      year: "numeric",
+                      month: "long",
+                      day: "numeric",
+                    })
+                    : new Date(changelog.updatedAt).toLocaleDateString("en-US", {
+                      year: "numeric",
+                      month: "long",
+                      day: "numeric",
+                    })}
+                </div>
+                <p className="text-semantic-fg-primary product-body-text-3-regular hover:underline">
+                  {changelog.title}
+                </p>
+              </a>
             </div>
-            <p className="text-semantic-fg-primary product-body-text-3-regular hover:underline">
-              {changelog.title}
-            </p>
-          </a>
-        ))}
+          ))}
+        </div>
+      )}
       <a
         href="https://instill-ai.productlane.com/changelog"
         target="_blank"
         rel="noopener noreferrer"
       >
-        <div className="my-auto ml-2 mt-4 flex justify-start text-semantic-accent-default product-button-button-2 hover:!underline">
+        <div className="my-auto ml-2 flex justify-start text-semantic-accent-default product-button-button-3 hover:!underline items-center gap-2">
           View changelog
-          <Icons.ChevronRight className="my-auto h-4 w-4 stroke-semantic-accent-default" />
+          <Icons.ChevronRight className="h-4 w-4 stroke-semantic-accent-default" />
         </div>
       </a>
     </div>
