52e8b17a84ffc53f86be3617e941edb6632aac0b
thewbuk <57405495+thewbuk@users.noreply.github.com>
2024-05-28 09:42:39 +0100

    remove the colour and use hex

--- packages/toolkit/src/view/hub/hub-view/LatestChangesCard.tsx
+++ packages/toolkit/src/view/hub/hub-view/LatestChangesCard.tsx
@@ -47,7 +47,7 @@ export const LatestChangesCard = () => {
           {changelogs.data.map((changelog: Changelog, index: number) => (
             <React.Fragment key={changelog.id}>
               <div className="relative flex items-start  justify-center ">
-                <div className="h-3 w-3 rounded-full bg-semantic-bg-dot" />
+                <div className="h-3 w-3 rounded-full bg-[#CBD2E1]" />
                 {index !== changelogs.data.length && (
                   <div className="absolute bottom-0.5 left-1/2 top-3.5 w-0.5 -translate-x-1/2 rounded-full bg-semantic-node-disconnected-bg" />
                 )}
@@ -61,18 +61,18 @@ export const LatestChangesCard = () => {
                 <div className="-mt-0.5 mb-1 w-min whitespace-nowrap text-xs font-semibold capitalize text-semantic-accent-default">
                   {changelog.date && new Date(changelog.date).getTime() !== 0
                     ? new Date(changelog.date).toLocaleDateString("en-US", {
+                      year: "numeric",
+                      month: "long",
+                      day: "numeric",
+                    })
+                    : new Date(changelog.updatedAt).toLocaleDateString(
+                      "en-US",
+                      {
                         year: "numeric",
                         month: "long",
                         day: "numeric",
-                      })
-                    : new Date(changelog.updatedAt).toLocaleDateString(
-                        "en-US",
-                        {
-                          year: "numeric",
-                          month: "long",
-                          day: "numeric",
-                        }
-                      )}
+                      }
+                    )}
                 </div>
                 <p className="text-semantic-fg-primary product-body-text-3-regular hover:underline">
                   {changelog.title}
