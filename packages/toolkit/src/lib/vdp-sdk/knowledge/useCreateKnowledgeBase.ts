08ffcd1930579c4a100b7da24529a0c774fe72f1
Wojciech Bandzerewicz <57405495+thewbuk@users.noreply.github.com>
2024-05-28 15:57:10 +0100

    backend setup

---
+++ packages/toolkit/src/lib/vdp-sdk/knowledge/useCreateKnowledgeBase.ts
@@ -0,0 +1,23 @@
+// lib/useCreateKnowledgeBase.ts
+
+import { useMutation } from "@tanstack/react-query";
+import { KnowledgeBase } from "./knowledgeBase";
+import { createKnowledgeBaseMutation } from "./knowledgeBaseMutations";
+
+export function useCreateKnowledgeBase() {
+  return useMutation({
+    mutationFn: async ({
+      payload,
+      accessToken,
+    }: {
+      payload: Partial<KnowledgeBase>;
+      accessToken: string | null;
+    }) => {
+      const knowledgeBase = await createKnowledgeBaseMutation({
+        payload,
+        accessToken,
+      });
+      return knowledgeBase;
+    },
+  });
+}
