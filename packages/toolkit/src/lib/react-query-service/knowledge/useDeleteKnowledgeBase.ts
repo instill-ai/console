226f2df8e6c1db038b9e7935f1fa3f0453e994a0
Wojciech Bandzerewicz <57405495+thewbuk@users.noreply.github.com>
2024-06-03 13:37:52 +0200

    update apis

--- packages/toolkit/src/lib/react-query-service/knowledge/useDeleteKnowledgeBase.ts
+++ packages/toolkit/src/lib/react-query-service/knowledge/useDeleteKnowledgeBase.ts
@@ -13,7 +13,7 @@ export function useDeleteKnowledgeBase() {
       if (!accessToken) {
         return Promise.reject(new Error("accessToken not provided"));
       }
-      const client = createInstillAxiosClient(accessToken);
+      const client = createInstillAxiosClient(accessToken, true);
       const response = await client.delete<{
         error_msg: string;
         status_code: number;
