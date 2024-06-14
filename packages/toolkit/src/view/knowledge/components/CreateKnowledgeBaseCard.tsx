93cb4b7778ff7a8864fb75a89c6dd19b0f7553a3
Wojciech Bandzerewicz <57405495+thewbuk@users.noreply.github.com>
2024-06-14 14:49:25 +0100

    formatting

--- packages/toolkit/src/view/knowledge/components/CreateKnowledgeBaseCard.tsx
+++ packages/toolkit/src/view/knowledge/components/CreateKnowledgeBaseCard.tsx
@@ -1,4 +1,11 @@
-import { Icons, Separator, DropdownMenu, Button, Dialog, LinkButton } from "@instill-ai/design-system";
+import {
+  Icons,
+  Separator,
+  DropdownMenu,
+  Button,
+  Dialog,
+  LinkButton,
+} from "@instill-ai/design-system";
 import * as React from "react";
 import { useUpdateKnowledgeBase } from "../../../lib/react-query-service/knowledge/useUpdateKnowledgeBase";
 import { useDeleteKnowledgeBase } from "../../../lib/react-query-service/knowledge/useDeleteKnowledgeBase";
@@ -26,22 +33,25 @@ const Menu = ({ onDelete, onEdit, onDuplicate }: MenuProps) => {
         <DropdownMenu.Root>
           <DropdownMenu.Trigger asChild>
             <Button className="" variant="tertiaryGrey">
-              <Icons.DotsHorizontal className="w-4 h-4 stroke-semantic-fg-secondary" />
+              <Icons.DotsHorizontal className="h-4 w-4 stroke-semantic-fg-secondary" />
             </Button>
           </DropdownMenu.Trigger>
-          <DropdownMenu.Content align="end" className="w-[195px] rounded-md !p-0">
+          <DropdownMenu.Content
+            align="end"
+            className="w-[195px] rounded-md !p-0"
+          >
             <DropdownMenu.Item
               onClick={onEdit}
               className="!px-4 !py-2.5 !text-semantic-fg-secondary product-body-text-4-medium"
             >
-              <Icons.Edit03 className="w-4 h-4 mr-2 stroke-semantic-fg-secondary" />
+              <Icons.Edit03 className="mr-2 h-4 w-4 stroke-semantic-fg-secondary" />
               Edit info
             </DropdownMenu.Item>
             <DropdownMenu.Item
               onClick={onDuplicate}
               className="!px-4 !py-2.5 !text-semantic-fg-secondary product-body-text-4-medium"
             >
-              <Icons.Copy07 className="w-4 h-4 mr-2 stroke-semantic-fg-secondary" />
+              <Icons.Copy07 className="mr-2 h-4 w-4 stroke-semantic-fg-secondary" />
               Duplicate
             </DropdownMenu.Item>
             <Separator orientation="horizontal" />
@@ -49,7 +59,7 @@ const Menu = ({ onDelete, onEdit, onDuplicate }: MenuProps) => {
               onClick={onDelete}
               className="!px-4 !py-2.5 !text-semantic-error-default product-body-text-4-medium"
             >
-              <Icons.Trash01 className="w-4 h-4 mr-2 stroke-semantic-error-default" />
+              <Icons.Trash01 className="mr-2 h-4 w-4 stroke-semantic-error-default" />
               Delete
             </DropdownMenu.Item>
           </DropdownMenu.Content>
@@ -92,7 +102,10 @@ export const CreateKnowledgeBaseCard = ({
         payload: clonedKnowledgeBase,
         accessToken: accessToken,
       });
-      setKnowledgeBases((prevKnowledgeBases) => [...prevKnowledgeBases, newKnowledgeBase]);
+      setKnowledgeBases((prevKnowledgeBases) => [
+        ...prevKnowledgeBases,
+        newKnowledgeBase,
+      ]);
     } catch (error) {
       console.error("Error cloning knowledge base:", error);
     }
@@ -171,35 +184,59 @@ export const CreateKnowledgeBaseCard = ({
   return (
     <React.Fragment>
       {!isDeleted && (
-        <div className="flex shadow cursor-pointer flex-col rounded-md border border-semantic-bg-line bg-semantic-bg-primary p-5 w-[360px] h-[175px]" onClick={onCardClick}>
+        <div
+          className="flex h-[175px] w-[360px] cursor-pointer flex-col rounded-md border border-semantic-bg-line bg-semantic-bg-primary p-5 shadow"
+          onClick={onCardClick}
+        >
           <div className="flex items-center justify-between">
-            <div className="product-headings-heading-4">{knowledgeBase.name}</div>
+            <div className="product-headings-heading-4">
+              {knowledgeBase.name}
+            </div>
           </div>
           <Separator orientation="horizontal" className="my-[10px]" />
-          <p className="product-body-text-3-regular line-clamp-3 mb-auto">{knowledgeBase.description}</p>
-          <div className="flex justify-end items-end">
-            <Menu onDelete={handleDelete} onEdit={handleEdit} onDuplicate={handleDuplicate} />
+          <p className="mb-auto line-clamp-3 product-body-text-3-regular">
+            {knowledgeBase.description}
+          </p>
+          <div className="flex items-end justify-end">
+            <Menu
+              onDelete={handleDelete}
+              onEdit={handleEdit}
+              onDuplicate={handleDuplicate}
+            />
           </div>
         </div>
       )}
-      <Dialog.Root open={deleteDialogIsOpen} onOpenChange={setDeleteDialogIsOpen}>
+      <Dialog.Root
+        open={deleteDialogIsOpen}
+        onOpenChange={setDeleteDialogIsOpen}
+      >
         <Dialog.Content className="!w-[350px] rounded-sm !p-0">
-          <div className="rounded-sm border border-b-semantic-bg-secondary flex flex-col justify-start items-center gap-6 p-6">
-            <div className="flex items-center justify-center w-12 h-12 p-3 rounded-full bg-semantic-warning-bg">
-              <Icons.AlertTriangle className="w-6 h-6 stroke-semantic-warning-on-bg" />
+          <div className="flex flex-col items-center justify-start gap-6 rounded-sm border border-b-semantic-bg-secondary p-6">
+            <div className="flex h-12 w-12 items-center justify-center rounded-full bg-semantic-warning-bg p-3">
+              <Icons.AlertTriangle className="h-6 w-6 stroke-semantic-warning-on-bg" />
             </div>
-            <div className="flex flex-col items-start self-stretch justify-start gap-6">
+            <div className="flex flex-col items-start justify-start gap-6 self-stretch">
               <div className="flex flex-col items-center justify-center gap-1">
-                <div className="product-headings-heading-3">Delete {knowledgeBase.name}</div>
+                <div className="product-headings-heading-3">
+                  Delete {knowledgeBase.name}
+                </div>
                 <div className="text-center product-body-text-2-regular">
                   Are you sure you want to delete this knowledge base?
                 </div>
               </div>
               <div className="flex w-full gap-2">
-                <Button variant="secondaryGrey" onClick={() => setDeleteDialogIsOpen(false)} className="w-full">
+                <Button
+                  variant="secondaryGrey"
+                  onClick={() => setDeleteDialogIsOpen(false)}
+                  className="w-full"
+                >
                   Cancel
                 </Button>
-                <Button variant="danger" onClick={confirmDelete} className="w-full">
+                <Button
+                  variant="danger"
+                  onClick={confirmDelete}
+                  className="w-full"
+                >
                   Delete
                 </Button>
               </div>
@@ -208,21 +245,34 @@ export const CreateKnowledgeBaseCard = ({
         </Dialog.Content>
       </Dialog.Root>
       {showDeleteMessage && (
-        <div className="fixed bottom-4 right-4 w-[400px] h-[136px] p-4 bg-semantic-bg-primary rounded-lg shadow border border-semantic-bg-line flex">
-          <Icons.AlertTriangle className="w-6 h-6 stroke-semantic-warning-on-bg mr-4" />
-          <div className="grow shrink basis-0 flex-col justify-start items-start mr-4 space-y-4">
-            <div className="self-stretch flex-col justify-start items-start gap-1 flex">
-              <div className="self-stretch product-body-text-2-semibold ">{knowledgeBase.name} has been deleted</div>
-              <div className="self-stretch product-body-text-3-regular text-semantic-fg-secondary">
-                If this was a mistake, click "Undo Action" to reapply your changes.
+        <div className="fixed bottom-4 right-4 flex h-[136px] w-[400px] rounded-lg border border-semantic-bg-line bg-semantic-bg-primary p-4 shadow">
+          <Icons.AlertTriangle className="mr-4 h-6 w-6 stroke-semantic-warning-on-bg" />
+          <div className="mr-4 shrink grow basis-0 flex-col items-start justify-start space-y-4">
+            <div className="flex flex-col items-start justify-start gap-1 self-stretch">
+              <div className="self-stretch product-body-text-2-semibold ">
+                {knowledgeBase.name} has been deleted
+              </div>
+              <div className="self-stretch text-semantic-fg-secondary product-body-text-3-regular">
+                If this was a mistake, click "Undo Action" to reapply your
+                changes.
               </div>
             </div>
-            <LinkButton className="!p-0" variant="secondary" size="md" onClick={undoDelete}>
+            <LinkButton
+              className="!p-0"
+              variant="secondary"
+              size="md"
+              onClick={undoDelete}
+            >
               Undo Action
             </LinkButton>
           </div>
-          <Button className="absolute top-2 right-2" variant="tertiaryGrey" size="sm" onClick={handleCloseDeleteMessage}>
-            <Icons.X className="w-6 h-6 stroke-semantic-fg-secondary" />
+          <Button
+            className="absolute right-2 top-2"
+            variant="tertiaryGrey"
+            size="sm"
+            onClick={handleCloseDeleteMessage}
+          >
+            <Icons.X className="h-6 w-6 stroke-semantic-fg-secondary" />
           </Button>
         </div>
       )}
