f018ab1f9ff6d78efa25e7c2726752f79964b205
thewbuk <57405495+thewbuk@users.noreply.github.com>
2024-06-15 22:09:29 +0100

    PR fixes

--- packages/toolkit/src/lib/react-query-service/knowledge/useCreateKnowledgeBase.ts
+++ packages/toolkit/src/lib/react-query-service/knowledge/useCreateKnowledgeBase.ts
@@ -2,29 +2,31 @@ import { useMutation } from "@tanstack/react-query";
 import { createInstillAxiosClient } from "../../vdp-sdk/helper";
 import { KnowledgeBase } from "../../vdp-sdk/knowledge/types";
 
+async function createKnowledgeBaseMutation({
+  payload,
+  accessToken,
+}: {
+  payload: {
+    name: string;
+    description: string;
+    tags?: string[];
+  };
+  accessToken: string | null;
+}): Promise<KnowledgeBase> {
+  if (!accessToken) {
+    return Promise.reject(new Error("accessToken not provided"));
+  }
+  const client = createInstillAxiosClient(accessToken, true);
+  const response = await client.post<{
+    body: KnowledgeBase;
+    error_msg: string;
+    status_code: number;
+  }>(`/knowledge-base`, payload);
+  return response.data.body;
+}
+
 export function useCreateKnowledgeBase() {
   return useMutation({
-    mutationFn: async ({
-      payload,
-      accessToken,
-    }: {
-      payload: {
-        name: string;
-        description: string;
-        tags?: string[];
-      };
-      accessToken: string | null;
-    }) => {
-      if (!accessToken) {
-        return Promise.reject(new Error("accessToken not provided"));
-      }
-      const client = createInstillAxiosClient(accessToken, true);
-      const response = await client.post<{
-        body: KnowledgeBase;
-        error_msg: string;
-        status_code: number;
-      }>(`/knowledge-base`, payload);
-      return response.data.body;
-    },
+    mutationFn: createKnowledgeBaseMutation,
   });
 }
