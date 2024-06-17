d61bea0929b38bc1cf8e649b9f5859da12feea66
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2024-06-17 17:59:56 +0800

    fix: fix format

--- apps/console/src/app/sitemap.ts
+++ apps/console/src/app/sitemap.ts
@@ -16,7 +16,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   while (continueFetching) {
     try {
       const pipelineResponse = await fetch(
-        nextToken ? `${pipelinesUrl}&pageToken=${nextToken}` : pipelinesUrl
+        nextToken ? `${pipelinesUrl}&pageToken=${nextToken}` : pipelinesUrl,
       );
 
       if (pipelineResponse.ok) {
