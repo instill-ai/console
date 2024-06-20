a6ad9ef228aaa95647596c6da75332f017ec0a18
Alexander Petcoglo <mail@rckt.cc>
2024-06-20 18:02:31 +0300

    show 0 credits

--- packages/toolkit/src/components/top-bar/NamespaceSwitch.tsx
+++ packages/toolkit/src/components/top-bar/NamespaceSwitch.tsx
@@ -357,16 +357,14 @@ export const NamespaceSwitch = () => {
                           <p className="text-semantic-fg-primary product-body-text-3-medium">
                             {namespace.id}
                           </p>
-                          {namespace.remainingCredit ? (
-                            <Tag
-                              className="flex flex-row gap-x-1"
-                              variant="lightBlue"
-                              size="sm"
-                            >
-                              <Icons.Coins01 className="h-2.5 w-2.5 stroke-semantic-accent-default" />
-                              {`${namespace.remainingCredit.toLocaleString("en-US", { style: "decimal" })} credits left`}
-                            </Tag>
-                          ) : null}
+                          <Tag
+                            className="flex flex-row gap-x-1"
+                            variant="lightBlue"
+                            size="sm"
+                          >
+                            <Icons.Coins01 className="h-2.5 w-2.5 stroke-semantic-accent-default" />
+                            {`${namespace.remainingCredit.toLocaleString("en-US", { style: "decimal" })} credits left`}
+                          </Tag>
                         </div>
                       </div>
                     </Select.Item>
