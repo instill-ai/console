1bd9ea13b35e09b104eb6ff09bca2a0822650e1b
thewbuk <57405495+thewbuk@users.noreply.github.com>
2024-06-20 12:53:32 +0100

    code refactor start

--- packages/toolkit/src/view/knowledge/components/CatalogFilesTab.tsx
+++ packages/toolkit/src/view/knowledge/components/CatalogFilesTab.tsx
@@ -9,6 +9,7 @@ import {
 import { KnowledgeBase } from "../../../lib/vdp-sdk/knowledge/types";
 import * as React from "react";
 import { DELETE_FILE_TIMEOUT } from "./undoDeleteTime";
+import DeleteFileNotification from "./Notifications/DeleteFileNotification";
 
 type CatalogFilesTabProps = {
   knowledgeBase: KnowledgeBase;
@@ -287,35 +288,11 @@ export const CatalogFilesTab = ({ knowledgeBase }: CatalogFilesTabProps) => {
             </div>
           </div> */}
           {showDeleteMessage && deletedFile ? (
-            <div className="fixed bottom-4 right-8 flex h-[136px] w-[400px] rounded-lg border border-semantic-bg-line bg-semantic-bg-primary p-4 shadow">
-              <Icons.AlertTriangle className="mr-4 h-6 w-6 stroke-semantic-warning-on-bg" />
-              <div className="mr-4 shrink grow basis-0 flex-col items-start justify-start space-y-4">
-                <div className="flex flex-col items-start justify-start gap-1 self-stretch">
-                  <div className="self-stretch product-body-text-2-semibold">
-                    {deletedFile.fileName} has been deleted
-                  </div>
-                  <div className="self-stretch text-semantic-fg-secondary product-body-text-3-regular">
-                    If this was a mistake, click &quot;Undo Action&quot; to reapply your changes.
-                  </div>
-                </div>
-                <LinkButton
-                  className="!p-0"
-                  variant="secondary"
-                  size="md"
-                  onClick={undoDelete}
-                >
-                  Undo Action
-                </LinkButton>
-              </div>
-              <Button
-                className="absolute right-2 top-2"
-                variant="tertiaryGrey"
-                size="sm"
-                onClick={() => setShowDeleteMessage(false)}
-              >
-                <Icons.X className="h-6 w-6 stroke-semantic-fg-secondary" />
-              </Button>
-            </div>
+            <DeleteFileNotification
+              deletedFileName={deletedFile.fileName}
+              undoDelete={undoDelete}
+              setShowDeleteMessage={setShowDeleteMessage}
+            />
           ) : null}
         </div>
       </div>
