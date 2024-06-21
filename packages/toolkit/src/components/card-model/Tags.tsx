de866f8852e2592956ed5d9aca73156c0154c636
Alexander Petcoglo <mail@rckt.cc>
2024-06-21 18:55:37 +0300

    hide model visibility status if needed

--- packages/toolkit/src/components/card-model/Tags.tsx
+++ packages/toolkit/src/components/card-model/Tags.tsx
@@ -2,35 +2,38 @@
 
 import { Icons, Tag } from "@instill-ai/design-system";
 import React from "react";
+import { Visibility } from "../../lib";
 
 export type TagsProps = {
-  isPrivate: boolean;
   region: string;
   hardware: string;
+  visibilityStatus: Visibility | null;
 };
 
 export const Tags = (props: TagsProps) => {
-  const { isPrivate, region, hardware } = props;
+  const { visibilityStatus, region, hardware } = props;
 
   return (
     <div className="flex shrink-0 flex-row gap-x-2">
-      <Tag
-        variant="lightNeutral"
-        size="md"
-        className="gap-x-1.5 rounded-sm !py-0.5"
-      >
-        {isPrivate ? (
-          <React.Fragment>
-            <Icons.Lock03 className="h-2.5 w-2.5 stroke-semantic-fg-secondary" />
-            Private
-          </React.Fragment>
-        ) : (
-          <React.Fragment>
-            <Icons.BookOpen02 className="h-2.5 w-2.5 stroke-semantic-fg-secondary" />
-            Public
-          </React.Fragment>
-        )}
-      </Tag>
+      {visibilityStatus ? (
+        <Tag
+          variant="lightNeutral"
+          size="md"
+          className="gap-x-1.5 rounded-sm !py-0.5"
+        >
+          {visibilityStatus === "VISIBILITY_PRIVATE" ? (
+            <React.Fragment>
+              <Icons.Lock03 className="h-2.5 w-2.5 stroke-semantic-fg-secondary" />
+              Private
+            </React.Fragment>
+          ) : (
+            <React.Fragment>
+              <Icons.BookOpen02 className="h-2.5 w-2.5 stroke-semantic-fg-secondary" />
+              Public
+            </React.Fragment>
+          )}
+        </Tag>
+      ) : null}
       <Tag
         variant="lightNeutral"
         size="md"
