a48c160ace9b6ed92e89ce7c782bf188709a3c4b
Alexander Petcoglo <mail@rckt.cc>
2024-06-24 10:33:18 +0300

    type fix

--- packages/toolkit/src/view/model/ModelsTable.tsx
+++ packages/toolkit/src/view/model/ModelsTable.tsx
@@ -75,11 +75,7 @@ export const ModelsTable = (props: ModelsTableProps) => {
         return (
           <div className="grid justify-items-center">
             <StateLabel
-              state={
-                modelsWatchState.isSuccess
-                  ? modelsWatchState.data[name]?.state
-                  : "STATE_UNSPECIFIED"
-              }
+              state={(modelsWatchState.isSuccess && modelsWatchState.data[name]?.state) || "STATE_UNSPECIFIED"}
             />
           </div>
         );
