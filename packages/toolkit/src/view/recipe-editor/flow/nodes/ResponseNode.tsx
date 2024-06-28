075f286558e349caa77919ed496e5c79b060da90
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2024-06-28 16:40:37 +0800

    feat: add editor and viewer

---
+++ packages/toolkit/src/view/recipe-editor/flow/nodes/ResponseNode.tsx
@@ -0,0 +1,8 @@
+import { NodeProps } from "reactflow";
+
+import { ResponseNodeData } from "../../../pipeline-builder";
+
+export const ResponseNode = ({ data, id }: NodeProps<ResponseNodeData>) => {
+  console.log(data, id);
+  return <div />;
+};
