f73aa57f03602cc90e33e72c28c1a1d44060ef59
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2024-07-01 17:41:14 +0800

    feat: add more functionality

--- packages/toolkit/src/view/recipe-editor/RecipeEditorView.tsx
+++ packages/toolkit/src/view/recipe-editor/RecipeEditorView.tsx
@@ -12,6 +12,8 @@ import {
 } from "../../lib";
 import { Editor } from "./Editor";
 import { Flow } from "./flow";
+import { ComponentCmdk } from "./cmdk";
+import { EditorProvider } from "./EditorContext";
 
 const selector = (store: InstillStore) => ({
   accessToken: store.accessToken,
@@ -32,21 +34,24 @@ export const RecipeEditorView = () => {
     <PageBase>
       <AppTopbar topbarControllerChildren={<></>} disabledTopbarNav={true} />
       <PageBase.Container>
-        <div className="flex w-full h-[calc(100vh-var(--topbar-controller-height))] flex-row">
-          <Resizable.PanelGroup direction="horizontal" className="w-full">
-            <Resizable.Panel defaultSize={50} minSize={25}>
-              <Editor recipe={pipeline.data?.rawRecipe ?? null} />
-            </Resizable.Panel>
-            <Resizable.Handle />
-            <Resizable.Panel defaultSize={50} minSize={25}>
-              <Flow
-                pipelineId={pipeline.data?.id ?? null}
-                recipe={pipeline.data?.recipe ?? null}
-                pipelineMetadata={pipeline.data?.metadata ?? null}
-              />
-            </Resizable.Panel>
-          </Resizable.PanelGroup>
-        </div>
+        <EditorProvider>
+          <div className="flex h-[calc(100vh-var(--topbar-controller-height))] w-full flex-col">
+            <ComponentCmdk />
+            <Resizable.PanelGroup direction="horizontal" className="w-full">
+              <Resizable.Panel defaultSize={50} minSize={25}>
+                <Editor recipe={pipeline.data?.rawRecipe ?? null} />
+              </Resizable.Panel>
+              <Resizable.Handle />
+              <Resizable.Panel defaultSize={50} minSize={25}>
+                <Flow
+                  pipelineId={pipeline.data?.id ?? null}
+                  recipe={pipeline.data?.recipe ?? null}
+                  pipelineMetadata={pipeline.data?.metadata ?? null}
+                />
+              </Resizable.Panel>
+            </Resizable.PanelGroup>
+          </div>
+        </EditorProvider>
       </PageBase.Container>
     </PageBase>
   );
--- packages/toolkit/src/view/recipe-editor/flow/nodes/GeneralNode.tsx
+++ packages/toolkit/src/view/recipe-editor/flow/nodes/GeneralNode.tsx
@@ -1,21 +1,55 @@
-import { NodeProps } from "reactflow";
+"use client";
+import * as React from "react";
+
+import { NodeProps, Position, useEdges } from "reactflow";
 
 import { Icons } from "@instill-ai/design-system";
 
 import { ImageWithFallback } from "../../../../components";
-import { GeneralNodeData } from "../../../pipeline-builder";
+import { CustomHandle, GeneralNodeData } from "../../../pipeline-builder";
 
-export const GeneralNode = ({ data }: NodeProps<GeneralNodeData>) => {
+export const GeneralNode = ({ data, id }: NodeProps<GeneralNodeData>) => {
+  const reactflowEdges = useEdges();
+  const hasTargetEdges = React.useMemo(() => {
+    return reactflowEdges.some((edge) => edge.target === id);
+  }, [reactflowEdges, id]);
+
+  const hasSourceEdges = React.useMemo(() => {
+    return reactflowEdges.some((edge) => edge.source === id);
+  }, [id, reactflowEdges]);
+
   return (
-    <div className="w-20 border border-semantic-bg-line flex items-center justify-center h-20 rounded-md bg-semantic-bg-primary">
-      <ImageWithFallback
-        src={`/icons/${data.definition?.id}.svg`}
-        width={32}
-        height={32}
-        alt={`${data.definition?.title}-icon`}
-        fallbackImg={
-          <Icons.Box className="my-auto h-8 w-8 stroke-semantic-fg-primary" />
-        }
+    <div
+      onClick={() => {}}
+      className="flex w-[320px] flex-col rounded-md border border-semantic-bg-line bg-semantic-bg-primary p-4"
+    >
+      <div className="flex flex-row items-center gap-x-2">
+        <div className="flex rounded-sm border border-semantic-bg-line p-2">
+          <ImageWithFallback
+            src={`/icons/${data.definition?.id}.svg`}
+            width={32}
+            height={32}
+            alt={`${data.definition?.title}-icon`}
+            fallbackImg={
+              <Icons.Box className="my-auto h-8 w-8 stroke-semantic-fg-primary" />
+            }
+          />
+        </div>
+        <p className="productbody-text-2-medium text-semantic-fg-primary">
+          {id}
+        </p>
+      </div>
+      <CustomHandle
+        className={hasTargetEdges ? "" : "!opacity-0"}
+        type="target"
+        position={Position.Left}
+        id={id}
+      />
+      <CustomHandle
+        className={hasSourceEdges ? "" : "!opacity-0"}
+        type="source"
+        position={Position.Right}
+        id={id}
       />
     </div>
   );
