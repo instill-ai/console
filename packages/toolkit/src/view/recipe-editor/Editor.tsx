d03caa6203fbcaf1236d65f90a2912ebee828262
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2024-07-01 17:41:14 +0800

    feat: add more functionality

--- packages/toolkit/src/view/recipe-editor/Editor.tsx
+++ packages/toolkit/src/view/recipe-editor/Editor.tsx
@@ -7,10 +7,71 @@ import { langs } from "@uiw/codemirror-extensions-langs";
 import { githubLight } from "@uiw/codemirror-theme-github";
 import CodeMirror from "@uiw/react-codemirror";
 
-import { Nullable } from "../../lib";
+import {
+  InstillStore,
+  Nullable,
+  useInstillStore,
+  useRouteInfo,
+  useShallow,
+  useUpdateUserPipeline,
+} from "../../lib";
 import { validateYaml } from "./validateYaml";
+import debounce from "lodash.debounce";
+import { useEditor } from "./EditorContext";
+import { PrettifyButton } from "./PrettifyButton";
+
+const selector = (store: InstillStore) => ({
+  accessToken: store.accessToken,
+});
 
 export const Editor = ({ recipe }: { recipe: Nullable<string> }) => {
+  const { editorRef } = useEditor();
+  const [schemaIsWrong, setSchemaIsWrong] = React.useState(false);
+  const routeInfo = useRouteInfo();
+  const updatePipeline = useUpdateUserPipeline();
+  const { accessToken } = useInstillStore(useShallow(selector));
+
+  const recipeUpdater = React.useCallback(
+    debounce(
+      ({
+        pipelineName,
+        rawRecipe,
+        accessToken,
+      }: {
+        pipelineName: string;
+        rawRecipe: string;
+        accessToken: Nullable<string>;
+      }) => {
+        if (!accessToken) {
+          return;
+        }
+
+        const diagnostics = validateYaml(rawRecipe, true);
+
+        if (diagnostics.length > 0) {
+          setSchemaIsWrong(true);
+          return;
+        }
+
+        setSchemaIsWrong(false);
+
+        try {
+          updatePipeline.mutateAsync({
+            payload: {
+              rawRecipe,
+              name: pipelineName,
+            },
+            accessToken,
+          });
+        } catch (error) {
+          console.error(error);
+        }
+      },
+      1500
+    ),
+    [updatePipeline]
+  );
+
   return (
     <React.Fragment>
       <style jsx>
@@ -20,8 +81,13 @@ export const Editor = ({ recipe }: { recipe: Nullable<string> }) => {
           }
         `}
       </style>
-      <div className="w-full h-full">
+      <div className="h-full w-full">
+        <div className="flex h-10 flex-row">
+          <PrettifyButton />
+          {schemaIsWrong ? <p>These has issue in the recipe</p> : null}
+        </div>
         <CodeMirror
+          ref={editorRef}
           className="h-full"
           value={recipe ?? ""}
           extensions={[
@@ -31,6 +97,19 @@ export const Editor = ({ recipe }: { recipe: Nullable<string> }) => {
             EditorView.lineWrapping,
           ]}
           theme={githubLight}
+          onChange={(value, view) => {
+            if (!routeInfo.isSuccess || !routeInfo.data.pipelineName) {
+              return;
+            }
+
+            console.log(view.state.doc.toString());
+
+            recipeUpdater({
+              rawRecipe: value,
+              accessToken,
+              pipelineName: routeInfo.data.pipelineName,
+            });
+          }}
         />
       </div>
     </React.Fragment>
