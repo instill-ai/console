f42ac87564a65003e60f647343fd812722b19c84
thewbuk <57405495+thewbuk@users.noreply.github.com>
2024-07-09 13:20:34 +0100

    refactor: Update knowledge base creation logic

--- packages/toolkit/src/lib/react-query-service/knowledge/useDeleteKnowledgeBase.ts
+++ packages/toolkit/src/lib/react-query-service/knowledge/useDeleteKnowledgeBase.ts
@@ -1,22 +1,24 @@
 import { useMutation } from "@tanstack/react-query";
 import { createInstillAxiosClient } from "../../vdp-sdk/helper";
+import { KnowledgeBase } from "../../vdp-sdk/knowledge/types";
 
 async function deleteKnowledgeBaseMutation({
-  id,
+  ownerId,
+  kbId,
   accessToken,
 }: {
-  id: string;
+  ownerId: string;
+  kbId: string;
   accessToken: string | null;
-}): Promise<{ error_msg: string; status_code: number }> {
+}): Promise<KnowledgeBase> {
   if (!accessToken) {
     return Promise.reject(new Error("accessToken not provided"));
   }
   const client = createInstillAxiosClient(accessToken, true);
   const response = await client.delete<{
-    error_msg: string;
-    status_code: number;
-  }>(`/knowledge-base/${id}`);
-  return response.data;
+    knowledge_base: KnowledgeBase;
+  }>(`/owners/${ownerId}/knowledge-bases/${kbId}`);
+  return response.data.knowledge_base;
 }
 
 export function useDeleteKnowledgeBase() {
