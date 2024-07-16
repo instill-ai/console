cbacfe2408c266bc9a2bcfbae147fb8c547dd74a
thewbuk <57405495+thewbuk@users.noreply.github.com>
2024-07-16 10:14:59 +0100

    add limit to number of kb created

--- packages/toolkit/src/view/knowledge/components/KnowledgeBaseCard.tsx
+++ packages/toolkit/src/view/knowledge/components/KnowledgeBaseCard.tsx
@@ -2,13 +2,21 @@ import { Icons } from "@instill-ai/design-system";
 
 type KnowledgeBaseCardProps = {
   onClick?: () => void;
+  disabled?: boolean;
 };
 
-export const KnowledgeBaseCard = ({ onClick }: KnowledgeBaseCardProps) => {
+export const KnowledgeBaseCard = ({ onClick, disabled = false }: KnowledgeBaseCardProps) => {
+  const cardContent = disabled
+    ? "You have reached the maximum limit of 3 knowledge bases that can be created. Please consider deleting any unused knowledge bases if you wish to create a new one."
+    : "Import your own text data or write data in real-time via Webhook for LLM context enhancement.";
+
   return (
     <div
-      className="flex h-[175px] w-[360px] cursor-pointer flex-col gap-y-2.5 rounded-md border border-semantic-bg-line bg-semantic-bg-base-bg p-6"
-      onClick={onClick}
+      className={`flex h-[175px] w-[360px] flex-col gap-y-2.5 rounded-md border border-semantic-bg-line p-6 ${disabled
+        ? "bg-semantic-bg-secondary cursor-not-allowed"
+        : "bg-semantic-bg-base-bg cursor-pointer"
+        }`}
+      onClick={disabled ? undefined : onClick}
     >
       <div className="flex items-center gap-x-2">
         <Icons.Plus className="h-4 w-4 stroke-semantic-fg-secondary" />
@@ -17,8 +25,7 @@ export const KnowledgeBaseCard = ({ onClick }: KnowledgeBaseCardProps) => {
         </h3>
       </div>
       <p className="text-semantic-fg-secondary product-body-text-3-regular">
-        Import your own text data or write data in real-time via Webhook for LLM
-        context enhancement.
+        {cardContent}
       </p>
     </div>
   );
