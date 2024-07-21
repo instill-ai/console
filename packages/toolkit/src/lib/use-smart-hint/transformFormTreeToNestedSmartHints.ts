87ea9edae5a56887ce666ff13734c62a2edb3a67
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2024-07-21 15:49:14 +0800

    feat: make autocomplete work

---
+++ packages/toolkit/src/lib/use-smart-hint/transformFormTreeToNestedSmartHints.ts
@@ -0,0 +1,42 @@
+import { GeneralRecord } from "../type";
+import { InstillFormTree } from "../use-instill-form";
+
+export function transformFormTreeToNestedSmartHints(
+  tree: InstillFormTree,
+): GeneralRecord {
+  if (tree._type === "formGroup") {
+    const properties = Object.fromEntries(
+      tree.properties.map((value) => {
+        return [
+          value.fieldKey,
+          {
+            ...transformFormTreeToNestedSmartHints(value),
+          },
+        ];
+      }),
+    );
+
+    return properties;
+  }
+
+  if (tree._type === "objectArray" && tree.fieldKey) {
+    return {
+      ...transformFormTreeToNestedSmartHints(tree.properties),
+      type: "objectArray",
+    };
+  }
+
+  if (tree._type === "arrayArray") {
+    return {};
+  }
+
+  if (tree._type === "formCondition") {
+    return {};
+  }
+
+  return {
+    key: tree.fieldKey ?? "null",
+    instillFormat: tree.instillFormat ?? "null",
+    description: tree.description,
+  };
+}
