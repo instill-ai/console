fb2a25225e0ffcbc107386d5bb5ff279a1257b6d
thewbuk <57405495+thewbuk@users.noreply.github.com>
2024-07-29 10:39:39 +0100

    add artifact client to api calls

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -1,4 +1,4 @@
-export { InstillAPIClient } from "./main";
+export { InstillAPIClient, instillArtifactAPIClient } from "./main";
 export type * from "./core";
 export type * from "./model";
 export type * from "./vdp";
