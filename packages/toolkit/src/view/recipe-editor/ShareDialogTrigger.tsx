5582fe6beda07c2706830d562f072242cd1a3fd5
Po Chun Chiu <57251712+EiffelFly@users.noreply.github.com>
2024-08-06 15:06:41 +0800

    feat: add topbar elements (#1350)
    Because
    
    - add topbar elements
    
    This commit
    
    - add topbar elements

---
+++ packages/toolkit/src/view/recipe-editor/ShareDialogTrigger.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import * as React from "react";
+
+import { Button, Icons } from "@instill-ai/design-system";
+
+import {
+  InstillStore,
+  useInstillStore,
+  useNamespacePipeline,
+  useRouteInfo,
+  useShallow,
+} from "../../lib";
+import { SharePipelineDialog } from "../pipeline-builder";
+
+const selector = (store: InstillStore) => ({
+  accessToken: store.accessToken,
+  enabledQuery: store.enabledQuery,
+  updateDialogSharePipelineIsOpen: store.updateDialogSharePipelineIsOpen,
+});
+
+export const ShareDialogTrigger = () => {
+  const { accessToken, enabledQuery, updateDialogSharePipelineIsOpen } =
+    useInstillStore(useShallow(selector));
+
+  const routeInfo = useRouteInfo();
+
+  const pipeline = useNamespacePipeline({
+    namespacePipelineName: routeInfo.data.pipelineName,
+    accessToken,
+    enabled: enabledQuery && routeInfo.isSuccess,
+  });
+
+  return (
+    <React.Fragment>
+      <Button
+        size="md"
+        variant="secondaryGrey"
+        className="flex !h-8 flex-row gap-x-2"
+        onClick={() => updateDialogSharePipelineIsOpen((prev) => !prev)}
+      >
+        Share
+        <Icons.Share07 className="my-auto h-4 w-4 stroke-semantic-fg-primary" />
+      </Button>
+
+      {pipeline.isSuccess ? (
+        <SharePipelineDialog
+          pipelineName={routeInfo.data.pipelineName}
+          namespaceId={routeInfo.data.namespaceId}
+          id={routeInfo.data.resourceId}
+          ownerDisplayName={
+            "user" in pipeline.data.owner
+              ? (pipeline.data.owner.user.profile?.displayName ?? null)
+              : (pipeline.data.owner.organization.profile?.displayName ?? null)
+          }
+        />
+      ) : null}
+    </React.Fragment>
+  );
+};
