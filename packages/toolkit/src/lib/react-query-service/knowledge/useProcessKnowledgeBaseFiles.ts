f3c5469882948563658f4177b50c107dc9f62dc7
thewbuk <57405495+thewbuk@users.noreply.github.com>
2024-08-10 18:51:05 +0100

    add geaders to process file

--- packages/toolkit/src/lib/react-query-service/knowledge/useProcessKnowledgeBaseFiles.ts
+++ packages/toolkit/src/lib/react-query-service/knowledge/useProcessKnowledgeBaseFiles.ts
@@ -1,8 +1,8 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-
 import { Nullable } from "../../type";
 import { createInstillAxiosClient } from "../../vdp-sdk/helper";
 import { File } from "./types";
+import { getInstillAdditionalHeaders } from "instill-sdk";
 
 export function useProcessKnowledgeBaseFiles() {
   const queryClient = useQueryClient();
@@ -23,16 +23,21 @@ export function useProcessKnowledgeBaseFiles() {
       if (!namespace) {
         return Promise.reject(new Error("namespace not provided"));
       }
+
       const client = createInstillAxiosClient(accessToken, true);
+
+      const additionalHeaders = getInstillAdditionalHeaders({
+        requesterUid: namespace,
+      });
+
       const response = await client.post<{ files: File[] }>(
         `/catalogs/files/processAsync`,
         { file_uids: fileUids },
-        // {
-        //   headers: {
-        //     Requester: namespace,
-        //   },
-        // },
+        {
+          headers: additionalHeaders,
+        }
       );
+
       return response.data.files;
     },
     onSuccess: () => {
