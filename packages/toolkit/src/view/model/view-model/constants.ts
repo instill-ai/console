bba7d9de68a0c80a5b243059bb224e0243d8522d
Wojciech Bandzerewicz <57405495+thewbuk@users.noreply.github.com>
2024-08-28 16:28:22 +0100

    formatting

--- packages/toolkit/src/view/model/view-model/constants.ts
+++ packages/toolkit/src/view/model/view-model/constants.ts
@@ -3,18 +3,21 @@ import { ModelState, Nullable } from "instill-sdk";
 export const OPERATION_POLL_TIMEOUT = 5000;
 export const TABLE_PAGE_SIZE = 10;
 
-export const getStatusMessage = (state: Nullable<ModelState>, hardwareType: string) => {
-    switch (state) {
-        case "STATE_OFFLINE":
-            return "Model deployment takes time. Credits won't be consumed during this period. Please wait.";
-        case "STATE_ACTIVE":
-        case "STATE_SCALING_DOWN":
-        case "STATE_STARTING":
-        case "STATE_IDLE":
-            return "Your request is being processed, please hold on.";
-        case "STATE_SCALING_UP":
-            return `The requested ${hardwareType || ""} compute resource is being provisioned, please wait.`;
-        default:
-            return "Our CPU/GPU is powering up—it might take a moment.";
-    }
+export const getStatusMessage = (
+  state: Nullable<ModelState>,
+  hardwareType: string,
+) => {
+  switch (state) {
+    case "STATE_OFFLINE":
+      return "Model deployment takes time. Credits won't be consumed during this period. Please wait.";
+    case "STATE_ACTIVE":
+    case "STATE_SCALING_DOWN":
+    case "STATE_STARTING":
+    case "STATE_IDLE":
+      return "Your request is being processed, please hold on.";
+    case "STATE_SCALING_UP":
+      return `The requested ${hardwareType || ""} compute resource is being provisioned, please wait.`;
+    default:
+      return "Our CPU/GPU is powering up—it might take a moment.";
+  }
 };
