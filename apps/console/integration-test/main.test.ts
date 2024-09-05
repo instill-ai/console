13f2f741d2645e30dfb49d3fe1eadf1afe1881a6
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2024-09-05 13:15:39 +0800

    chore: disable e2e test

--- apps/console/integration-test/main.test.ts
+++ apps/console/integration-test/main.test.ts
@@ -1,9 +1,10 @@
-import { test } from "@playwright/test";
+// import { test } from "@playwright/test";
 
-import { shouldChangeComponentID } from "./tests/should-change-component-id.test";
-import { shouldEditAndCreateStartAndEndOperatorField } from "./tests/should-edit-create-start-end-field.test";
-import { shouldUnmarshalJSONInput } from "./tests/should-unmarshal.test";
+// import { shouldChangeComponentID } from "./tests/should-change-component-id.test";
+// import { shouldEditAndCreateStartAndEndOperatorField } from "./tests/should-edit-create-start-end-field.test";
+// import { shouldUnmarshalJSONInput } from "./tests/should-unmarshal.test";
 
-test.describe(shouldChangeComponentID);
-test.describe(shouldUnmarshalJSONInput);
-test.describe(shouldEditAndCreateStartAndEndOperatorField);
+// We don't have the no-code builder anymore
+// test.describe(shouldChangeComponentID);
+// test.describe(shouldUnmarshalJSONInput);
+// test.describe(shouldEditAndCreateStartAndEndOperatorField);
