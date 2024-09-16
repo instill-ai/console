05dbd60c5de6cd8cc4f961c804fe3f59f70bd692
thewbuk <57405495+thewbuk@users.noreply.github.com>
2024-09-17 00:40:04 +0100

    final redirect update

--- apps/console/src/app/[entity]/models/[id]/page.tsx
+++ apps/console/src/app/[entity]/models/[id]/page.tsx
@@ -1,43 +1,36 @@
-import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
-
-import { Nullable } from "@instill-ai/toolkit";
-import { fetchNamespaceType, fetchUserModel } from "@instill-ai/toolkit/server";
+import { redirect } from 'next/navigation';
+import { cookies } from 'next/headers';
+import { Nullable } from '@instill-ai/toolkit';
+import { fetchNamespaceType, fetchUserModel } from '@instill-ai/toolkit/server';
 
 type RedirectionModelPageProps = {
   params: { id: string; entity: string };
 };
 
-async function getModelData(
-  entity: string,
-  id: string,
-  accessToken: Nullable<string>,
-) {
+async function getModelData(entity: string, id: string, accessToken: Nullable<string>) {
   try {
     const namespaceType = await fetchNamespaceType({
       namespace: entity,
       accessToken,
     });
-    if (
-      namespaceType === "NAMESPACE_USER" ||
-      namespaceType === "NAMESPACE_ORGANIZATION"
-    ) {
+    if (namespaceType === "NAMESPACE_USER" || namespaceType === "NAMESPACE_ORGANIZATION") {
       const namespaceName = `${namespaceType === "NAMESPACE_USER" ? "users" : "organizations"}/${entity}`;
-      return await fetchUserModel({
+      const modelData = await fetchUserModel({
         modelName: `${namespaceName}/models/${id}`,
         accessToken,
       });
+      if (modelData) {
+        return modelData;
+      }
     }
+    return Promise.reject(null);
   } catch (error) {
     console.error(error);
-    return null;
+    return Promise.reject(null);
   }
-  return null; // Return null if namespaceType doesn't match
 }
 
-export default async function RedirectionModelPage({
-  params,
-}: RedirectionModelPageProps) {
+export default async function RedirectionModelPage({ params }: RedirectionModelPageProps) {
   const { entity, id } = params;
 
   const cookieStore = cookies();
@@ -48,17 +41,11 @@ export default async function RedirectionModelPage({
   }
 
   try {
-    const modelData = await getModelData(entity, id, accessToken);
-
-    // Redirect to 404 if the model doesn't exist
-    if (!modelData) {
-      return redirect("/404");
-    }
-
+    await getModelData(entity, id, accessToken);
     // If the model exists, redirect to the playground
     return redirect(`/${entity}/models/${id}/playground`);
   } catch (error) {
     console.error("Error in RedirectionModelPage:", error);
-    return redirect("/404"); // Redirect to 404 on any error
+    return redirect('/404'); // Redirect to 404 if the model doesn't exist or on any error
   }
 }
--- apps/console/src/app/[entity]/pipelines/[id]/page.tsx
+++ apps/console/src/app/[entity]/pipelines/[id]/page.tsx
@@ -1,46 +1,36 @@
-import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
-
-import { Nullable } from "@instill-ai/toolkit";
-import {
-  fetchNamespacePipeline,
-  fetchNamespaceType,
-} from "@instill-ai/toolkit/server";
+import { redirect } from 'next/navigation';
+import { cookies } from 'next/headers';
+import { fetchNamespacePipeline, fetchNamespaceType } from '@instill-ai/toolkit/server';
+import { Nullable } from '@instill-ai/toolkit';
 
 type RedirectionPipelinePageProps = {
   params: { id: string; entity: string };
 };
 
-async function getPipelineData(
-  entity: string,
-  id: string,
-  accessToken: Nullable<string>,
-) {
+async function getPipelineData(entity: string, id: string, accessToken: Nullable<string>) {
   try {
     const namespaceType = await fetchNamespaceType({
       namespace: entity,
       accessToken,
     });
-    if (
-      namespaceType === "NAMESPACE_USER" ||
-      namespaceType === "NAMESPACE_ORGANIZATION"
-    ) {
+    if (namespaceType === "NAMESPACE_USER" || namespaceType === "NAMESPACE_ORGANIZATION") {
       const namespaceName = `${namespaceType === "NAMESPACE_USER" ? "users" : "organizations"}/${entity}`;
-      return await fetchNamespacePipeline({
+      const pipelineData = await fetchNamespacePipeline({
         namespacePipelineName: `${namespaceName}/pipelines/${id}`,
         accessToken,
       });
+      if (pipelineData) {
+        return pipelineData;
+      }
     }
+    return Promise.reject(null);
   } catch (error) {
     console.error(error);
-    return null;
+    return Promise.reject(null);
   }
-  return null; // Return null if namespaceType doesn't match
 }
 
-export default async function RedirectionPipelinePage({
-  params,
-}: RedirectionPipelinePageProps) {
+export default async function RedirectionPipelinePage({ params }: RedirectionPipelinePageProps) {
   const { entity, id } = params;
 
   const cookieStore = cookies();
@@ -51,17 +41,11 @@ export default async function RedirectionPipelinePage({
   }
 
   try {
-    const pipelineData = await getPipelineData(entity, id, accessToken);
-
-    // Redirect to 404 if the pipeline doesn't exist
-    if (!pipelineData) {
-      return redirect("/404");
-    }
-
+    await getPipelineData(entity, id, accessToken);
     // If the pipeline exists, redirect to the playground
     return redirect(`/${entity}/pipelines/${id}/playground`);
   } catch (error) {
     console.error("Error in RedirectionPipelinePage:", error);
-    return redirect("/404"); // Redirect to 404 on any error
+    return redirect('/404'); // Redirect to 404 if the pipeline doesn't exist or on any error
   }
 }
