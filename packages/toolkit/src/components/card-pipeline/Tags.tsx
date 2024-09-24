5a3cec72902b884cff7f7ff3fe69a28c7c3b0800
Wojciech Bandzerewicz <57405495+thewbuk@users.noreply.github.com>
2024-09-24 10:31:28 +0100

    formatting

--- packages/toolkit/src/components/card-pipeline/Tags.tsx
+++ packages/toolkit/src/components/card-pipeline/Tags.tsx
@@ -16,7 +16,7 @@ export const Tags = (props: TagsProps) => {
           size="sm"
           className={cn(
             "gap-x-1.5 !py-0.5 rounded-full product-body-text-4-medium text-semantic-fg-secondary",
-            title?.toLowerCase() === 'featured' ? '!capitalize' : '!lowercase'
+            title?.toLowerCase() === "featured" ? "!capitalize" : "!lowercase",
           )}
         >
           {title}
