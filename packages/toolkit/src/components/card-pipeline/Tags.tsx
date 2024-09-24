3931d5bf42f84fe90192e06abc3ffae46576152e
thewbuk <57405495+thewbuk@users.noreply.github.com>
2024-09-24 09:53:43 +0100

    lowercase tags

--- packages/toolkit/src/components/card-pipeline/Tags.tsx
+++ packages/toolkit/src/components/card-pipeline/Tags.tsx
@@ -14,7 +14,7 @@ export const Tags = (props: TagsProps) => {
           key={index}
           variant="lightNeutral"
           size="sm"
-          className="gap-x-1.5 !py-0.5 rounded-full product-body-text-4-medium text-semantic-fg-secondary"
+          className="gap-x-1.5 !py-0.5 rounded-full product-body-text-4-medium text-semantic-fg-secondary lowercase"
         >
           {title}
         </Tag>
