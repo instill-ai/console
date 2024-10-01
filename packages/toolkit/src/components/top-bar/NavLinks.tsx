f642ec7cefaf49d1a066fd84243ae1b31cd7c1e4
Wojciech Bandzerewicz <57405495+thewbuk@users.noreply.github.com>
2024-10-01 10:34:30 +0100

    formatting

--- packages/toolkit/src/components/top-bar/NavLinks.tsx
+++ packages/toolkit/src/components/top-bar/NavLinks.tsx
@@ -140,17 +140,17 @@ export const NavLinks = ({ isExploreRoute }: { isExploreRoute?: boolean }) => {
     <React.Fragment>
       {me.isSuccess
         ? navLinkItems
-          .filter((item) => isCloud || item.pathname !== "applications")
-          .map(({ pathname, Icon, title }) => (
-            <NavLink
-              key={pathname}
-              pathname={pathname}
-              Icon={Icon}
-              title={title}
-              isExploreRoute={isExploreRoute}
-              strict={true}
-            />
-          ))
+            .filter((item) => isCloud || item.pathname !== "applications")
+            .map(({ pathname, Icon, title }) => (
+              <NavLink
+                key={pathname}
+                pathname={pathname}
+                Icon={Icon}
+                title={title}
+                isExploreRoute={isExploreRoute}
+                strict={true}
+              />
+            ))
         : null}
     </React.Fragment>
   );
