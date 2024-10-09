031dd54379a555cc77814b0b4cea99d15b371bbd
Wojciech Bandzerewicz <57405495+thewbuk@users.noreply.github.com>
2024-10-09 14:04:12 +0100

    set base url

--- packages/toolkit/src/lib/vdp-sdk/index.ts
+++ packages/toolkit/src/lib/vdp-sdk/index.ts
@@ -57,10 +57,10 @@ export function getInstillModelAPIClient({
 
 export function getInstillApplicationAPIClient({ accessToken }: { accessToken?: string }) {
   if (!instillApplicationAPIClient) {
-    const baseURL = `${process.env.NEXT_SERVER_API_GATEWAY_URL ??
-      env("NEXT_PUBLIC_API_GATEWAY_URL")
-      }/${env("NEXT_PUBLIC_APPLICATION_API_VERSION")}`;
-
+    // const baseURL = `${process.env.NEXT_SERVER_API_GATEWAY_URL ??
+    //   env("NEXT_PUBLIC_API_GATEWAY_URL")
+    //   }/${env("NEXT_PUBLIC_MODEL_API_VERSION")}`;
+    const baseURL = `v1alpha`;
     instillApplicationAPIClient = new InstillAPIClient({
       baseURL,
       apiToken: accessToken,
