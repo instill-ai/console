4c3291a0f4c0cf22f1c2e14d91989c276c8835a7
Wojciech Bandzerewicz <57405495+thewbuk@users.noreply.github.com>
2024-10-15 12:39:13 +0100

    remove

--- packages/sdk/src/application/ApplicationClient.ts
+++ packages/sdk/src/application/ApplicationClient.ts
@@ -448,16 +448,14 @@ export class ApplicationClient extends APIResource {
 
   async chat(
     props: ChatRequest & { stream: true },
-  ): Promise<ReadableStream<Uint8Array>>;
+  ): Promise<ChatWithStreamResponse>;
   async chat(props: ChatRequest & { stream: false }): Promise<ChatResponse>;
   async chat(
     props: ChatRequest & { stream?: undefined },
   ): Promise<ChatResponse>;
   async chat(
     props: ChatRequest & { stream?: boolean },
-  ): Promise<
-    ChatResponse | ReadableStream<Uint8Array> | ChatWithStreamResponse
-  > {
+  ): Promise<ChatResponse | ChatWithStreamResponse> {
     const { ownerId, stream, appId, requesterUid, ...payload } = props;
     if (!ownerId) {
       throw new Error("Required parameter missing: ownerId");
