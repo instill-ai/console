e40d8ebf0edd0efc53a0d0568267a43ba5a91bb1
Alexander Petcoglo <mail@rckt.cc>
2024-10-15 15:41:28 +0200

    fix: fix formatting

--- packages/toolkit/src/lib/clientCookies.ts
+++ packages/toolkit/src/lib/clientCookies.ts
@@ -1,19 +1,23 @@
-export const clientCookieSet = (cname: string, cvalue: string, exdays: number = 30) => {
+export const clientCookieSet = (
+  cname: string,
+  cvalue: string,
+  exdays: number = 30,
+) => {
   const d = new Date();
-  
-  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-  
-  const expires = "expires="+ d.toUTCString();
-  
+
+  d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
+
+  const expires = "expires=" + d.toUTCString();
+
   document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-}
+};
 
 export const clientCookieGet = (cname: string) => {
   const name = cname + "=";
   const decodedCookie = decodeURIComponent(document.cookie);
-  const ca = decodedCookie.split(';');
-  
-  for(let i = 0; i < ca.length; i++) {
+  const ca = decodedCookie.split(";");
+
+  for (let i = 0; i < ca.length; i++) {
     let c = ca[i];
 
     if (!c) {
@@ -21,11 +25,11 @@ export const clientCookieGet = (cname: string) => {
     }
 
     c = c.trim();
-    
+
     if (c.indexOf(name) === 0) {
       return c.substring(name.length, c.length);
     }
   }
-  
+
   return null;
-}
+};
