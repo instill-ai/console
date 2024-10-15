24c54503975af7ccaccf4a15e89d3105e323fd1d
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2024-10-15 16:09:20 +0800

    fix: refactor onboarding form and align topdown and profile page with cloud

--- apps/console/src/components/OnboardingForm.tsx
+++ apps/console/src/components/OnboardingForm.tsx
@@ -1,304 +1,259 @@
 "use client";
 
-import { ChangeEvent, useCallback, useState } from "react";
+import * as React from "react";
 import { useRouter } from "next/navigation";
-import axios from "axios";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
 import { v4 as uuidv4 } from "uuid";
+import { z } from "zod";
 
-import type { AuthenticatedUser, Nullable } from "@instill-ai/toolkit";
 import {
-  BasicProgressMessageBox,
-  BasicSingleSelect,
-  BasicTextField,
-  BasicToggleField,
-  FormRoot,
-  ProgressMessageBoxState,
-  SelectOption,
-  SolidButton,
+  Button,
+  Form,
+  Input,
+  Select,
+  Switch,
+  useToast,
 } from "@instill-ai/design-system";
 import {
   instillUserRoles,
-  sendAmplitudeData,
-  useAmplitudeCtx,
+  toastInstillError,
   useInstillStore,
   useUpdateAuthenticatedUser,
 } from "@instill-ai/toolkit";
 
-export type OnboardingFormValues = {
-  email: Nullable<string>;
-  companyName: Nullable<string>;
-  role: Nullable<string>;
-  newsletterSubscription: Nullable<boolean>;
-};
-
-type OnboardingFormErrors = {
-  email: Nullable<string>;
-  companyName: Nullable<string>;
-  role: Nullable<string>;
-};
+const OnboardingFormSchema = z.object({
+  displayName: z.string().min(1, "Display name is required"),
+  email: z.string().email(),
+  role: z.string(),
+  companyName: z.string().min(1, "Company name is required"),
+  newsletterSubscription: z.boolean(),
+});
 
 export const OnboardingForm = () => {
   const router = useRouter();
-  const updateUser = useUpdateAuthenticatedUser();
-  const { amplitudeIsInit } = useAmplitudeCtx();
-
+  const { toast } = useToast();
+  const [isSubmitting, setIsSubmitting] = React.useState(false);
   const accessToken = useInstillStore((store) => store.accessToken);
-
-  const [fieldValues, setFieldValues] = useState<OnboardingFormValues>({
-    email: null,
-    companyName: null,
-    role: null,
-    newsletterSubscription: true,
-  });
-
-  const [selectedRoleOption, setSelectedRoleOption] = useState<
-    Nullable<SelectOption>
-  >(instillUserRoles.find((e) => e.value === fieldValues.role) || null);
-
-  const [messageBoxState, setMessageBoxState] =
-    useState<ProgressMessageBoxState>({
-      activate: false,
-      message: null,
-      description: null,
-      status: null,
-    });
-
-  // Handle fields change and per-field validation here
-
-  const handleFieldChange = useCallback(
-    (key: keyof OnboardingFormValues, event: ChangeEvent<HTMLInputElement>) => {
-      const value =
-        key === "newsletterSubscription"
-          ? event.target.checked
-          : event.target.value;
-
-      if (key === "email") {
-        let error: Nullable<string> = null;
-
-        if (!value) {
-          error = "Email is required";
-        } else {
-          if (
-            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(
-              event.target.value,
-            )
-          ) {
-            error = "Invalid email address";
-          }
-        }
-        setFieldErrors((prev) => ({
-          ...prev,
-          email: error,
-        }));
-      }
-
-      if (key === "companyName") {
-        let error: Nullable<string> = null;
-        if (!value) {
-          error = "Company name is required";
-        }
-        setFieldErrors((prev) => ({
-          ...prev,
-          orgName: error,
-        }));
-      }
-
-      setFieldValues((prev) => ({
-        ...prev,
-        [key]:
-          key === "newsletterSubscription"
-            ? event.target.checked
-            : event.target.value,
-      }));
+  const form = useForm<z.infer<typeof OnboardingFormSchema>>({
+    resolver: zodResolver(OnboardingFormSchema),
+    defaultValues: {
+      displayName: "",
+      email: "",
+      role: "Creative Production",
+      companyName: "",
+      newsletterSubscription: false,
     },
-    [],
-  );
-
-  const handleRoleChange = useCallback((option: Nullable<SelectOption>) => {
-    if (!option) return;
-    setSelectedRoleOption(option);
-
-    let error: Nullable<string> = null;
-
-    if (!option.value) {
-      error = "Role is required";
-    }
-
-    setFieldErrors((prev) => ({
-      ...prev,
-      role: error,
-    }));
-
-    setFieldValues((prev) => ({
-      ...prev,
-      role: option.value as string,
-    }));
-  }, []);
-
-  // Validate form and deal with error handling
-
-  const [fieldErrors, setFieldErrors] = useState<OnboardingFormErrors>({
-    email: null,
-    companyName: null,
-    role: null,
   });
 
-  const handleSubmit = useCallback(async () => {
-    if (!accessToken) {
-      return;
-    }
-
-    const errors = {} as OnboardingFormErrors;
-
-    if (!fieldValues.email) {
-      errors["email"] = "Email is required";
-    } else {
-      if (
-        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(fieldValues.email)
-      ) {
-        errors["email"] = "Invalid email address";
-      }
-    }
-
-    if (!fieldValues.companyName) {
-      errors["companyName"] = "Company name is required";
-    }
-
-    if (!fieldValues.role) {
-      errors["role"] = "Role is required";
-    }
-
-    setFieldErrors(errors);
-
-    if (Object.keys(errors).length !== 0) {
-      return;
-    }
-
+  const updateAuthenticatedUser = useUpdateAuthenticatedUser();
+  async function onSubmit(data: z.infer<typeof OnboardingFormSchema>) {
     let token: string | undefined = undefined;
 
     token = uuidv4();
 
-    const payload: Partial<AuthenticatedUser> = {
-      email: fieldValues.email as string,
+    setIsSubmitting(true);
+
+    const payload = {
+      email: data.email,
       profile: {
-        companyName: fieldValues.companyName as string,
+        companyName: data.companyName,
+        displayName: data.displayName,
       },
-      role: fieldValues.role as string,
-      newsletterSubscription: fieldValues.newsletterSubscription
-        ? fieldValues.newsletterSubscription
+      role: data.role,
+      newsletterSubscription: data.newsletterSubscription
+        ? data.newsletterSubscription
         : false,
       cookieToken: token,
     };
 
-    setMessageBoxState(() => ({
-      activate: true,
-      status: "progressing",
-      description: null,
-      message: "Uploading...",
-    }));
-
     try {
-      const user = await updateUser.mutateAsync({
+      const user = await updateAuthenticatedUser.mutateAsync({
         payload,
         accessToken,
       });
 
-      if (amplitudeIsInit) {
-        sendAmplitudeData("submit_onboarding_form");
-      }
-
-      setMessageBoxState(() => ({
-        activate: true,
-        status: "success",
-        description: null,
-        message: "Succeed.",
-      }));
-
-      await axios.post("/api/set-user-cookie", {
-        key: "instill-ai-user",
-        value: JSON.stringify({
-          cookieToken: token,
+      await fetch("/api/set-user-cookie", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          key: "instill-ai-user",
+          value: JSON.stringify({
+            cookieToken: token,
+          }),
         }),
       });
       router.push(`/${user.id}/pipelines`);
     } catch (error) {
-      const message =
-        error instanceof Error
-          ? error.message
-          : "Something went wrong when uploading the form";
-      setMessageBoxState(() => ({
-        activate: true,
-        status: "error",
-        description: null,
-        message,
-      }));
+      toastInstillError({
+        title: "Something went wrong when onboarding your account",
+        toast,
+        error,
+      });
+    } finally {
+      setIsSubmitting(false);
     }
-  }, [fieldValues, amplitudeIsInit, router, updateUser, accessToken]);
+  }
 
   return (
-    <FormRoot formLess={false}>
-      <div className="mb-10 flex flex-col gap-y-5">
-        <BasicTextField
-          id="email"
-          label="Your email"
-          description="Fill your email address"
-          required={true}
-          value={fieldValues.email}
-          onChange={(event) => handleFieldChange("email", event)}
-          error={fieldErrors.email}
+    <Form.Root {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex flex-col gap-y-5"
+      >
+        <Form.Field
+          control={form.control}
+          name="email"
+          render={({ field }) => {
+            return (
+              <Form.Item>
+                <Form.Label className="product-body-text-3-semibold">
+                  Email
+                </Form.Label>
+                <Form.Control>
+                  <Input.Root>
+                    <Input.Core
+                      {...field}
+                      type="text"
+                      className="!product-body-text-2-regular"
+                      value={field.value ?? ""}
+                      autoComplete="off"
+                    />
+                  </Input.Root>
+                </Form.Control>
+                <Form.Message />
+                <Form.Description text="Fill your email address" />
+              </Form.Item>
+            );
+          }}
         />
-        <BasicTextField
-          id="org-name"
-          label="Your company"
-          required={true}
-          description="Fill your company name"
-          value={fieldValues.companyName}
-          onChange={(event) => handleFieldChange("companyName", event)}
-          error={fieldErrors.companyName}
+        <Form.Field
+          control={form.control}
+          name="displayName"
+          render={({ field }) => {
+            return (
+              <Form.Item>
+                <Form.Label className="product-body-text-3-semibold">
+                  Display Name
+                </Form.Label>
+                <Form.Control>
+                  <Input.Root>
+                    <Input.Core
+                      {...field}
+                      type="text"
+                      className="!product-body-text-2-regular"
+                      value={field.value ?? ""}
+                      autoComplete="off"
+                    />
+                  </Input.Root>
+                </Form.Control>
+                <Form.Message />
+                <Form.Description text="Fill your name" />
+              </Form.Item>
+            );
+          }}
         />
-        <BasicSingleSelect
-          id="role"
-          label="Your role"
-          required={true}
-          options={instillUserRoles}
-          value={selectedRoleOption}
-          description="Pick a role closest to your job in your company"
-          onChange={handleRoleChange}
-          error={fieldErrors.role}
+        <Form.Field
+          control={form.control}
+          name="companyName"
+          render={({ field }) => {
+            return (
+              <Form.Item>
+                <Form.Label className="product-body-text-3-semibold">
+                  Your Company
+                </Form.Label>
+                <Form.Control>
+                  <Input.Root>
+                    <Input.Core
+                      {...field}
+                      type="text"
+                      className="!product-body-text-2-regular"
+                      value={field.value ?? ""}
+                      autoComplete="off"
+                    />
+                  </Input.Root>
+                </Form.Control>
+                <Form.Message />
+                <Form.Description text="Fill your company name" />
+              </Form.Item>
+            );
+          }}
         />
-        <BasicToggleField
-          id="newsletter-subscription"
-          label="Newsletter subscription"
-          value={fieldValues.newsletterSubscription || false}
-          required={true}
-          description="Receive the latest news from Instill AI for product updates, community highlights, blog posts, useful tutorials and more! You can unsubscribe any time."
-          onChange={(event) =>
-            handleFieldChange("newsletterSubscription", event)
-          }
+        <Form.Field
+          control={form.control}
+          name="role"
+          render={({ field }) => {
+            return (
+              <Form.Item className="flex flex-col gap-y-2.5 md:w-1/2">
+                <Form.Label className="product-body-text-3-semibold">
+                  Your role
+                </Form.Label>
+                <Form.Control>
+                  <Select.Root
+                    value={field?.value}
+                    onValueChange={(value: string) => {
+                      field.onChange(value);
+                    }}
+                  >
+                    <Select.Trigger className="mt-auto w-full">
+                      <Select.Value />
+                    </Select.Trigger>
+                    <Select.Content>
+                      <Select.Group>
+                        {instillUserRoles.map((role) => (
+                          <Select.Item
+                            key={role.value}
+                            value={role.value}
+                            label={role.label}
+                          />
+                        ))}
+                      </Select.Group>
+                    </Select.Content>
+                  </Select.Root>
+                </Form.Control>
+                <Form.Message />
+                <Form.Description text="Pick a role closest to your job in your company" />
+              </Form.Item>
+            );
+          }}
         />
-      </div>
-      <div className="flex flex-row">
-        <BasicProgressMessageBox
-          state={messageBoxState}
-          setActivate={(activate) =>
-            setMessageBoxState((prev) => ({
-              ...prev,
-              activate,
-            }))
-          }
-          width="w-[25vw]"
-          closable={true}
+        <Form.Field
+          control={form.control}
+          name="newsletterSubscription"
+          render={({ field }) => {
+            return (
+              <div className="flex-rol flex gap-x-3">
+                <div className="pt-1">
+                  <Form.Control>
+                    <Switch
+                      checked={field.value}
+                      onCheckedChange={(e) => {
+                        field.onChange(e);
+                      }}
+                    />
+                  </Form.Control>
+                </div>
+                <div className="flex flex-col">
+                  <p className="text-semantic-fg-primary product-body-text-2-semibold">
+                    Subscribe to our newsletter
+                  </p>
+                  <p className="text-semantic-fg-secondary product-body-text-2-regular">
+                    For product updates, community highlights, blog posts,
+                    useful tutorials and more!
+                  </p>
+                </div>
+              </div>
+            );
+          }}
         />
-        <SolidButton
-          disabled={false}
-          type="button"
-          position="ml-auto my-auto"
-          color="primary"
-          onClickHandler={handleSubmit}
-        >
-          Start
-        </SolidButton>
-      </div>
-    </FormRoot>
+        <div className="flex flex-row-reverse w-full">
+          <Button type="submit" variant="primary" disabled={isSubmitting}>
+            {isSubmitting ? "Submitting..." : "Start"}
+          </Button>
+        </div>
+      </form>
+    </Form.Root>
   );
 };
