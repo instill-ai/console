e630cb068e43e5e4df5f7cca04d0adb7202167b8
Po Chun Chiu <57251712+EiffelFly@users.noreply.github.com>
2024-10-18 17:07:12 +0900

    fix: fix onboarding form e2e (#1545)
    Because
    
    - fix onboarding form e2e
    
    This commit
    
    - fix onboarding form e2e

--- apps/console/integration-test/data-testid.ts
+++
@@ -1,6 +0,0 @@
-export const DataTestID = {
-  deleteResourceDialog: "delete-resource-dialog",
-  setupComponentDialog: "setup-component-dialog",
-  createPipelineDialog: "create-pipeline-dialog",
-  SelectComponentDialog: "select-component-dialog",
-};
