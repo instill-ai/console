ed9a9f6f7424278bfe4ea8af4f7ff4b9e9b19878
Wojciech Bandzerewicz <57405495+thewbuk@users.noreply.github.com>
2024-10-25 14:43:50 +0100

    fix index

--- packages/toolkit/src/view/dashboard/index.ts
+++ packages/toolkit/src/view/dashboard/index.ts
@@ -1,7 +1,5 @@
 export * from "./DashboardPipelineDetailsPageMainView";
 export * from "./SemiCircleProgress";
 export * from "./DashboardContainer";
-export * from "./cost/model/ModelCreditCostTrendChart";
-export * from "./cost/pipeline/PipelineCreditCostTrendChart";
 export * from "./activity";
 export * from "./cost";
