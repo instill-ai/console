61f697795defc137aee973ef27b115f9444b7adc
Wojciech Bandzerewicz <57405495+thewbuk@users.noreply.github.com>
2024-10-29 12:15:38 +0000

    break slider into components

--- packages/design-system/src/new-ui/Slider/Slider.tsx
+++ packages/design-system/src/new-ui/Slider/Slider.tsx
@@ -1,21 +1,14 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import * as SliderPrimitive from "@radix-ui/react-slider"
-import { cn } from "../../utils"
-
+import * as React from "react";
+import * as SliderPrimitive from "@radix-ui/react-slider";
+import { cn } from "../../utils";
 import { ComponentPropsWithoutRef } from "react";
 
-type SliderProps = ComponentPropsWithoutRef<typeof SliderPrimitive.Root> & {
-  trackClassName?: string;
-  rangeClassName?: string;
-  thumbClassName?: string;
-};
-
-const Slider = React.forwardRef<
+const Root = React.forwardRef<
   React.ElementRef<typeof SliderPrimitive.Root>,
-  SliderProps
->(({ className, trackClassName, rangeClassName, thumbClassName, ...props }, ref) => (
+  ComponentPropsWithoutRef<typeof SliderPrimitive.Root>
+>(({ className, ...props }, ref) => (
   <SliderPrimitive.Root
     ref={ref}
     className={cn(
@@ -23,25 +16,55 @@ const Slider = React.forwardRef<
       className
     )}
     {...props}
-  >
-    <SliderPrimitive.Track
-      className={cn(
-        "relative h-2 w-full grow overflow-hidden rounded-full bg-secondary",
-        trackClassName
-      )}
-    >
-      <SliderPrimitive.Range
-        className={cn("absolute h-full bg-primary", rangeClassName)}
-      />
-    </SliderPrimitive.Track>
-    <SliderPrimitive.Thumb
-      className={cn(
-        "block h-5 w-5 rounded-full border-2 border-primary bg-background ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
-        thumbClassName
-      )}
-    />
-  </SliderPrimitive.Root>
-))
-Slider.displayName = SliderPrimitive.Root.displayName
+  />
+));
+Root.displayName = SliderPrimitive.Root.displayName;
 
-export { Slider }
+const Track = React.forwardRef<
+  React.ElementRef<typeof SliderPrimitive.Track>,
+  ComponentPropsWithoutRef<typeof SliderPrimitive.Track>
+>(({ className, ...props }, ref) => (
+  <SliderPrimitive.Track
+    ref={ref}
+    className={cn(
+      "relative h-2 w-full grow overflow-hidden rounded-full bg-secondary",
+      className
+    )}
+    {...props}
+  />
+));
+Track.displayName = SliderPrimitive.Track.displayName;
+
+const Range = React.forwardRef<
+  React.ElementRef<typeof SliderPrimitive.Range>,
+  ComponentPropsWithoutRef<typeof SliderPrimitive.Range>
+>(({ className, ...props }, ref) => (
+  <SliderPrimitive.Range
+    ref={ref}
+    className={cn("absolute h-full bg-primary", className)}
+    {...props}
+  />
+));
+Range.displayName = SliderPrimitive.Range.displayName;
+
+const Thumb = React.forwardRef<
+  React.ElementRef<typeof SliderPrimitive.Thumb>,
+  ComponentPropsWithoutRef<typeof SliderPrimitive.Thumb>
+>(({ className, ...props }, ref) => (
+  <SliderPrimitive.Thumb
+    ref={ref}
+    className={cn(
+      "block h-5 w-5 rounded-full border-2 border-primary bg-background ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+      className
+    )}
+    {...props}
+  />
+));
+Thumb.displayName = SliderPrimitive.Thumb.displayName;
+
+export const Slider = {
+  Root,
+  Track,
+  Range,
+  Thumb,
+};
