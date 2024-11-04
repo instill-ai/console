94e7acb6f32e1509c44753bcead182604c179a36
Wojciech Bandzerewicz <57405495+thewbuk@users.noreply.github.com>
2024-11-04 18:45:22 +0000

    add const

--- packages/toolkit/src/view/dashboard/activity/DashboardActivityPageMainView.tsx
+++ packages/toolkit/src/view/dashboard/activity/DashboardActivityPageMainView.tsx
@@ -237,14 +237,14 @@ export const DashboardActivityPageMainView = () => {
 
     return {
       completed: {
-        statusType: "STATUS_COMPLETED",
-        type: "model",
+        statusType: "STATUS_COMPLETED" as const,
+        type: "model" as const,
         amount: completedModel?.triggerCount || 0,
         delta: 0
       },
       errored: {
-        statusType: "STATUS_ERRORED",
-        type: "model",
+        statusType: "STATUS_ERRORED" as const,
+        type: "model" as const,
         amount: erroredModel?.triggerCount || 0,
         delta: 0
       }
