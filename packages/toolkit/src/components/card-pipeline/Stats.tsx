f5a828ff140eae5d471d10a71703fe22de9c4595
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2024-11-26 16:04:40 +0800

    fix: fix prettier

--- packages/toolkit/src/components/card-pipeline/Stats.tsx
+++ packages/toolkit/src/components/card-pipeline/Stats.tsx
@@ -17,7 +17,9 @@ export const Stats = (props: StatsProps) => {
     <div className="mt-auto flex w-full flex-row items-center gap-x-4">
       <div className="flex flex-row items-center gap-x-1 uppercase text-semantic-fg-secondary font-medium text-sm">
         <Icons.Rocket01 className="w-4 h-4 stroke-semantic-fg-primary" />
-        {runCount ? convertLongNumberToK(runCount) : 0} Run
+        {runCount
+          ? convertLongNumberToK(runCount)
+          : 0} Run
         {runCount !== 1 ? "s" : ""}
       </div>
       <div className="ml-auto product-body-text-3-regular text-semantic-node-connector-off">
