8fc89d918d62471e8c217b05307c1c064ea560cc
Chang, Hui-Tang <huitang.chang@instill.tech>
2024-12-09 21:33:52 +0800

    fix: allow more data types in the variable default field

--- packages/toolkit/src/view/recipe-editor/lib/schema.ts
+++ packages/toolkit/src/view/recipe-editor/lib/schema.ts
@@ -91,7 +91,6 @@ export const InstillYamlSchema = {
               type: "string",
             },
             default: {
-              type: "string",
             },
             "instill-ui-multiline": {
               type: "boolean",
