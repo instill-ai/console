3d25078da3d87410171c769a534dca37718611a8
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2025-01-23 19:22:12 +0800

    fix: fix UId to Uid

--- packages/sdk/src/table/types.ts
+++ packages/sdk/src/table/types.ts
@@ -242,10 +242,3 @@ export type MoveNamespaceTableRowRequest = {
   rowUids: string[];
   beforeRowUid?: string;
 };
-
-export type MoveNamespaceTableRowRequest = {
-  namespaceId: string;
-  tableUId: string;
-  rowUids: string[];
-  afterRowUId?: string;
-};
