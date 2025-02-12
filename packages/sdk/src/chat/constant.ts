46016698371523352540f96c6c49e4295a2f224e
Alexander Petcoglo <mail@rckt.cc>
2025-02-12 16:01:51 +0100

    fix: move enum to constants

---
+++ packages/sdk/src/chat/constant.ts
@@ -0,0 +1,7 @@
+export const InstillChatMessageEnum = {
+  Started: "CHAT_STARTED",
+  StatusUpdated: "CHAT_STATUS_UPDATED",
+  OutputUpdated: "CHAT_OUTPUT_UPDATED",
+  Ended: "CHAT_ENDED",
+  UserMessage: "CHAT_USER_MESSAGE",
+} as const;
