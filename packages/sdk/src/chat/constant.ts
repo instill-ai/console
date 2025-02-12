8fafe5b19774300b2b2e92f2cc31bc9041677c19
Alexander Petcoglo <mail@rckt.cc>
2025-02-12 16:30:36 +0100

    remove redundant file

--- packages/sdk/src/chat/constant.ts
+++
@@ -1,7 +0,0 @@
-export const instillChatMessageEnum = {
-  Started: "CHAT_STARTED",
-  StatusUpdated: "CHAT_STATUS_UPDATED",
-  OutputUpdated: "CHAT_OUTPUT_UPDATED",
-  Ended: "CHAT_ENDED",
-  UserMessage: "CHAT_USER_MESSAGE",
-} as const;
