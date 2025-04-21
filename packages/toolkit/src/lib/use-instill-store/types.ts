ee2e5b211b1d49e64ea179e43e353933aab01d76
RCKT <3314891+orangecoloured@users.noreply.github.com>
2025-04-21 17:23:55 +0200

    fix: instill storage typing (#1701)

--- packages/toolkit/src/lib/use-instill-store/types.ts
+++ packages/toolkit/src/lib/use-instill-store/types.ts
@@ -357,8 +357,10 @@ export type ChatSlice = {
   updateUploadFilesPending: (
     fn: (prev: Record<string, PendingFile[]>) => Record<string, PendingFile[]>,
   ) => void;
-  tempFileCatalogId: string;
-  updateTempFileCatalogId: (fn: (prev: string) => string) => void;
+  tempFileCatalogId: Nullable<string>;
+  updateTempFileCatalogId: (
+    fn: (prev: Nullable<string>) => Nullable<string>,
+  ) => void;
 };
 
 export type TableMode = "table" | "preview";
@@ -419,6 +421,6 @@ export type PendingFile = {
   progress: number;
   status: "uploading" | "success" | "error";
   type: string;
-  catalogFileUid?: string;
+  fileUid?: string;
   size: number;
 };
