7435958714751766d65430a29acccf52ad902c0b
Alexander Petcoglo <mail@rckt.cc>
2025-06-20 14:43:17 +0200

    feat(hooks): extend react query props

--- packages/toolkit/src/lib/react-query-service/types.ts
+++ packages/toolkit/src/lib/react-query-service/types.ts
@@ -1,6 +1,8 @@
+import { QueryObserverOptions } from "@tanstack/react-query";
 import { Nullable } from "instill-sdk";
 
-export type QueryBaseProps = {
+export type QueryBaseProps<T = unknown> = {
   accessToken: Nullable<string>;
   enabled: boolean;
+  refetchInterval?: QueryObserverOptions<T>["refetchInterval"];
 };
